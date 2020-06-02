67c1692805f5ac39f781991c452aa91c1a5adc40
Tony Fast <tony.fast@gmail.com>
2020-06-02 19:00:07 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -7,7 +7,7 @@ html_theme = "pydata_sphinx_theme"
 master_doc = "README"
 source_suffix = ".rst .md .ipynb .py".split()
 #extensions = "recommonmark nbsphinx sphinx.ext.autodoc sphinx.ext.coverage sphinx.ext.napoleon autoapi.extension sphinx.ext.mathjax sphinx_copybutton     sphinx.ext.viewcode".split()
-extensions = "nbsphinx".split()
+extensions = "recommonmark nbsphinx".split()
 
 # Exclude build directory and Jupyter backup files:
 exclude_patterns = ["_build", "*checkpoint*"]
