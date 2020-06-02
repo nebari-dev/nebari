4cb0494061204afde9412d2d6f04fe474ca98b7a
Tony Fast <tony.fast@gmail.com>
2020-06-02 18:52:55 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,11 +1,9 @@
-from jupyter_sphinx_theme import *
-init_theme()
 # import sphinx.writers.html5
 # sphinx.writers.html5.HTML5Translator.visit_pending_xref = lambda *x:...
 # sphinx.writers.html5.HTML5Translator.depart_pending_xref = lambda *x:...
 BLOG_TITLE = title = html_title = "Qhub code as infrastructure."
 BLOG_AUTHOR = author = "Quansight"
-html_theme = "classic"
+html_theme = "pydata_sphinx_theme"
 master_doc = "README"
 source_suffix = ".rst .md .ipynb .py".split()
 #extensions = "recommonmark nbsphinx sphinx.ext.autodoc sphinx.ext.coverage sphinx.ext.napoleon autoapi.extension sphinx.ext.mathjax sphinx_copybutton     sphinx.ext.viewcode".split()
