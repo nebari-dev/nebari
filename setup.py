40573f58fbb0824f623a641b79f0734b9ec8c83a
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2020-06-03 12:46:16 -0400

    Force configuration application

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='qhub-ops',
-    version='0.1.3',
+    version='0.1.4',
     description='Management of QHub on Cloud Infrastructure',
     long_description=long_description,
     long_description_content_type='text/markdown',
