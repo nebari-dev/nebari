61166b5efc08c5da29cbd97e214927ed12b4de4a
Tony Fast <tony.fast@gmail.com>
2020-06-10 13:36:21 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -9,6 +9,37 @@ source_suffix = ".rst .md .ipynb .py".split()
 #extensions = "recommonmark nbsphinx sphinx.ext.autodoc sphinx.ext.coverage sphinx.ext.napoleon autoapi.extension sphinx.ext.mathjax sphinx_copybutton     sphinx.ext.viewcode".split()
 extensions = "recommonmark nbsphinx".split()
 
+html_theme = 'sphinx_material'
+
+# Material theme options (see theme.conf for more information)
+html_theme_options = {
+
+    # Set the name of the project to appear in the navigation.
+    'nav_title': 'Project Name',
+
+    # Set you GA account ID to enable tracking
+    #'google_analytics_account': 'UA-XXXXX',
+
+    # Specify a base_url used to generate sitemap.xml. If not
+    # specified, then no sitemap will be built.
+    'base_url': 'https://project.github.io/project',
+
+    # Set the color and the accent color
+    'color_primary': 'blue',
+    'color_accent': 'light-blue',
+
+    # Set the repo location to get a badge with stats
+    'repo_url': 'https://github.com/project/project/',
+    'repo_name': 'Project',
+
+    # Visible levels of the global TOC; -1 means unlimited
+    'globaltoc_depth': 3,
+    # If False, expand all TOC entries
+    'globaltoc_collapse': False,
+    # If True, show hidden TOC entries
+    'globaltoc_includehidden': False,
+}
+
 # Exclude build directory and Jupyter backup files:
 exclude_patterns = ["_build", "*checkpoint*"]
 autoapi_type = "python"
