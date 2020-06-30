b493f5f105c1d564853f1c94115844e576e183f5
Amit Kumar <dtu.amit@gmail.com>
2020-06-30 10:27:35 +0100

    Add high level architecture diagram

--- architecture/README.md
+++ architecture/README.md
@@ -1,21 +1,25 @@
 # Architecture Diagrams
 
+## High Level Architecture
+
+![High Level](generated/high_level_architecture.png "High Level Architecture Diagram")
+
 ## AWS
 
 * Low level
 
-![alt text](generated/qhub_aws_architecture.png "AWS Low Level Architecture Diagram")
+![AWS](generated/qhub_aws_architecture.png "AWS Low Level Architecture Diagram")
 
 
 ## GCP
 
 * Low level
 
-![alt text](generated/qhub_gcp_architecture.png "GCP Low Level Architecture Diagram")
+![GCP](generated/qhub_gcp_architecture.png "GCP Low Level Architecture Diagram")
 
 
 ## Digital Ocean
 
 * Low level
 
-![alt text](generated/qhub_do_architecture.png "DO Low Level Architecture Diagram")
+![DO](generated/qhub_do_architecture.png "DO Low Level Architecture Diagram")
---
+++ architecture/high_level.py
@@ -0,0 +1,35 @@
+import os
+
+from diagrams.custom import Custom
+
+from diagrams import Cluster, Diagram
+from diagrams.k8s.compute import Pod
+from diagrams.k8s.storage import PersistnetVolume as PersistentVolume
+
+from diagrams.gcp.network import LoadBalancing
+
+
+def custom_icon(filename):
+    return os.path.join('architecture/icons', filename)
+
+
+with Diagram(f"QHub Architecture: High Level", filename=f'high_level_architecture', show=False, direction="TB",):
+    with Cluster('Kubernetes Cluster'):
+        conda_pvc = PersistentVolume('Conda (Persistent Volume)')
+        nfs_pvc = PersistentVolume('NFS (Persistent Volume)')
+        elb = LoadBalancing('Ingress')
+
+        with Cluster("Pods"):
+            dask_gateway = Custom('Dask Gateway', custom_icon('dask.png'))
+            jupyterhub = Custom('JupyterHub', custom_icon('jupyter_hub.png'))
+            dask_scheduler = Custom('Dask Scheduler', custom_icon('dask.png'))
+            nfs_server = Pod('NFS Server')
+            conda_store = Custom('Conda Store', custom_icon('conda.png'))
+            nginx = Custom('Nginx', custom_icon('nginx.png'))
+
+    nginx >> elb
+    nginx >> jupyterhub
+    [nfs_server, conda_store, dask_gateway] << jupyterhub
+    [conda_store, dask_scheduler] << dask_gateway
+    nfs_server >> nfs_pvc
+    conda_store >> conda_pvc
