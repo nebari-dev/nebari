a214a794d60192c7b661adec2ca2a43ded81852a
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2020-09-24 17:14:27 -0400

    Move qhub images to using true environment.yaml file

--- qhub/template/{{ cookiecutter.repo_directory }}/image/scripts/install-conda-environment.sh
+++ qhub/template/{{ cookiecutter.repo_directory }}/image/scripts/install-conda-environment.sh
@@ -4,7 +4,7 @@ set -xe
 # Assumes conda packages to install in argument "$1"
 
 # ==== install conda dependencies ====
-/opt/conda/bin/conda install -c conda-forge --freeze-installed -y --file $1
+/opt/conda/bin/conda env update -f $1
 
 # ========= list dependencies ========
 /opt/conda/bin/conda list
