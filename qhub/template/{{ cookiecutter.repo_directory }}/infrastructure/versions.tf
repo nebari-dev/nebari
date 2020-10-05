ab879d4d1a2839ae12485ba9d54d6ff09aed4a66
t-potts <tylerjacobpotts@gmail.com>
2020-10-05 16:37:07 -0500

    add files for .13 upgrade

--- qhub/template/{{ cookiecutter.repo_directory }}/infrastructure/versions.tf
+++ qhub/template/{{ cookiecutter.repo_directory }}/infrastructure/versions.tf
@@ -1,7 +1,15 @@
 terraform {
   required_providers {
+{%- if cookiecutter.provider == "aws" %}
+    aws = {
+      source = "hashicorp/aws"
+{%- elif cookiecutter.provider == "gcp" %}
+    google = {
+      source = "hashicorp/google"
+{%- elif cookiecutter.provider == "do" %}
     digitalocean = {
-      source = "digitalocean/digitalocean"
+      source = "digitalocean/digitalocean" 
+{% endif %}
     }
     helm = {
       source = "hashicorp/helm"
---
+++ qhub/template/{{ cookiecutter.repo_directory }}/terraform-state/versions.tf
@@ -0,0 +1,18 @@
+{%- if cookiecutter.provider == "aws" %}
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+  required_version = ">= 0.13"
+}
+{%- elif cookiecutter.provider == "gcp" %}
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+  required_version = ">= 0.13"
+}
