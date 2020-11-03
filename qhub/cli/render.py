71b9bca46c9d972b1484f296007180e70372b7b0
Prasun Anand <prasunanand.bitsp@gmail.com>
2020-11-03 17:34:06 +0530

    Remove config option from Qhub render

--- qhub/cli/render.py
+++ qhub/cli/render.py
@@ -9,7 +9,6 @@ def create_render_subcommand(subparser):
         "-f", "--force", action="store_true", help="overwrite existing files"
     )
     subparser.add_argument("-o", "--output", default="./", help="output directory")
-    subparser.add_argument("-c", "--config", help="cookiecutter configuration")
     subparser.set_defaults(func=handle_render)
 
 
@@ -17,7 +16,7 @@ def handle_render(args):
     import pathlib
     import yaml
 
-    config_filename = pathlib.Path(args.config)
+    config_filename = pathlib.Path("./qhub-config.yaml")
     if not config_filename.is_file():
         raise ValueError(
             f"passed in configuration filename={config_filename} must exist"
@@ -29,6 +28,6 @@ def handle_render(args):
     verify(config)
 
     if args.input is None:
-        render_default_template(args.output, args.config, force=args.force)
+        render_default_template(args.output, ".", force=args.force)
     else:
-        render_template(args.input, args.output, args.config, force=args.force)
+        render_template(args.input, args.output, ".", force=args.force)
