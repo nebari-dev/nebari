e0d2ec76b5ba489daf00f9f717527eb9f399d596
t-potts <tylerjacobpotts@gmail.com>
2020-11-16 18:10:36 -0600

    Add changes to schema file

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -7,19 +7,19 @@ from qhub.initialize import render_config
 @pytest.mark.parametrize(
     "project, domain, cloud_provider, ci_provider, oauth_provider",
     [
-        ('do-pytest', 'do.qhub.dev', 'do', 'github-actions', 'github'),
-        ('aws-pytest', 'aws.qhub.dev', 'aws', 'github-actions', 'github'),
-        ('gcp-pytest', 'gcp.qhub.dev', 'gcp', 'github-actions', 'github'),
+        ("do-pytest", "do.qhub.dev", "do", "github-actions", "github"),
+        ("aws-pytest", "aws.qhub.dev", "aws", "github-actions", "github"),
+        ("gcp-pytest", "gcp.qhub.dev", "gcp", "github-actions", "github"),
     ],
 )
 def test_schema(project, domain, cloud_provider, ci_provider, oauth_provider):
     config = render_config(
         project_name=project,
-        terraform_version='0.13.15'
+        terraform_version="0.13.15",
         qhub_domain=domain,
         cloud_provider=cloud_provider,
         ci_provider=ci_provider,
-        repository='github.com/test/test',
+        repository="github.com/test/test",
         repository_auto_provision=False,
         oauth_provider=oauth_provider,
         oauth_auto_provision=False,
