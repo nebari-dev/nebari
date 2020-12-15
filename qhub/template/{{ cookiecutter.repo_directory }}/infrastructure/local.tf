259cf92814b501fed609891f3cd6ac0524104edf
Brian Larsen <B_R_L@hotmail.com>
2020-12-15 12:41:14 -0600

    Format

--- qhub/template/{{ cookiecutter.repo_directory }}/infrastructure/local.tf
+++ qhub/template/{{ cookiecutter.repo_directory }}/infrastructure/local.tf
@@ -2,5 +2,5 @@ provider "kind" {}
 
 module "kubernetes" {
   source = "github.com/quansight/qhub-terraform-modules//modules/local/kubernetes?ref=brl0_local"
-  name     = local.cluster_name
+  name   = local.cluster_name
 }
