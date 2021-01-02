2525944a4bc42e1cf1c72b1f893cf258cdf137de
synaptic <rajat@synaptic.com>
2021-01-03 02:49:28 +0530

    qhub destroy implementation

---
+++ qhub/destroy.py
@@ -0,0 +1,37 @@
+import logging
+from os import path
+import os
+import re
+import json
+from subprocess import check_output, run
+from shutil import which
+
+from qhub.utils import timer, change_directory
+from qhub.provider.dns.cloudflare import update_record
+from qhub.constants import SUPPORTED_TERRAFORM_MINOR_RELEASES
+
+
+logger = logging.getLogger(__name__)
+
+
+def destroy_configuration(config):
+    logger.info(f'Removing all infrastructure, your local files will still remain, 
+    you can use `qhub deploy` to re-install infrastructure using same config file')
+
+    with timer(logger, "destroying QHub"):
+        # 01 Verify configuration file exists
+        verify_configuration_file_exists()
+
+        # 02 Check terraform
+        check_terraform()
+
+        # 03 Check Environment Variables
+        check_cloud_credentials(config)
+
+        # 04 Remove all infrastructure
+        with change_directory("infrastructure"):
+            run(["terraform", "destroy", "-auto-approve"])
+
+        # 06 Remove terraform backend remote state bucket
+        with change_directory("terraform-state"):
+            run(["terraform", "destroy", "-auto-approve"])
--- qhub/utils.py
+++ qhub/utils.py
@@ -1,7 +1,9 @@
 import time
 import os
 import contextlib
+import re
 
+from qhub.constants import SUPPORTED_TERRAFORM_MINOR_RELEASES
 
 @contextlib.contextmanager
 def timer(logger, prefix):
@@ -16,3 +18,73 @@ def change_directory(directory):
     os.chdir(directory)
     yield
     os.chdir(current_directory)
+
+def check_cloud_credentials(config):
+    if config["provider"] == "gcp":
+        for variable in {"GOOGLE_CREDENTIALS"}:
+            if variable not in os.environ:
+                raise Exception(
+                    f"""Missing the following required environment variable: {variable}\n
+                    Please see the documentation for more information: {GCP_ENV_DOCS}"""
+                )
+    elif config["provider"] == "aws":
+        for variable in {
+            "AWS_ACCESS_KEY_ID",
+            "AWS_SECRET_ACCESS_KEY",
+            "AWS_DEFAULT_REGION",
+        }:
+            if variable not in os.environ:
+                raise Exception(
+                    f"""Missing the following required environment variable: {variable}\n
+                    Please see the documentation for more information: {AWS_ENV_DOCS}"""
+                )
+    elif config["provider"] == "do":
+        for variable in {
+            "AWS_ACCESS_KEY_ID",
+            "AWS_SECRET_ACCESS_KEY",
+            "SPACES_ACCESS_KEY_ID",
+            "SPACES_SECRET_ACCESS_KEY",
+            "DIGITALOCEAN_TOKEN",
+        }:
+            if variable not in os.environ:
+                raise Exception(
+                    f"""Missing the following required environment variable: {variable}\n
+                    Please see the documentation for more information: {DO_ENV_DOCS}"""
+                )
+
+        if os.environ["AWS_ACCESS_KEY_ID"] != os.environ["SPACES_ACCESS_KEY_ID"]:
+            raise Exception(
+                f"""The environment variables AWS_ACCESS_KEY_ID and SPACES_ACCESS_KEY_ID must be equal\n
+                See {DO_ENV_DOCS} for more information"""
+            )
+
+        if (
+            os.environ["AWS_SECRET_ACCESS_KEY"]
+            != os.environ["SPACES_SECRET_ACCESS_KEY"]
+        ):
+            raise Exception(
+                f"""The environment variables AWS_SECRET_ACCESS_KEY and SPACES_SECRET_ACCESS_KEY must be equal\n
+                See {DO_ENV_DOCS} for more information"""
+            )
+    else:
+        raise Exception("Cloud Provider configuration not supported")
+
+def check_terraform():
+    # 01 Check if Terraform works
+    if which("terraform") is None:
+        raise Exception(
+            f"Please install Terraform with one of the following minor releases: {SUPPORTED_TERRAFORM_MINOR_RELEASES}"
+        )
+
+    # 02 Check version of Terraform
+    version_out = check_output(["terraform", "--version"]).decode("utf-8")
+    minor_release = re.search(r"(\d+)\.(\d+)", version_out).group(0)
+
+    if minor_release not in SUPPORTED_TERRAFORM_MINOR_RELEASES:
+        raise Exception(
+            f"Unsupported Terraform version. Supported minor releases: {SUPPORTED_TERRAFORM_MINOR_RELEASES}"
+        )
+
+def verify_configuration_file_exists():
+    if not path.exists("qhub-config.yaml"):
+        raise Exception('Configuration file "qhub-config.yaml" does not exist')
