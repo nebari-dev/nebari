b8883748779c15066e23b1232703dffd8523124c
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2021-01-14 20:10:27 -0500

    Initial work towards adding password based auth

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -5,7 +5,7 @@ from qhub.initialize import render_config
 
 
 @pytest.mark.parametrize(
-    "project, terraform_version, domain, cloud_provider, ci_provider, oauth_provider",
+    "project, terraform_version, domain, cloud_provider, ci_provider, auth_provider",
     [
         ("do-pytest", "0.13.5", "do.qhub.dev", "do", "github-actions", "github"),
         ("aws-pytest", "0.13.5", "aws.qhub.dev", "aws", "github-actions", "github"),
@@ -13,7 +13,7 @@ from qhub.initialize import render_config
     ],
 )
 def test_schema(
-    project, terraform_version, domain, cloud_provider, ci_provider, oauth_provider
+    project, terraform_version, domain, cloud_provider, ci_provider, auth_provider
 ):
     config = render_config(
         project_name=project,
@@ -22,10 +22,10 @@ def test_schema(
         cloud_provider=cloud_provider,
         ci_provider=ci_provider,
         repository="github.com/test/test",
-        oauth_provider=oauth_provider,
+        auth_provider=auth_provider,
         repository_auto_provision=False,
-        oauth_auto_provision=False,
-        kubernetes_version="1.18.0",
+        auth_auto_provision=False,
+        kubernetes_version='1.18.0',
         disable_prompt=True,
     )
     assert qhub.schema.verify(config) is None
