7e78efe437c3ab1b87a14445f2905fed8c5a6ff1
Adam Lewis <balast@users.noreply.github.com>
2021-02-10 10:12:25 -0600

    delete another dash in name

--- qhub/template/{{ cookiecutter.repo_directory }}/infrastructure/state.tf
+++ qhub/template/{{ cookiecutter.repo_directory }}/infrastructure/state.tf
@@ -31,7 +31,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "{{ cookiecutter.project_name }}-terraform-state"
     storage_account_name = "{{ cookiecutter.project_name }}storage"
-    container_name       = "{{ cookiecutter.project_name }}-state"
+    container_name       = "{{ cookiecutter.project_name }}state"
     key                  = "terraform/{{ cookiecutter.project_name }}.tfstate"
   }
 }    
