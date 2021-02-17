7074c9fd09472731e761ba7bae582a9e5d5a53e9
Brian Larsen <B_R_L@hotmail.com>
2021-02-17 16:31:51 -0600

    Add node-group for conda-store

--- qhub/template/{{ cookiecutter.repo_directory }}/infrastructure/kubernetes.tf
+++ qhub/template/{{ cookiecutter.repo_directory }}/infrastructure/kubernetes.tf
@@ -54,6 +54,7 @@ module "kubernetes-conda-store-server" {
   name         = "conda-store"
   namespace    = var.environment
   nfs_capacity = "{{ cookiecutter.storage.conda_store }}"
+  node-group   = local.node_groups.general
   environments = {
 {% for key in cookiecutter.environments %}
     "{{ key }}" = file("../environments/{{ key }}")
