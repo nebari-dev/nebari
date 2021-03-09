fc8cdef0633facc7cd81bcf570f2fc08f9fcd454
Brian Larsen <B_R_L@hotmail.com>
2021-03-08 18:29:20 -0600

    Format

--- qhub/template/{{ cookiecutter.repo_directory }}/infrastructure/kubernetes.tf
+++ qhub/template/{{ cookiecutter.repo_directory }}/infrastructure/kubernetes.tf
@@ -129,7 +129,7 @@ module "kubernetes-ingress" {
   source     = "{{ cookiecutter.terraform_modules.repository }}//modules/kind/ingress?ref={{ cookiecutter.terraform_modules.rev }}"
 {% endif -%}
 
-  namespace = var.environment
+  namespace  = var.environment
 
   depends_on = [
     module.kubernetes-initialization
