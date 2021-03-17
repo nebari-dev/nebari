47618ce142a9110d7f28f5b00d6e298fd7d2e76b
viniciusdc <vinivdc2009@hotmail.com>
2021-03-17 14:12:59 -0300

    new subparser for linter

--- qhub/cli/validate.py
+++ qhub/cli/validate.py
@@ -8,10 +8,12 @@ from qhub.provider.cicd.linter import qhub_linter
 def create_validate_subcommand(subparser):
     subparser = subparser.add_parser("validate")
     subparser.add_argument("config", help="qhub configuration")
-    subparser.add_argument(
-        "--enable-commenting", help="Turn on PR commenting", func=qhub_linter
-    )
     subparser.set_defaults(func=handle_validate)
+
+    linter_parser = subparser.add_parser("linter")
+    linter_parser.add_argument(
+        "--enable-commenting", help="Turn on PR commenting")
+    linter_parser.set_defaults(func=qhub_linter)
 
 
 def handle_validate(args):
