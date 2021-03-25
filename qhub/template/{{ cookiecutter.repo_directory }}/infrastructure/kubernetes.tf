a41e1f6768c58a9e32e40d7dabc8de3db5ac2050
Brian Larsen <blarsen@quansight.com>
2021-03-24 19:22:53 -0500

    Affinity for nfs and conda-store (#323)
    * Add node-group for conda-store
    
    * Add node group for conda-store

--- qhub/template/{{ cookiecutter.repo_directory }}/infrastructure/kubernetes.tf
+++ qhub/template/{{ cookiecutter.repo_directory }}/infrastructure/kubernetes.tf
@@ -43,6 +43,7 @@ module "kubernetes-nfs-server" {
   name         = "nfs-server"
   namespace    = var.environment
   nfs_capacity = "{{ cookiecutter.storage.shared_filesystem }}"
+  node-group   = local.node_groups.general
 
   depends_on = [
     module.kubernetes-initialization
@@ -69,6 +70,7 @@ module "kubernetes-conda-store-server" {
   name         = "conda-store"
   namespace    = var.environment
   nfs_capacity = "{{ cookiecutter.storage.conda_store }}"
+  node-group   = local.node_groups.general
   environments = {
 {% for key in cookiecutter.environments %}
     "{{ key }}" = file("../environments/{{ key }}")
