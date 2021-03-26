d6d443cd791522c34580cbf32eebe11d348dc621
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2021-03-25 22:29:31 -0400

    Adding a well defined schema for current qhub-config.yaml

--- qhub/schema.py
+++ qhub/schema.py
@@ -3,6 +3,9 @@ import typing
 
 import pydantic
 
+class TerraformStateEnum(str, enum.Enum):
+    remote = "remote"
+    local = "local"
 
 class ProviderEnum(str, enum.Enum):
     local = "local"
@@ -11,171 +14,138 @@ class ProviderEnum(str, enum.Enum):
     gcp = "gcp"
     azure = "azure"
 
-
 class CiEnum(str, enum.Enum):
     github_actions = "github-actions"
 
+class AuthenticationEnum(str, enum.Enum):
+    password = "password"
+    github = "GitHub"
+    auth0 = "Auth0"
 
 class Base(pydantic.BaseModel):
     ...
 
+# =========== Authentication ==============
+
+class GitHubConfig(Base):
+    oauth_callback_url: str
+    client_id: str
+    client_secret: str
+
+class Auth0Config(Base):
+    client_id: str
+    client_secret: str
+    oauth_callback_url: str
+    scope: typing.List[str]
+    auth0_subdomain: str
+
+class Authentication(Base):
+    type: AuthenticationEnum
+    config: typing.Optional[typing.Union[Auth0Config, GitHubConfig]]
+
+# =========== Users and Groups =============
+
+class User(Base):
+    uid: str
+    primary_group: str
+    secondary_groups: Optional[typing.List[str]]
+
+class Group(Base):
+    gid: int
+
+# ============== Security ================
+
+class Security(Base):
+    # authentication
+    users: typing.Dict[str, User]
+    groups: typing.Dict[str, Group]
+
+# ================ Providers ===============
+
+class KeyValueDict(Base):
+    key: str
+    value: str
+
+class NodeSelector(Base):
+    general: KeyValueDict
+    user: KeyValueDict
+    worker: KeyValueDict
 
 class NodeGroup(Base):
     instance: str = "s-2vcpu-4gb"
     min_nodes: int = 1
     max_nodes: int = 1
 
-    class Config:
-        @staticmethod
-        def schema_extra(schema, model):
-            schema.update(
-                examples=[
-                    NodeGroup(instance="s-2vcpu-4gb", min_nodes=1, max_nodes=1).dict(),
-                    NodeGroup(instance="s-2vcpu-4gb", min_nodes=1, max_nodes=4).dict(),
-                ]
-            )
-            return schema
-
-
-class Provider(Base):
+class DigitalOceanProvider(Base):
     region: str
+    kubernetes_version: str
     node_groups: typing.Dict[str, NodeGroup]
 
-    class Config:
-        @staticmethod
-        def schema_extra(schema, model):
-            schema.update(
-                examples=[
-                    {
-                        "general": NodeGroup(
-                            instance="s-2vcpu-4gb", min_nodes=1, max_nodes=1
-                        ).dict(),
-                        "user": NodeGroup(
-                            instance="s-2vcpu-4gb", min_nodes=1, max_nodes=1
-                        ).dict(),
-                        "work": NodeGroup(
-                            instance="s-2vcpu-4gb", min_nodes=1, max_nodes=1
-                        ).dict(),
-                    }
-                ]
-            )
-            return schema
+class GoogleCloudPlatformProvider(Base):
+    project: str
+    region: str
+    zone: str
+    availability_zones: typing.List[str]
+    kubernetes_version: str
+    node_groups: typing.Dict[str, NodeGroup]
+
+class AzureProvider(Base):
+    project: str
+    region: str
+    kubernetes_version: str
+    node_groups: typing.Dict[str, NodeGroup]
+    storage_account_postfix: str
+
+class AmazonWebServicesProvider(Base):
+    region: str
+    availability_zones: typing.List[str]
+    kubernetes_version: str
+    node_groups: typing.Dict[str, NodeGroup]
+
+class LocalProvider(Base):
+    kube_context: typing.Optional[str]
+    node_selectors: typing.Dict[str, KeyValueDict]
 
 
-class KubeSpawner(pydantic.BaseModel):
+# ================= Theme ==================
+
+class Theme(Base):
+    jupyterhub: typing.Dict[str, str]
+
+# ================== Profiles ==================
+
+class KubeSpawner(Base):
     cpu_limit: int
     cpu_guarantee: int
     mem_limit: str
     mem_guarantee: str
     image: str
 
-    class Config:
-        @staticmethod
-        def schema_extra(schema, model):
-            schema.update(
-                examples=[
-                    KubeSpawner(
-                        cpu_limit=1,
-                        cpu_guarantee=1,
-                        mem_limit="1G",
-                        mem_guarantee="1G",
-                        image="quansight/qhub-jupyterlab:398e040a7d26bcc1d04fc3576f452bfa261032bc",
-                    ).dict(),
-                    KubeSpawner(
-                        cpu_limit=1.5,
-                        cpu_guarantee=1.25,
-                        mem_limit="2G",
-                        mem_guarantee="2G",
-                        image="quansight/qhub-jupyterlab:398e040a7d26bcc1d04fc3576f452bfa261032bc",
-                    ).dict(),
-                ]
-            )
-            return schema
-
-
-class LabProfile(Base):
-    "Stable environment with 1 cpu / 1 GB ram"
+class JupyterLabProfile(Base):
     display_name: str
+    description: str
     groups: typing.List[str]
     kubespawner_override: KubeSpawner
 
-
-class User(Base):
-    uid: str
-    primary_group: str
-    secondary_group: str = None
-
-
-class Group(Base):
-    gid: int
-
-
-class AuthConfig(Base):
-    oauth_callback_url: str = "https://jupyter.do.qhub.dev/hub/oauth_callback"
-
-
-class GithubAuth(Base):
-    """
-    >>> assert Authentication(type='Github', config = GithubAuth(client_id="", client_secret=""))
-    """
-
-    client_id: str
-    client_secret: str
-
-
-class Authentication(Base):
-    type: str
-    config: typing.Optional[AuthConfig]
-
-
-class Security(Base):
-    authentication: Authentication
-    users: typing.Dict[str, User]
-    group: typing.Dict[str, Group] = {}
-
-
-class DaskWorker(Base):
+class DaskWorkerProfile(Base):
     worker_cores_limit: int
     worker_cores: int
     worker_memory_limit: str
     worker_memory: str
     image: str
 
-    class Config:
-        @staticmethod
-        def schema_extra(schema, model):
-            schema.update(
-                examples=[
-                    DaskWorker(
-                        worker_cores_limit=1,
-                        worker_cores=1,
-                        worker_memory_limit="1G",
-                        worker_memory="1G",
-                        image="quansight/qhub-dask-worker:398e040a7d26bcc1d04fc3576f452bfa261032bc",
-                    ).dict(),
-                    DaskWorker(
-                        worker_cores_limit=1.5,
-                        worker_cores=1.25,
-                        worker_memory_limit="2G",
-                        worker_memory="2G",
-                        image="quansight/qhub-dask-worker:398e040a7d26bcc1d04fc3576f452bfa261032bc",
-                    ).dict(),
-                ]
-            )
-            return schema
-
-
-class JupyterLabProfile(Base):
-    display_name: str
-    description: str
-    groups: typing.List[str] = []
-    kubespawner_override: KubeSpawner
-
-
 class Profiles(Base):
-    jupyterlab: typing.List[JupyterLabProfile] = []
-    dask_worker: typing.Dict[str, DaskWorker] = {}
+    jupyterlab: typing.List[JupyterLabProfile]
+    dask_worker: typing.Dict[str, DaskWorkerProfile]
 
+# ================ Environment ================
+
+class CondaEnvironment(Base):
+    name: str
+    channels: typing.Optional[typing.List[str]]
+    dependencies: typing.List[typing.Union[str, typing.Dict[str, str]]]
+
+# ==================== Main ===================
 
 project_name_regex_type = pydantic.constr(regex="^[A-Za-z-_]+$")
 
@@ -184,12 +154,19 @@ class Main(Base):
     project_name: project_name_regex_type
     provider: ProviderEnum
     ci_cd: CiEnum
+    domain: str
+    terraform_state: TerraformStateEnum
     security: Security
-    profiles: Profiles = []
-
-
-class DigitalOcean(Main):
-    digital_ocean: Provider = None
+    default_images: typing.Dict[str, str]
+    storage: typing.Dict[str, str]
+    local: typing.Optional[LocalProvider]
+    google_cloud_platform: typing.Optional[GoogleCloudPlatformProvider]
+    amazon_web_services: typing.Optional[AmazonWebServicesProvider]
+    azure: typing.Optional[AzureProvider]
+    digital_ocean: typing.Optional[DigitalOceanProvider]
+    theme: Theme
+    profiles: Profiles
+    environments: typing.Dict[str, CondaEnvironment]
 
 
 def verify(config):
