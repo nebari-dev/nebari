35da4971896e2c0a01c90fbcc33a7e417572412b
Dan Lester <dan@ideonate.com>
2021-04-06 13:29:56 +0100

    load yaml within cypress

---
+++ tests_e2e/cypress/plugins/index.js
@@ -0,0 +1,27 @@
+const yaml_fields = [
+    'security.authentication.type'
+];
+
+module.exports = (on, config) => {
+        
+    const fs = require('fs');
+    const _ = require('lodash');
+    const yaml = require('js-yaml');
+
+    let new_config = {};
+  
+    let fileContents = fs.readFileSync(process.env.QHUB_CONFIG_PATH, 'utf8');
+    let data = yaml.load(fileContents);
+
+    console.log(data);
+
+    new_config['env'] = _.fromPairs(
+            _.map(yaml_fields, 
+                field => ['qhub_'+field.replace(/\./g, '_') , _.get(data, field, '')]
+                    )
+        );
+
+    return new_config;
+  };
+
+  
