3b316c46b8f410fa705b13f6c07ed9eb9d214cb3
Dan Lester <dan@ideonate.com>
2021-04-12 18:43:50 +0100

    JupyterLab in Cypress

--- tests_e2e/cypress/integration/main.js
+++ tests_e2e/cypress/integration/main.js
@@ -1,3 +1,5 @@
+const { divide } = require("lodash");
+
 const security_authentication_type = Cypress.env('qhub_security_authentication_type');
 
 
@@ -21,14 +23,24 @@ describe('First Test', () => {
 
     } else if (security_authentication_type == 'password') {
 
-      cy.loginWithPassword('example-user', Cypress.env('EXAMPLE_USER_PASSWORD'));
-      
-    /*   // If multiple profiles are available
-       cy.get('h1')
-        .should('contain', 'Server Options');  */
+      cy.loginWithPassword(Cypress.env('EXAMPLE_USER_NAME') || 'example-user', Cypress.env('EXAMPLE_USER_PASSWORD'));
       
       cy.get('#start')
-        .should('contain', 'My Server')
+        .should('contain', 'My Server').click();
+
+       cy.get('h1')
+        .should('contain', 'Server Options');
+
+      cy.get('input.btn.btn-jupyter')
+        .should('have.attr', 'value', 'Start').click();
+
+      cy.get('div.jp-LauncherCard[data-category="Notebook"] > div.jp-LauncherCard-label[title="Python 3"] > p', { timeout: 20000 })
+        .click(); // Launch new Python notebook
+
+      cy.get('div.jp-Notebook textarea').type('print("HELLO"+"QHUB")');
+
+      cy.get('div.jp-NotebookPanel-toolbar > div:nth-child(1) > button').click(); // Save
+      
 
     } else {
       throw new Error("No security_authentication_type env var is set");
