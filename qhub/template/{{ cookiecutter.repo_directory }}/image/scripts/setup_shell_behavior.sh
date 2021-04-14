2ceaba651bc5cdb10121c534148aef46682c82b8
leej3 <johnleenimh@gmail.com>
2021-04-14 08:10:03 +0100

    do not use /home/jovyan for config or installation
    Some tweaks to the docker layer caching are made in the process...
    overall files should be copied into the container as required so
    that cache invalidation occurs as late as possible in the Dockerfile.
    
    Instead of using the home directory for config, /etc files are
    used to store config content for all users.
    
    Permissions to files should be fixed in the docker layer that they
    are created in to minimize layer size. The fix-permissions script
    does this recursively with minimal modifications to the file tree.
    
    Software installation should go in /opt instead of home

--- qhub/template/{{ cookiecutter.repo_directory }}/image/scripts/setup_shell_behavior.sh
+++ qhub/template/{{ cookiecutter.repo_directory }}/image/scripts/setup_shell_behavior.sh
@@ -6,10 +6,17 @@ DEFAULT_ENV="$1"; shift
 cp /etc/skel/.bashrc /etc/profile.d/term_settings.sh
 sed -i 's/^#force_color_prompt=yes/force_color_prompt=yes/' /etc/profile.d/term_settings.sh
 
-# Always try to activate default conda environment.
-cat << EOF > /etc/profile.d/conda_init.sh
-. /opt/conda/etc/profile.d/conda.sh
-conda activate "${DEFAULT_ENV}"
+# Always try to activate default conda environment (in /etc/profile.d)
+cat << EOF > /etc/profile.d/PATH_modification.sh
+PATH="\$PATH:/opt/scripts"
+
+# initialize conda last
+if [ -f /opt/conda/etc/profile.d/conda.sh ];then
+  # conda has been successfully installed and the conda.sh has been linked
+  # appropriately...
+  . /opt/conda/etc/profile.d/conda.sh
+  conda activate "${DEFAULT_ENV}"
+fi
 EOF
 
 # Define ETC_BASHRC_WAS_SOURCED in bash.bashrc which is sourced for interactive
@@ -18,13 +25,14 @@ echo "ETC_BASHRC_WAS_SOURCED=yes" >> /etc/bash.bashrc
 echo "source /etc/profile" >> /etc/bash.bashrc
 
 # Create and /etc/profile that reciprocally sources bash.bashrc so that basic
-# env is always the same.
-# Quotes in the following line make the heredoc defined verbatim (variables are
-# not substituted.
+# env is always the same regardless of whether  a login shell is used.
+# Quotes in the following line make the heredoc defined below verbatim
+# (variables are not substituted.
 cat << "EOF" > /etc/profile
 # /etc/profile: system-wide .profile file for the Bourne shell (sh(1))
 # and Bourne compatible shells (bash(1), ksh(1), ash(1), ...).
 
+# Initialize PATH. Subsequent modifications should be made in /etc/profile.d
 if [ "`id -u`" -eq 0 ]; then
   PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 else
