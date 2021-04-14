658b47ff9c099bb01dd6ddf865c0a045cf4b71e2
leej3 <johnleenimh@gmail.com>
2021-04-14 07:52:35 +0100

    finish conda lock stuff

--- qhub/template/{{ cookiecutter.repo_directory }}/image/scripts/install-conda-environment.sh
+++ qhub/template/{{ cookiecutter.repo_directory }}/image/scripts/install-conda-environment.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 set -xe
-
-# Assumes conda packages to install in argument "$1"
+ENV_FILE="${1}"; shift
+# Capture last optional arg or set a ENV_NAME
+if [[ -z "${1+x}" ]] || [[ "${1}" == "" ]]; then
+    ENV_NAME=default
+else
+    ENV_NAME="${1}"; shift
+fi
 
 # ==== install conda dependencies ====
-/opt/conda/bin/conda env update -f $1
+conda create --prefix=/opt/conda/envs/${ENV_NAME} --file "${ENV_FILE}"
+PATH="/opt/conda/envs/${ENV_NAME}/bin:${PATH}"
+# For now install pip section manually. We could consider using pip-tools...
+# See https://github.com/conda-incubator/conda-lock/issues/4
+pip install https://github.com/dirkcgrunwald/jupyter_codeserver_proxy-/archive/5596bc9c2fbd566180545fa242c659663755a427.tar.gz
 
 # ========= list dependencies ========
 /opt/conda/bin/conda list
