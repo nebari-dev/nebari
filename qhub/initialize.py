ad7eb7830b057e08c7554555eb45c4d8964f201d
Dan Lester <dan@ideonate.com>
2021-04-19 18:32:31 +0100

    black formatting

--- qhub/initialize.py
+++ qhub/initialize.py
@@ -344,8 +344,8 @@ def render_config(
             "hub_subtitle"
         ] = "Autoscaling Compute Environment on Amazon Web Services"
         config["amazon_web_services"] = AMAZON_WEB_SERVICES
-        if 'AWS_DEFAULT_REGION' in os.environ:
-            config["amazon_web_services"]['region'] = os.environ['AWS_DEFAULT_REGION']
+        if "AWS_DEFAULT_REGION" in os.environ:
+            config["amazon_web_services"]["region"] = os.environ["AWS_DEFAULT_REGION"]
         if kubernetes_version:
             config["amazon_web_services"]["kubernetes_version"] = kubernetes_version
     elif cloud_provider == "local":
