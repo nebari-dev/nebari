dc8a4d569c2ae5e4871bbb4b92587a88831c9730
john lee <leej3@quansight.com>
2021-04-28 17:19:11 -0700

    update docker images (#522)
    * use conda lock file
    
    * add lock file
    
    * finish conda lock stuff
    
    * add permissions file
    
    * add setup default shell behavior.sh
    
    * do not use /home/jovyan for config or installation
    
    Some tweaks to the docker layer caching are made in the process...
    overall files should be copied into the container as required so
    that cache invalidation occurs as late as possible in the Dockerfile.
    
    Instead of using the home directory for config, /etc files are
    used to store config content for all users.
    
    Permissions to files should be fixed in the docker layer that they
    are created in to minimize layer size. The fix-permissions script
    does this recursively with minimal modifications to the file tree.
    
    Software installation should go in /opt instead of home
    
    * update other dockerfiles
    
    * update settings
    
    umask now added
    conda guard inplace in case conda env already activated
    minimal apt install file now used across all images.
    since username is arbitrary we use a qhub one
    warning comment about arbitrary uid and gid
    
    * add script for minimal apt install
    
    * update fix-permissions for qhub
    
    at deployment any sort of uid/gid combination can occur
    we want all users to be able access system files including
    sys installs, config files, and conda envs
    
    * do not fix permissions for full conda dir
    
    * oops
    
    * fix logic for checking for conda env in postBuild
    
    * prevent accidental write to home directory
    
    software/configuration written to home will not work upon deployment
    
    Solves https://github.com/Quansight/qhub/issues/497
    
    * tidy code-server install

---
+++ qhub/template/{{ cookiecutter.repo_directory }}/image/scripts/install-apt-minimal.sh
@@ -0,0 +1,5 @@
+apt-get update --fix-missing && \
+    apt-get install -y wget bzip2 ca-certificates curl git && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/* /var/tmp/* /tmp/*
+
--- qhub/template/{{ cookiecutter.repo_directory }}/image/scripts/install-conda.sh
+++ qhub/template/{{ cookiecutter.repo_directory }}/image/scripts/install-conda.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 set -xe
-
-# Requires environment CONDA_SHA256, CONDA_VERSION
-
-apt-get update
-apt-get install -y wget bzip2
-
+# Requires environment CONDA_SHA256, CONDA_VERSION, and DEFAULT_ENV
 wget --quiet https://repo.anaconda.com/miniconda/Miniconda3-$CONDA_VERSION-Linux-x86_64.sh
 
 echo "${CONDA_SHA256}  Miniconda3-$CONDA_VERSION-Linux-x86_64.sh" > miniconda.checksum
 
 if [ $(sha256sum -c miniconda.checksum | awk '{print $2}') != "OK" ]; then
+   echo Error when testing checksum
    exit 1;
 fi
 
@@ -19,8 +15,6 @@ sh ./miniconda.sh -b -p /opt/conda
 rm miniconda.sh miniconda.checksum
 
 ln -s /opt/conda/etc/profile.d/conda.sh /etc/profile.d/conda.sh
-echo ". /opt/conda/etc/profile.d/conda.sh" >> ~/.bashrc && \
-echo "conda activate base" >> ~/.bashrc
 
 mkdir -p /etc/conda
 cat <<EOF > /etc/conda/condarc
@@ -35,6 +29,6 @@ pkgs_dirs:
  - /home/jovyan/.conda/pkgs
 EOF
 
-apt-get autoremove --purge -y wget bzip2
-apt-get clean
-rm -rf /var/lib/apt/lists/*
+# Fix permissions in accordance with jupyter stack permissions
+# model
+fix-permissions /opt/conda /etc/conda /etc/profile.d
