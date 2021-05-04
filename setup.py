69951056f11bfcc94e8eb1fe4cde3cb2f80904ab
Tyler <49161327+tylerpotts@users.noreply.github.com>
2021-05-04 13:56:27 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, <4",
     install_requires=[
         "cookiecutter",
-        "ruamel.yaml",
+        "ruamel",
         "cloudflare",
         "auth0-python",
         "pydantic",
