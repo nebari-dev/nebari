744060aeaf19c194d39fd418c9834790c21f9f00
john lee <leej3@quansight.com>
2021-05-27 12:04:54 +0100

    make message for schema validation failure more informative (#596)
    * make message for schema validation failure more informative
    
    * add imports
    
    * validate take -c flag, schema errors have better msg everywhere
    
    Co-authored-by: Dan Lester <dan@ideonate.com>

--- qhub/cli/validate.py
+++ qhub/cli/validate.py
@@ -1,14 +1,20 @@
 import pathlib
 
 from ruamel import yaml
-
 from qhub.schema import verify
 from qhub.provider.cicd.linter import comment_on_pr
 
 
 def create_validate_subcommand(subparser):
     subparser = subparser.add_parser("validate")
-    subparser.add_argument("config", help="qhub configuration")
+    subparser.add_argument(
+        "configdeprecated",
+        help="qhub configuration yaml file (deprecated - please pass in as -c/--config flag)",
+        nargs="?",
+    )
+    subparser.add_argument(
+        "-c", "--config", help="qhub configuration yaml file", required=False
+    )
     subparser.add_argument(
         "--enable-commenting", help="Turn on PR commenting", action="store_true"
     )
@@ -16,7 +22,18 @@ def create_validate_subcommand(subparser):
 
 
 def handle_validate(args):
-    config_filename = pathlib.Path(args.config)
+    if args.configdeprecated and args.config:
+        raise ValueError(
+            "Please pass in -c/--config flag specifying your qhub-config.yaml file, and do NOT pass it as a standalone argument"
+        )
+
+    config_filename = args.config or args.configdeprecated
+    if not config_filename:
+        raise ValueError(
+            "Please pass in a qhub-config.yaml filename using the -c/--config argument"
+        )
+
+    config_filename = pathlib.Path(args.config or args.configdeprecated)
     if not config_filename.is_file():
         raise ValueError(
             f"passed in configuration filename={config_filename} must exist"
