616c7740eeea4b510fc05b71e9ba77140ad91c74
Dan Lester <dan@ideonate.com>
2021-06-10 11:03:26 +0100

    stop jupyter server

--- tests_e2e/cypress/integration/main.js
+++ tests_e2e/cypress/integration/main.js
@@ -24,6 +24,8 @@ describe('First Test', () => {
     } else if (security_authentication_type == 'password') {
 
       cy.loginWithPassword(Cypress.env('EXAMPLE_USER_NAME') || 'example-user', Cypress.env('EXAMPLE_USER_PASSWORD'));
+
+      // Start my Jupyter server
       
       cy.get('#start')
         .should('contain', 'My Server').click();
@@ -34,8 +36,21 @@ describe('First Test', () => {
       cy.get('input.btn.btn-jupyter')
         .should('have.attr', 'value', 'Start').click();
 
-      cy.get('div#jp-MainLogo', { timeout: 20000 }).should('exist');
-        
+      // Minimal check that JupyterLab has opened
+
+      cy.get('div#jp-MainLogo', { timeout: 30000 }).should('exist');
+
+      // Stop my Jupyter server - must do this so PVC can be destroyed on Minikube
+
+      cy.visit('/hub/home');
+
+        // wait because otherwise event handler is not yet registered
+        // 'Correct' solution is here: https://www.cypress.io/blog/2019/01/22/when-can-the-test-click/
+      cy.get('#stop')
+        .should('contain', 'Stop My Server').wait(500).click();
+
+      cy.get('#start', { timeout: 30000 })
+        .should('contain', 'Start My Server');
 
     } else {
       throw new Error("No security_authentication_type env var is set");
