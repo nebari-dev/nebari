51016c584362f978f3dbaa5cc65c64e95ea0ab62
Amit Kumar <dtu.amit@gmail.com>
2021-06-15 17:57:16 +0100

    Undo changes to dask gateway middleware

--- qhub/template/{{ cookiecutter.repo_directory }}/infrastructure/modules/kubernetes/services/dask-gateway/middleware.tf
+++ qhub/template/{{ cookiecutter.repo_directory }}/infrastructure/modules/kubernetes/services/dask-gateway/middleware.tf
@@ -32,29 +32,6 @@ resource "kubernetes_manifest" "chain-middleware" {
       namespace = var.namespace
     }
     spec = {
-      stripPrefixRegex = {
-        regex = [
-          "/gateway/clusters/[a-zA-Z0-9.-]+"
-        ]
-      }
-    }
-  }
-}
-
-# Create one chain middleware for the IngressRoutes that will be dynamically created by Dask Gateway
-# The chain combines traefik-forward-auth and stripprefix middleware defined below.
-
-resource "kubernetes_manifest" "cluster-middleware" {
-  provider = kubernetes-alpha
-
-  manifest = {
-    apiVersion = "traefik.containo.us/v1alpha1"
-    kind       = "Middleware"
-    metadata = {
-      name      = "qhub-dask-gateway-cluster"
-      namespace = var.namespace
-    }
-    spec = {
       chain = {
         middlewares = [
           {
@@ -65,6 +42,26 @@ resource "kubernetes_manifest" "cluster-middleware" {
             name      = kubernetes_manifest.cluster-middleware-stripprefix.manifest.metadata.name
             namespace = var.namespace
           }
+        ]
+      }
+    }
+  }
+}
+
+resource "kubernetes_manifest" "cluster-middleware-stripprefix" {
+  provider = kubernetes-alpha
+
+  manifest = {
+    apiVersion = "traefik.containo.us/v1alpha1"
+    kind       = "Middleware"
+    metadata = {
+      name      = "qhub-dask-gateway-cluster-stripprefix"
+      namespace = var.namespace
+    }
+    spec = {
+      stripPrefixRegex = {
+        regex = [
+          "/gateway/clusters/[a-zA-Z0-9.-]+"
         ]
       }
     }
