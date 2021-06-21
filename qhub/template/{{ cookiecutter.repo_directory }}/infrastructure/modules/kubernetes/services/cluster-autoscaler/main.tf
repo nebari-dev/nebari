a1542e336cddabc78bb33892ed8df0673feb8ff9
Amit Kumar <dtu.amit@gmail.com>
2021-06-21 14:34:31 +0100

    Fix helm upgrade: remove helm_repository data source (#672)

--- qhub/template/{{ cookiecutter.repo_directory }}/infrastructure/modules/kubernetes/services/cluster-autoscaler/main.tf
+++ qhub/template/{{ cookiecutter.repo_directory }}/infrastructure/modules/kubernetes/services/cluster-autoscaler/main.tf
@@ -1,13 +1,8 @@
-data "helm_repository" "autoscaler" {
-  name = "stable"
-  url  = "https://charts.helm.sh/stable"
-}
-
 resource "helm_release" "autoscaler" {
   name      = "cluster-autoscaler"
   namespace = var.namespace
 
-  repository = data.helm_repository.autoscaler.metadata[0].name
+  repository = "https://charts.helm.sh/stable"
   chart      = "stable/cluster-autoscaler"
   version    = "7.1.0"
 
