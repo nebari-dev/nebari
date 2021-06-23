2770c9793532a190e17637ebc5e289bd0fad51bb
Dan Lester <dan@ideonate.com>
2021-06-23 15:32:02 +0100

    More AWS Tags (#685)
    * More AWS Tags
    
    * tf fmt fix

--- qhub/template/{{ cookiecutter.repo_directory }}/infrastructure/modules/aws/accounting/variables.tf
+++ qhub/template/{{ cookiecutter.repo_directory }}/infrastructure/modules/aws/accounting/variables.tf
@@ -7,3 +7,9 @@ variable "environment" {
   description = "Environment for resource group filter"
   type        = string
 }
+
+variable "tags" {
+  description = "Additional tags to apply to all network resource"
+  type        = map(string)
+  default     = {}
+}
