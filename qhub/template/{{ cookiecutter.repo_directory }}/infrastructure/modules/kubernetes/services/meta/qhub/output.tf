08c94b91428f68d47270ba003a7461c45ef64f48
Amit Kumar <dtu.amit@gmail.com>
2021-07-01 21:30:35 +0100

    Move clearml ingress to clearml folder (#713)

--- qhub/template/{{ cookiecutter.repo_directory }}/infrastructure/modules/kubernetes/services/meta/qhub/output.tf
+++ qhub/template/{{ cookiecutter.repo_directory }}/infrastructure/modules/kubernetes/services/meta/qhub/output.tf
@@ -2,3 +2,8 @@ output "jupyterhub_api_token" {
   description = "API token to enable in jupyterhub server"
   value       = random_password.jupyterhub_api_token.result
 }
+
+output "tls" {
+  description = "TLS configuration"
+  value       = local.tls
+}
