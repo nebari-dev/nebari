3dba78631f3190daceec001080627aa39242f8b7
Abraham <utabe11@hotmail.com>
2021-07-19 09:17:22 -0400

    WIP test out different commands

--- qhub/cli/__init__.py
+++ qhub/cli/__init__.py
@@ -8,7 +8,7 @@ from qhub.cli.initialize import create_init_subcommand
 from qhub.cli.render import create_render_subcommand
 from qhub.cli.validate import create_validate_subcommand
 from qhub.cli.destroy import create_destroy_subcommand
-from qhub.cli.destroy import create_support_subcommand
+from qhub.cli.support import create_support_subcommand
 from qhub.provider.terraform import TerraformException
 from qhub.version import __version__
 from qhub.utils import QHUB_GH_BRANCH
--- qhub/cli/support.py
+++ qhub/cli/support.py
@@ -1,5 +1,5 @@
-
-
+from kubernetes import client, config
+from zipfile import ZipFile
 def create_support_subcommand(subparser):
     subparser = subparser.add_parser("support")
 
@@ -8,4 +8,23 @@ def create_support_subcommand(subparser):
 
 
 def handle_support(args):
-    pass
+    config.load_kube_config()
+
+    v1 = client.CoreV1Api()
+    # print(dir(v1))
+    print("Listing pods with their IPs:")
+    ret = v1.list_pod_for_all_namespaces(watch=False)
+    # v1.CustomResourceDefinition
+    print(v1.get_api_resources())
+    print(v1.get_api_resources_with_http_info())
+    # log = v1.read_namespaced_pod_log(watch=False)
+    # for i in log.items:
+    #     print(i)
+    # print(dir(ret.items[0]))
+    # for i in ret.items:
+    #     print("NEWLINE===================================")
+    #     try:
+    #         print(v1.read_namespaced_pod_log(name=i.metadata.name, namespace=i.metadata.namespace))
+    #         print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name, ))
+    #     except client.exceptions.ApiException as e:
+    #         print("%s not available"% i.metadata.name)
