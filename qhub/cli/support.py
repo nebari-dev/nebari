9315f56dc550318e60d7c79146a177e7d23d33a4
Abraham <utabe11@hotmail.com>
2021-07-20 17:03:22 -0400

    add zip support to logging

--- qhub/cli/support.py
+++ qhub/cli/support.py
@@ -1,8 +1,12 @@
 from kubernetes import client, config
+from pathlib import Path
 from zipfile import ZipFile
+
+
 def create_support_subcommand(subparser):
     subparser = subparser.add_parser("support")
 
+    subparser.add_argument("-f", "--filename", default="./qhub-support.zip", help="output filename")
 
     subparser.set_defaults(func=handle_support)
 
@@ -11,20 +15,20 @@ def handle_support(args):
     config.load_kube_config()
 
     v1 = client.CoreV1Api()
-    # print(dir(v1))
-    print("Listing pods with their IPs:")
-    ret = v1.list_pod_for_all_namespaces(watch=False)
-    # v1.CustomResourceDefinition
-    print(v1.get_api_resources())
-    print(v1.get_api_resources_with_http_info())
-    # log = v1.read_namespaced_pod_log(watch=False)
-    # for i in log.items:
-    #     print(i)
-    # print(dir(ret.items[0]))
-    # for i in ret.items:
-    #     print("NEWLINE===================================")
-    #     try:
-    #         print(v1.read_namespaced_pod_log(name=i.metadata.name, namespace=i.metadata.namespace))
-    #         print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name, ))
-    #     except client.exceptions.ApiException as e:
-    #         print("%s not available"% i.metadata.name)
+
+    # ret = v1.list_pod_for_all_namespaces(watch=False)
+    ret = v1.list_namespaced_pod(namespace='dev')
+
+    for i in ret.items:
+        parentDir = Path(f'./log/{i.metadata.namespace}').mkdir(parents=True, exist_ok=True)
+        path = Path(f'./log/{i.metadata.namespace}/{i.metadata.name}.txt')
+        with path.open(mode='wt') as file:
+            try:
+                file.write(v1.read_namespaced_pod_log(name=i.metadata.name, namespace=i.metadata.namespace))
+                file.write("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name,))
+            except client.exceptions.ApiException as e:
+                file.write("%s not available"% i.metadata.name)
+
+    with ZipFile('./qhub-support.zip', 'w') as zip:
+        for file in list(Path('./log').glob('*.txt')):
+            zip.write(file)
