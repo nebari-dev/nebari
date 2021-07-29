aa5a921a5e76eec463b9fc91848340dbd3067186
Abraham <utabe11@hotmail.com>
2021-07-29 17:03:31 -0400

    need sub directory for file writing

--- qhub/cli/support.py
+++ qhub/cli/support.py
@@ -6,7 +6,9 @@ from zipfile import ZipFile
 def create_support_subcommand(subparser):
     subparser = subparser.add_parser("support")
 
-    subparser.add_argument("-f", "--filename", default="./qhub-support.zip", help="output filename")
+    # subparser.add_argument("-c", "--config", help="qhub configuration yaml file", required=True)
+
+    subparser.add_argument("-o", "--output", default="./qhub-support.zip", help="output filename")
 
     subparser.set_defaults(func=handle_support)
 
@@ -17,18 +19,18 @@ def handle_support(args):
     v1 = client.CoreV1Api()
 
     # ret = v1.list_pod_for_all_namespaces(watch=False)
-    ret = v1.list_namespaced_pod(namespace='dev')
+    pods = v1.list_namespaced_pod(namespace='dev')
 
-    for i in ret.items:
-        parentDir = Path(f'./log/{i.metadata.namespace}').mkdir(parents=True, exist_ok=True)
-        path = Path(f'./log/{i.metadata.namespace}/{i.metadata.name}.txt')
+    for pod in pods.items:
+        parentDir = Path(f'./log/{pod.metadata.namespace}').mkdir(parents=True, exist_ok=True)
+        path = Path(f'./log/{pod.metadata.namespace}/{pod.metadata.name}.txt')
         with path.open(mode='wt') as file:
             try:
-                file.write(v1.read_namespaced_pod_log(name=i.metadata.name, namespace=i.metadata.namespace))
-                file.write("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name,))
+                file.write(v1.read_namespaced_pod_log(name=pod.metadata.name, namespace=pod.metadata.namespace))
+                file.write("%s\t%s\t%s" % (pod.status.pod_ip, pod.metadata.namespace, pod.metadata.name,))
             except client.exceptions.ApiException as e:
-                file.write("%s not available"% i.metadata.name)
+                file.write("%s not available"% pod.metadata.name)
 
     with ZipFile('./qhub-support.zip', 'w') as zip:
-        for file in list(Path('./log').glob('*.txt')):
+        for file in list(Path('./log/dev').glob('*.txt')):
             zip.write(file)
