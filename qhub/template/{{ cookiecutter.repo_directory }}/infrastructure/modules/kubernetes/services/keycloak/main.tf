a4fcafe5b406dfb2fc3655e7e204a6d5c2959a76
Dan Lester <dan@ideonate.com>
2021-08-12 14:20:54 +0100

    install keycloak operator

--- qhub/template/{{ cookiecutter.repo_directory }}/infrastructure/modules/kubernetes/services/keycloak/main.tf
+++ qhub/template/{{ cookiecutter.repo_directory }}/infrastructure/modules/kubernetes/services/keycloak/main.tf
@@ -16,7 +16,6 @@ resource "helm_release" "keycloak" {
   }
 }
 
-
 resource "kubernetes_manifest" "keycloak-http" {
   provider = kubernetes-alpha
 
@@ -37,12 +36,76 @@ resource "kubernetes_manifest" "keycloak-http" {
             {
               name      = "keycloak-headless"
               port      = 80
-              #namespace = var.namespace
             }
           ]
         }
       ]
       tls = var.tls
+    }
+  }
+}
+
+# Converted from https://github.com/keycloak/keycloak-operator/blob/master/deploy/operator.yaml
+# using https://github.com/jrhouston/tfk8s
+resource "kubernetes_manifest" "deployment_keycloak_operator" {
+  provider = kubernetes-alpha
+
+  manifest = {
+    "apiVersion" = "apps/v1"
+    "kind" = "Deployment"
+    "metadata" = {
+      "name" = "keycloak-operator"
+      "namespace" = "keycloak"
+    }
+    "spec" = {
+      "replicas" = 1
+      "selector" = {
+        "matchLabels" = {
+          "name" = "keycloak-operator"
+        }
+      }
+      "template" = {
+        "metadata" = {
+          "labels" = {
+            "name" = "keycloak-operator"
+          }
+        }
+        "spec" = {
+          "containers" = [
+            {
+              "command" = [
+                "keycloak-operator",
+              ]
+              "env" = [
+                {
+                  "name" = "WATCH_NAMESPACE"
+                  "valueFrom" = {
+                    "fieldRef" = {
+                      "fieldPath" = "metadata.namespace"
+                    }
+                  }
+                },
+                {
+                  "name" = "POD_NAME"
+                  "valueFrom" = {
+                    "fieldRef" = {
+                      "fieldPath" = "metadata.name"
+                    }
+                  }
+                },
+                {
+                  "name" = "OPERATOR_NAME"
+                  "value" = "keycloak-operator"
+                },
+              ]
+              "image" = "quay.io/keycloak/keycloak-operator:14.0.0"
+              "imagePullPolicy" = "Always"
+              "name" = "keycloak-operator"
+            },
+          ]
+          "serviceAccountName" = "keycloak-operator"
+        }
+      }
     }
   }
 }
