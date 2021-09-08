d670ae716ec25df11442c30f067570b745cfaf7f
Dan Lester <dan@ideonate.com>
2021-09-08 13:31:15 +0100

    fix conda-store build; allow list in jh template vars

--- qhub/schema.py
+++ qhub/schema.py
@@ -307,7 +307,7 @@ class LocalProvider(Base):
 
 
 class Theme(Base):
-    jupyterhub: typing.Dict[str, str]
+    jupyterhub: typing.Dict[str, typing.Union[str, list]]
 
 
 # ================== Profiles ==================
