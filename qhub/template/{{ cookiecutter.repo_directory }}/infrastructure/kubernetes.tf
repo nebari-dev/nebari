17bba4baa43d0102b934fc2e31e4b3b2fe136132
Amit Kumar <dtu.amit@gmail.com>
2021-09-21 19:13:15 +0100

    Make forwardauth optional for ClearML

--- qhub/template/{{ cookiecutter.repo_directory }}/infrastructure/kubernetes.tf
+++ qhub/template/{{ cookiecutter.repo_directory }}/infrastructure/kubernetes.tf
@@ -257,6 +257,9 @@ module "clearml" {
   namespace    = var.environment
   external-url = var.endpoint
   tls          = module.qhub.tls
+{% if cookiecutter.clearml.enable_forward_auth is defined -%}
+  enable-forward-auth = {{ cookiecutter.clearml.enable_forward_auth | default(false,true) | jsonify }}
+{% endif -%}
   depends_on = [
     module.qhub
   ]
