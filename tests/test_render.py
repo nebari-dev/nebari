087c46d489129a97c198c4917a9dc93f2c6f0ff1
tarundmsharma <tarun.sharma@elastech.com>
2021-10-07 00:28:53 +0530

    changes in project name for test files

--- tests/test_render.py
+++ tests/test_render.py
@@ -9,10 +9,10 @@ from qhub.initialize import render_config
 @pytest.mark.parametrize(
     "project, namespace, domain, cloud_provider, ci_provider, auth_provider",
     [
-        ("do-pytest", "dev", "do.qhub.dev", "do", "github-actions", "github"),
-        ("aws-pytest", "dev", "aws.qhub.dev", "aws", "github-actions", "github"),
-        ("gcp-pytest", "dev", "gcp.qhub.dev", "gcp", "github-actions", "github"),
-        ("azure-pytest", "dev", "azure.qhub.dev", "azure", "github-actions", "github"),
+        ("pytest-do", "dev", "do.qhub.dev", "do", "github-actions", "github"),
+        ("pytest-aws", "dev", "aws.qhub.dev", "aws", "github-actions", "github"),
+        ("pytest-gcp", "dev", "gcp.qhub.dev", "gcp", "github-actions", "github"),
+        ("pytest-azure", "dev", "azure.qhub.dev", "azure", "github-actions", "github"),
     ],
 )
 def test_render(
