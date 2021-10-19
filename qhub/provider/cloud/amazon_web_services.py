cbd4d1feb0738204cef15da590328a1b071d493c
iameskild <eskild@doublee.io>
2021-10-19 15:20:02 -0700

    Add k8s version checker for AWS

--- qhub/provider/cloud/amazon_web_services.py
+++ qhub/provider/cloud/amazon_web_services.py
@@ -1,6 +1,7 @@
 import json
 import subprocess
 import functools
+import boto3
 
 
 @functools.lru_cache()
@@ -20,8 +21,26 @@ def zones(region):
 
 
 @functools.lru_cache()
-def kubernetes_versions():
-    return {_: _ for _ in ["1.13", "1.14", "1.15"]}
+def kubernetes_versions(grab_latest_version=True):
+    # AWS SDK (boto3) currently doesn't offer an intuitive way to list available kubernetes version. This implementation grabs kubernetes versions for specific EKS addons. It will therefore always be (at the very least) a subset of all kubernetes versions still supported by AWS.
+    client = boto3.client("eks")
+    supported_kubernetes_versions = list()
+    available_addons = client.describe_addon_versions()
+    for addon in available_addons.get("addons", None):
+        for eksbuild in addon.get("addonVersions", None):
+            for k8sversion in eksbuild.get("compatibilities", None):
+                supported_kubernetes_versions.append(
+                    k8sversion.get("clusterVersion", None)
+                )
+
+    # remove duplicate values
+    # then sort from oldest to newest available versions
+    supported_kubernetes_versions = sorted(list(set(supported_kubernetes_versions)))
+
+    if grab_latest_version:
+        return supported_kubernetes_versions[-1]
+
+    return supported_kubernetes_versions
 
 
 @functools.lru_cache()
