bb091c3d59623070ce416df707de9aa6393398c4
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2021-10-21 00:05:36 -0400

    Using rich for terminal output

--- qhub/develop.py
+++ qhub/develop.py
@@ -1,6 +1,8 @@
 import os
 import logging
 
+from rich.console import Console
+
 from qhub.provider import docker, git
 from qhub import utils
 
@@ -17,20 +19,24 @@ QHUB_IMAGES = [
 ]
 
 
-def build_docker_images(image_tag):
+def build_docker_images(console, image_tag):
     for image in QHUB_IMAGES:
         dockerfile_name = image
         build_directory = os.path.dirname(dockerfile_name)
         image_name = os.path.splitext(dockerfile_name)[1][1:]
         logger.info(f'qhub develop build dockerfile={dockerfile_name}')
-        docker.build(dockerfile_name, build_directory, image_name, image_tag)
+
+        with console.status(f'building dockerfile {dockerfile_name}'):
+            docker.build(dockerfile_name, build_directory, image_name, image_tag)
 
 
 def develop():
+    console = Console()
+
     git_repo_root = git.is_git_repo()
     if git_repo_root is None:
         raise utils.QHubError('QHub develop required to run within QHub git repository')
 
     git_head_sha = git.current_sha()
 
-    build_docker_images(image_tag=git_head_sha)
+    build_docker_images(console, image_tag=git_head_sha)
