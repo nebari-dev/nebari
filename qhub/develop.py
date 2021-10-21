304a58d12b78e600fa581a4e7f7b2d54080febe3
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2021-10-21 00:29:21 -0400

    More concise text

--- qhub/develop.py
+++ qhub/develop.py
@@ -21,16 +21,14 @@ QHUB_IMAGES = [
 
 def build_docker_images(console, image_tag):
     for image in QHUB_IMAGES:
-        dockerfile_name = image
-        build_directory = os.path.dirname(dockerfile_name)
-        image_name = os.path.splitext(dockerfile_name)[1][1:]
-        logger.info(f'qhub develop build dockerfile={dockerfile_name}')
+        dockerfile_path = image
+        dockerfile_name = os.path.basename(dockerfile_path)
+        build_directory = os.path.dirname(dockerfile_path)
+        image_name = os.path.splitext(dockerfile_path)[1][1:]
 
-        with console.status(f'building dockerfile {dockerfile_name} image {image_name}:{image_tag}'):
-            import time
-            time.sleep(1)
+        with console.status(f'Building {dockerfile_name} image "{image_name}:{image_tag}"'):
             docker.build(dockerfile_name, build_directory, image_name, image_tag)
-        console.print(f"successfully built dockerfile {dockerfile_name} image {image_name}:{image_tag}")
+        console.print(f'Built {dockerfile_name} image "{image_name}:{image_tag}"')
 
 
 def develop():
