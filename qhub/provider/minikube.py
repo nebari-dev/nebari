2b03ee12c6b91324f3b37c6bf79d2ca4d5ea5d64
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2021-10-25 23:14:51 -0400

    Modify image build

--- qhub/provider/minikube.py
+++ qhub/provider/minikube.py
@@ -94,7 +94,7 @@ def image_load(image, overwrite=True, profile="qhub"):
 
 
 def image_build(dockerfile_path, build_directory, name, tag, verbose=False, profile="qhub"):
-    command = ['image', 'build', build_directory, f"--profile={profile}", f'--file="{os.path.basename(dockerfile_path)}"', f'--tag={name}:{tag}']
+    command = ['image', 'build', f"--profile={profile}", f'--file="{os.path.basename(dockerfile_path)}"', f'--tag={name}:{tag}', f'"{build_directory}"']
     run_minikube_subprocess(command, prefix='minikube')
 
 
