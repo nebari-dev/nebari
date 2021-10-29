aad4695c7491bd279930609b4da864d0ac314459
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2021-10-28 21:20:12 -0400

    More verbage around ip not matching

--- qhub/deploy.py
+++ qhub/deploy.py
@@ -147,7 +147,7 @@ def check_qhub_address(ip_or_hostname: str, domain: str):
             , style="orange1")
     elif resolved_load_balancer_ip != resolved_domain_ip:
         console.print(
-            f'Domain "{domain}" is set but does not resolve to "{ip_or_hostname}"\n'
+            f'DNS Domain "{domain}" is set but does not resolve to load balancer at "{ip_or_hostname}"\n'
             f'Currently resolving "{domain}" -> "{resolved_domain_ip}" and "{ip_or_hostname}" -> "{resolved_load_balancer_ip}"\n'
             'If this is a production deployment this most likely an error'
             , style="orange1")
