8b28886b6d69d4098f8dc311e659b2217b3c55b9
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2021-10-28 21:05:32 -0400

    Removing newline

--- qhub/deploy.py
+++ qhub/deploy.py
@@ -149,7 +149,7 @@ def check_qhub_address(ip_or_hostname: str, domain: str):
         console.print(
             f'Domain "{domain}" is set but does not resolve to "{ip_or_hostname}"\n'
             f'Currently resolving "{domain}" -> "{resolved_domain_ip}" and "{ip_or_hostname}" -> "{resolved_load_balancer_ip}"\n'
-            'If this is a production deployment this most likely an error\n'
+            'If this is a production deployment this most likely an error'
             , style="orange1")
     else:
         console.print(f'Domain "{domain}" properly resolves to "{ip_or_hostname}"')
