e98585a8bcd667075c4469b8ff1601b27945b254
iameskild <eskild@doublee.io>
2021-11-18 11:57:26 -0800

    Add support for multi-container pods

--- qhub/cli/support.py
+++ qhub/cli/support.py
@@ -3,6 +3,8 @@ from ruamel import yaml
 from pathlib import Path
 from zipfile import ZipFile
 
+QHUB_SUPPORT_LOG_FILE = "./qhub-support-logs.zip"
+
 
 def create_support_subcommand(subparser):
     subparser = subparser.add_parser("support")
@@ -12,7 +14,7 @@ def create_support_subcommand(subparser):
     )
 
     subparser.add_argument(
-        "-o", "--output", default="./qhub-support.zip", help="output filename"
+        "-o", "--output", default=QHUB_SUPPORT_LOG_FILE, help="output filename"
     )
 
     subparser.set_defaults(func=handle_support)
@@ -25,32 +27,45 @@ def handle_support(args):
 
     namespace = get_config_namespace(config=args.config)
 
-    # ret = v1.list_pod_for_all_namespaces(watch=False)
     pods = v1.list_namespaced_pod(namespace=namespace)
 
     for pod in pods.items:
-        Path(f"./log/{pod.metadata.namespace}").mkdir(parents=True, exist_ok=True)
-        path = Path(f"./log/{pod.metadata.namespace}/{pod.metadata.name}.txt")
+        Path(f"./log/{namespace}").mkdir(parents=True, exist_ok=True)
+        path = Path(f"./log/{namespace}/{pod.metadata.name}.txt")
         with path.open(mode="wt") as file:
             try:
                 file.write(
-                    v1.read_namespaced_pod_log(
-                        name=pod.metadata.name, namespace=pod.metadata.namespace
-                    )
-                )
-                file.write(
-                    "%s\t%s\t%s"
+                    "%s\t%s\t%s\n"
                     % (
                         pod.status.pod_ip,
-                        pod.metadata.namespace,
+                        namespace,
                         pod.metadata.name,
                     )
                 )
+
+                # some pods are running multiple containers
+                containers = [
+                    _.name if len(pod.spec.containers) > 1 else None
+                    for _ in pod.spec.containers
+                ]
+
+                for container in containers:
+                    if container is not None:
+                        file.write(f"Container: {container}\n")
+                    file.write(
+                        v1.read_namespaced_pod_log(
+                            name=pod.metadata.name,
+                            namespace=namespace,
+                            container=container,
+                        )
+                    )
+
             except client.exceptions.ApiException as e:
                 file.write("%s not available" % pod.metadata.name)
                 raise e
-    with ZipFile("./qhub-support.zip", "w") as zip:
-        for file in list(Path("./log/dev").glob("*.txt")):
+
+    with ZipFile(QHUB_SUPPORT_LOG_FILE, "w") as zip:
+        for file in list(Path(f"./log/{namespace}").glob("*.txt")):
             print(file)
             zip.write(file)
 
