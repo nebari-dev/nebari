c97471b62ad5aecf95fdf69e50cb8aed7801a0d4
Dan Lester <dan@ideonate.com>
2021-12-02 15:14:16 +0000

    cancel timeout thread if process finished

--- qhub/utils.py
+++ qhub/utils.py
@@ -75,12 +75,17 @@ def run_subprocess_cmd(processargs, **kwargs):
         preexec_fn=os.setsid,
     )
     # Set timeout thread
+    timeout_timer = None
     if timeout > 0:
 
         def kill_process():
-            os.killpg(process.pid, signal.SIGTERM)
+            try:
+                os.killpg(process.pid, signal.SIGTERM)
+            except ProcessLookupError:
+                pass  # Already finished
 
-        threading.Timer(timeout, kill_process).start()
+        timeout_timer = threading.Timer(timeout, kill_process)
+        timeout_timer.start()
 
     for line in iter(lambda: process.stdout.readline(), b""):
         full_line = line_prefix + line
@@ -93,6 +98,9 @@ def run_subprocess_cmd(processargs, **kwargs):
 
         sys.stdout.buffer.write(full_line)
         sys.stdout.flush()
+
+    if timeout_timer is not None:
+        timeout_timer.cancel()
 
     return process.wait(
         timeout=10
