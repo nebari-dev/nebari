7c71420b3802d1caefadcb31dc7adaa534c2c927
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2021-12-09 14:45:09 -0500

    Adding support for nix flakes

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "QHub";
+
+  inputs = {
+    nixpkgs = { url = "github:nixos/nixpkgs/nixpkgs-unstable"; };
+  };
+
+  outputs = inputs@{ self, nixpkgs, ... }: {
+    devShell.x86_64-linux =
+      let
+        pkgs = import nixpkgs { system = "x86_64-linux"; };
+        pythonPackages = pkgs.python3Packages;
+      in pkgs.mkShell {
+        buildInputs = [
+          pythonPackages.cookiecutter
+          pythonPackages.ruamel-yaml
+          pythonPackages.cloudflare
+          pythonPackages.auth0-python
+          pythonPackages.pydantic
+          pythonPackages.pynacl
+          pythonPackages.bcrypt
+          pythonPackages.kubernetes
+          pythonPackages.packaging
+          # development
+          pythonPackages.pytest
+          pythonPackages.black
+          pythonPackages.flake8
+        ];
+      };
+  };
+}
