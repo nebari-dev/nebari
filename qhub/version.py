7b2cf866d92b9cb8a410c8b1c25179a9d83f213a
tonyfast <tony.fast@gmail.com>
2022-01-14 10:19:48 -0800

    fallback for 3.7 importlib

--- qhub/version.py
+++ qhub/version.py
@@ -1,4 +1,10 @@
 """a backport for the qhub version references"""
-from importlib.metadata import distribution
+from ast import Mod
+
+
+try:
+    from importlib.metadata import distribution
+except ModuleNotFoundError:
+    from importlib_metadata import distribution
 
 __version__ = distribution("qhub").version
