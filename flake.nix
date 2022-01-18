a44e759f136322010f74562a4ba5f1d5e55821ef
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2022-01-18 18:56:42 -0500

    Adding run command for qhub in nix flake

--- flake.nix
+++ flake.nix
@@ -6,41 +6,64 @@
     nixpkgs-keycloak = { url = "github:costrouc/nixpkgs/python-keycloak"; };
   };
 
-  outputs = inputs@{ self, nixpkgs, nixpkgs-keycloak, ... }: {
-    devShell.x86_64-linux =
-      let
-        pkgs = import nixpkgs { system = "x86_64-linux"; };
-        pythonPackages = pkgs.python3Packages;
+  outputs = inputs@{ self, nixpkgs, nixpkgs-keycloak, ... }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+      pythonPackages = pkgs.python3Packages;
 
-        keycloak = (import nixpkgs-keycloak { system = "x86_64-linux"; }).python3Packages.python-keycloak;
-      in pkgs.mkShell {
-        buildInputs = [
-          pythonPackages.cookiecutter
-          pythonPackages.ruamel-yaml
-          pythonPackages.cloudflare
-          pythonPackages.auth0-python
-          pythonPackages.pydantic
-          pythonPackages.pynacl
-          pythonPackages.bcrypt
-          pythonPackages.kubernetes
-          pythonPackages.packaging
-          keycloak
+      keycloak = (import nixpkgs-keycloak { system = "x86_64-linux"; }).python3Packages.python-keycloak;
 
-          # cloud packages
-          pythonPackages.azure-mgmt-containerservice
-          pythonPackages.azure-identity
-          pythonPackages.boto3
+      propagatedDependencies = [
+        pythonPackages.cookiecutter
+        pythonPackages.ruamel-yaml
+        pythonPackages.cloudflare
+        pythonPackages.auth0-python
+        pythonPackages.pydantic
+        pythonPackages.pynacl
+        pythonPackages.bcrypt
+        pythonPackages.kubernetes
+        pythonPackages.packaging
+        keycloak
 
-          # development
-          pythonPackages.pytest
-          pythonPackages.black
-          pythonPackages.flake8
-          pythonPackages.sphinx
+        # cloud packages
+        pythonPackages.azure-mgmt-containerservice
+        pythonPackages.azure-identity
+        pythonPackages.boto3
+      ];
 
-          # additional
-          pkgs.minikube
-          pkgs.k9s
-        ];
+      devDependencies = [
+        # development
+        pythonPackages.pytest
+        pythonPackages.black
+        pythonPackages.flake8
+        pythonPackages.sphinx
+
+        # additional
+        pkgs.minikube
+        pkgs.k9s
+      ];
+    in {
+      defaultApp.x86_64-linux = pythonPackages.buildPythonPackage {
+        pname = "qhub";
+        version = "latest";
+
+        src = ./.;
+
+        propagatedBuildInputs = propagatedDependencies;
+
+        patchPhase = ''
+          substituteInPlace setup.py \
+            --replace "cookiecutter==1.7.2" "cookiecutter" \
+            --replace "azure-identity==1.6.1" "azure-identity" \
+            --replace "azure-mgmt-containerservice==16.2.0" "azure-mgmt-containerservice"
+        '';
+
+        doCheck = false;
       };
-  };
+
+      devShell.x86_64-linux =
+        pkgs.mkShell {
+          buildInputs = propagatedDependencies ++ devDependencies;
+        };
+    };
 }
