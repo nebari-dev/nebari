44071318395129e8add8ad02340d6421f942cdc4
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2022-01-22 17:15:09 -0500

    Further removal of cookiecutter

--- qhub/template/{{ cookiecutter.repo_directory }}/stages/07-kubernetes-services/monitoring.tf
+++ qhub/template/{{ cookiecutter.repo_directory }}/stages/07-kubernetes-services/monitoring.tf
@@ -1,8 +1,14 @@
-{% if cookiecutter.monitoring.enabled -%}
+variable "monitoring-enabled" {
+  description = "Prometheus and Grafana monitoring enabled"
+  type        = bool
+  default     = true
+}
+
 module "monitoring" {
+  count = var.monitoring-enabled ? 1 : 0
+
   source       = "./modules/kubernetes/services/monitoring"
   namespace    = var.environment
   external-url = var.endpoint
   realm_id     = var.realm_id
 }
-{% endif -%}
--- qhub/template/{{ cookiecutter.repo_directory }}/stages/07-kubernetes-services/prefect.tf
+++ qhub/template/{{ cookiecutter.repo_directory }}/stages/07-kubernetes-services/prefect.tf
@@ -1,26 +1,34 @@
 # ======================= VARIABLES ======================
-{% if cookiecutter.prefect.enabled -%}
-variable "prefect_token" {
+variable "prefect-enabled" {
+  description = "Prefect enabled or disabled"
+  type = bool
+}
+
+variable "prefect-image" {
+  description = "Prefect image"
   type = string
 }
-{%- endif %}
+
+variable "prefect-token" {
+  description = "Prefect token"
+  type = string
+}
+
+variable "prefect-overrides" {
+  description = "Prefect token"
+  type = map
+}
+
 
 # ====================== RESOURCES =======================
-{% if cookiecutter.prefect.enabled -%}
 module "prefect" {
+  count = var.prefect-enabled ? 1 : 0
+
   source = "./modules/kubernetes/services/prefect"
 
   namespace            = var.environment
   jupyterhub_api_token = module.jupyterhub.services.prefect.api_token
-  prefect_token        = var.prefect_token
-  {% if cookiecutter.prefect.image is defined -%}
-  image                = "{{ cookiecutter.prefect.image }}"
-  {% endif -%}
-  {% if cookiecutter.prefect.overrides is defined %}
-  overrides            = [<<EOT
-{{ cookiecutter.prefect.overrides|yamlify -}}
-    EOT
-    ]
-  {% endif %}
+  prefect_token        = var.prefect-token
+  image                = var.prefect-image
+  overrides = [yamlencode(var.prefect-overrides)]
 }
-{% endif -%}
