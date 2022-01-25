fd6adb5ca5cab1012b9f112e8b8349af85e44c47
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2022-01-24 22:01:01 -0500

    Adding authentication

--- qhub/template/{{ cookiecutter.repo_directory }}/stages/07-kubernetes-services/modules/kubernetes/services/meta/qhub/output.tf
+++
@@ -1,9 +0,0 @@
-output "jupyterhub_api_token" {
-  description = "API token to enable in jupyterhub server"
-  value       = random_password.jupyterhub_api_token.result
-}
-
-output "tls" {
-  description = "TLS configuration"
-  value       = local.tls
-}
