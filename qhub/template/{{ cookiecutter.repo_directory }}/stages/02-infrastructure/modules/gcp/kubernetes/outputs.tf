72a20f94c78c0e524be69c68d7c11270e0f34101
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2022-01-27 21:10:06 -0500

    Adding kubeconfig for gcp

--- qhub/template/{{ cookiecutter.repo_directory }}/stages/02-infrastructure/modules/gcp/kubernetes/outputs.tf
+++ qhub/template/{{ cookiecutter.repo_directory }}/stages/02-infrastructure/modules/gcp/kubernetes/outputs.tf
@@ -9,14 +9,17 @@ output "credentials" {
   }
 }
 
+
+# https://github.com/terraform-google-modules/terraform-google-kubernetes-engine/blob/9172b3eaeeb806caca29aa1e3e83e58a26df57b1/modules/auth/main.tf
+data "google_client_config" "provider" {}
+
 output "kubeconfig" {
   description = "Kubeconfig for connecting to kubernetes cluster"
   sensitive = true
   value = templatefile("${path.module}/templates/kubeconfig.yaml", {
-    cluster_name    = google_container_cluster.main.name
-    endpoint        = google_container_cluster.main.endpoint
-    cluster_ca      = google_container_cluster.main.master_auth.0.cluster_ca_certificate
-    client_cert     = google_container_cluster.main.master_auth.0.client_certificate
-    client_key      = google_container_cluster.main.master_auth.0.client_key
+    context                = google_container_cluster.main.name
+    cluster_ca_certificate = google_container_cluster.main.master_auth[0].cluster_ca_certificate
+    endpoint               = google_container_cluster.main.endpoint
+    token                  = data.google_client_config.provider.access_token
   })
 }
