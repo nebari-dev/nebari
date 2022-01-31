c6f89b12dae3cbeb53f81465862704906f0ac2f4
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2022-01-31 14:57:34 -0500

    State syncing for terraform state

--- qhub/deploy.py
+++ qhub/deploy.py
@@ -11,7 +11,6 @@ from qhub.provider import terraform
 from qhub.utils import timer, check_cloud_credentials, modified_environ, split_docker_image_name
 from qhub.provider.dns.cloudflare import update_record
 from qhub.render.terraform import QHubKubernetesProvider, QHubTerraformState, QHubGCPProvider, QHubAWSProvider
-from qhub.state import terraform_state_sync
 
 logger = logging.getLogger(__name__)
 
@@ -95,15 +94,21 @@ def provision_01_terraform_state(stage_outputs, config):
         stage_outputs[directory] = {}
     elif config['provider'] == 'do':
         stage_outputs[directory] = terraform.deploy(
-            os.path.join(directory, config['provider']),
+            terraform_import=True,
+            directory=os.path.join(directory, config['provider']),
             input_vars={
                 'name': config['project_name'],
                 'namespace': config['namespace'],
                 'region': config['digital_ocean']['region']
-            })
+            },
+            state_imports=[(
+                "module.terraform-state.module.spaces.digitalocean_spaces_bucket.main",
+                f"{config['digital_ocean']['region']},{config['project_name']}-{config['namespace']}-terraform-state",
+            )])
     elif config['provider'] == 'gcp':
         stage_outputs[directory] = terraform.deploy(
-            os.path.join(directory, config['provider']),
+            terraform_import=True,
+            directory=os.path.join(directory, config['provider']),
             input_vars={
                 'name': config['project_name'],
                 'namespace': config['namespace'],
@@ -111,26 +116,54 @@ def provision_01_terraform_state(stage_outputs, config):
             },
             terraform_objects=[
                 QHubGCPProvider(config),
-            ])
+            ],
+            state_imports=[(
+                "module.terraform-state.module.gcs.google_storage_bucket.static-site",
+                f"{config['project_name']}-{config['namespace']}-terraform-state",
+            )])
     elif config['provider'] == 'azure':
+        subscription_id = os.environ['ARM_SUBSCRIPTION_ID']
+        resource_group_name = f"{config['project_name']}-{config['namespace']}"
+        resource_group_name_safe = resource_group_name.replace("-", "")
+        resource_group_url = f"/subscriptions/{subscription_id}/resourceGroups/{config['project_name']}-{config['namespace']}"
+
         stage_outputs[directory] = terraform.deploy(
-            os.path.join(directory, config['provider']),
+            terraform_import=True,
+            directory=os.path.join(directory, config['provider']),
             input_vars={
                 'name': config['project_name'],
                 'namespace': config['namespace'],
                 'region': config['azure']['region'],
                 'storage_account_postfix': config['azure']['storage_account_postfix'],
-            })
+            },
+            state_imports=[(
+                "module.terraform-state.azurerm_resource_group.terraform-resource-group",
+                resource_group_url,
+            ), (
+                "module.terraform-state.azurerm_storage_account.terraform-storage-account",
+                f"{resource_group_url}/providers/Microsoft.Storage/storageAccounts/{resource_group_name_safe}{config['storage_account_postfix']}",
+            ), (
+                "module.terraform-state.azurerm_storage_container.storage_container",
+                f"https://{resource_group_name_safe}{config['storage_account_postfix']}.blob.core.windows.net/{resource_group_name}state",
+            )])
     elif config['provider'] == 'aws':
         stage_outputs[directory] = terraform.deploy(
-            os.path.join(directory, config['provider']),
+            terraform_import=True,
+            directory=os.path.join(directory, config['provider']),
             input_vars={
                 'name': config['project_name'],
                 'namespace': config['namespace'],
             },
             terraform_objects=[
                 QHubAWSProvider(config),
-            ])
+            ],
+            state_imports=[(
+                "module.terraform-state.aws_s3_bucket.terraform-state",
+                f"{config['project_name']}-{config['namespace']}-terraform-state",
+            ), (
+                "module.terraform-state.aws_dynamodb_table.terraform-state-lock",
+                f"{config['project_name']}-{config['namespace']}-terraform-state-lock",
+            )])
     else:
         raise NotImplementedError(f'provider {config["provider"]} not implemented for directory={directory}')
 
