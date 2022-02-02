1829fd411f6c3b7a0911fd3472ed9cc2f6c815e4
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2022-02-02 12:04:08 -0500

    Fixing centos EOL issue

--- qhub/template/image/scripts/install-yum-minimal.sh
+++ qhub/template/image/scripts/install-yum-minimal.sh
@@ -1,3 +1,9 @@
+# Should consider
+# https://github.com/CentOS/sig-cloud-instance-images/issues/190
+find /etc/yum.repos.d/ -type f -exec sed -i 's/mirrorlist=/#mirrorlist=/g' {} +
+find /etc/yum.repos.d/ -type f -exec sed -i 's/#baseurl=/baseurl=/g' {} +
+find /etc/yum.repos.d/ -type f -exec sed -i 's/mirror.centos.org/vault.centos.org/g' {} +
+
 yum -y update && \
     yum install -y wget bzip2 ca-certificates curl git && \
     yum clean all && \
