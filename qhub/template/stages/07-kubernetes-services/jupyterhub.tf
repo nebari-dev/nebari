c896986079886456a9cea2f0776cab8437cc4c78
viniciusdc <vinivdc2009@hotmail.com>
2022-02-28 15:27:28 -0300

    Ajust idle-culler settings

--- qhub/template/stages/07-kubernetes-services/jupyterhub.tf
+++ qhub/template/stages/07-kubernetes-services/jupyterhub.tf
@@ -107,6 +107,11 @@ module "jupyterhub" {
       name = "dask-etc"
       namespace = var.environment
       kind = "configmap"
+    },
+    "/etc/jupyter" = {
+      name = "server-idle-culling"
+      namespace = var.environment
+      kind = "configmap"
     }
   }
 
