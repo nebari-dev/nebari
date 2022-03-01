0e4ed91ef32c56926025ca01ece0798941c86ae3
viniciusdc <vinivdc2009@hotmail.com>
2022-03-01 11:38:28 -0300

    Adjust idle-culler settings

--- qhub/template/stages/07-kubernetes-services/modules/kubernetes/services/jupyterhub/files/04-idle-culler.py
+++ qhub/template/stages/07-kubernetes-services/modules/kubernetes/services/jupyterhub/files/04-idle-culler.py
@@ -5,19 +5,21 @@
 # Extra config available at:
 # https://zero-to-jupyterhub.readthedocs.io/en/1.x/jupyterhub/customizing/user-management.html#culling-user-pods
 
-# Shut down the server after N seconds with no kernels or terminals running and no activity.
-# c.NotebookApp.shutdown_no_activity_timeout = 400
+# Timeout (in seconds) in which a terminal has been inactive and ready to
+# be culled.
+c.TerminalManager.cull_inactive_timeout = 10 * 60
 
-c.TerminalManager.cull_inactive_timeout = 120
-c.TerminalManager.cull_interval = 60
+# The interval (in seconds) on which to check for terminals exceeding the
+# inactive timeout value.
+c.TerminalManager.cull_interval = 1 * 60
 
 # cull_idle_timeout: timeout (in seconds) after which an idle kernel is
 # considered ready to be culled
-c.MappingKernelManager.cull_idle_timeout = 3 * 60
+c.MappingKernelManager.cull_idle_timeout = 10 * 60
 
 # cull_interval: the interval (in seconds) on which to check for idle
 # kernels exceeding the cull timeout value
-c.MappingKernelManager.cull_interval = 90
+c.MappingKernelManager.cull_interval = 1 * 60
 
 # cull_connected: whether to consider culling kernels which have one
 # or more connections
@@ -26,3 +28,7 @@ c.MappingKernelManager.cull_connected = True
 # cull_busy: whether to consider culling kernels which are currently
 # busy running some code
 c.MappingKernelManager.cull_busy = False
+
+# Shut down the server after N seconds with no kernels or terminals
+# running and no activity.
+c.ServerApp.shutdown_no_activity_timeout = 10 * 60
