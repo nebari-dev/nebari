127324f69fb6e562d80fb273ca97a7280f87257a
viniciusdc <vinivdc2009@hotmail.com>
2022-03-03 11:30:33 -0300

    rm extra comments, update timout time and description

--- qhub/template/stages/07-kubernetes-services/modules/kubernetes/services/jupyterhub/files/04-idle-culler.py
+++ qhub/template/stages/07-kubernetes-services/modules/kubernetes/services/jupyterhub/files/04-idle-culler.py
@@ -7,19 +7,19 @@
 
 # Timeout (in seconds) in which a terminal has been inactive and ready to
 # be culled.
-c.TerminalManager.cull_inactive_timeout = 10 * 60
+c.TerminalManager.cull_inactive_timeout = 15 * 60
 
 # The interval (in seconds) on which to check for terminals exceeding the
 # inactive timeout value.
-c.TerminalManager.cull_interval = 1 * 60
+c.TerminalManager.cull_interval = 5 * 60
 
 # cull_idle_timeout: timeout (in seconds) after which an idle kernel is
 # considered ready to be culled
-c.MappingKernelManager.cull_idle_timeout = 10 * 60
+c.MappingKernelManager.cull_idle_timeout = 15 * 60
 
 # cull_interval: the interval (in seconds) on which to check for idle
 # kernels exceeding the cull timeout value
-c.MappingKernelManager.cull_interval = 1 * 60
+c.MappingKernelManager.cull_interval = 5 * 60
 
 # cull_connected: whether to consider culling kernels which have one
 # or more connections
@@ -31,4 +31,8 @@ c.MappingKernelManager.cull_busy = False
 
 # Shut down the server after N seconds with no kernels or terminals
 # running and no activity.
-c.ServerApp.shutdown_no_activity_timeout = 10 * 60
+c.NotebookApp.shutdown_no_activity_timeout = 15 * 60
+
+###############################################################################
+# JupyterHub idle culler total timeout corresponds (approximately) to:
+# max(cull_idle_timeout, cull_inactive_timeout) + shutdown_no_activity_timeout
