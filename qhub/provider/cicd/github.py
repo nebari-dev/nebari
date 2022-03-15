7fbc1496b2a78ec2c4a7fce6a1f444f0ce48cb60
iameskild <eskild@doublee.io>
2022-03-15 11:16:30 +0100

    Add QHUB_GH_BRANCH as env var to workflows

--- qhub/provider/cicd/github.py
+++ qhub/provider/cicd/github.py
@@ -95,6 +95,9 @@ def gha_env_vars(config):
         "GITHUB_TOKEN": "${{ secrets.GITHUB_TOKEN }}",
     }
 
+    if os.environ.get("QHUB_GH_BRANCH"):
+        env_vars["QHUB_GH_BRANCH"] = "${{ secrets.QHUB_GH_BRANCH }}"
+
     if config["provider"] == "aws":
         env_vars["AWS_ACCESS_KEY_ID"] = "${{ secrets.AWS_ACCESS_KEY_ID }}"
         env_vars["AWS_SECRET_ACCESS_KEY"] = "${{ secrets.AWS_SECRET_ACCESS_KEY }}"
