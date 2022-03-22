65eb8bc2337a09d32087465a54165b3cf49e0942
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2022-03-22 17:29:37 -0400

    Adding streamlit example

---
+++ qhub/template/stages/07-kubernetes-services/modules/kubernetes/services/jupyterhub/files/examples/dashboards/streamlit.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.title('My first StreamLit QHub app')
+
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
