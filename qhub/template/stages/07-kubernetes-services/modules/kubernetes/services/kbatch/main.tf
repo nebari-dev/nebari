3ef6b98400310b01512354cab5f7de42f3143ceb
iameskild <eskild@doublee.io>
2022-04-22 02:59:08 +0000

    add k8s role for kbatch

--- qhub/template/stages/07-kubernetes-services/modules/kubernetes/services/kbatch/main.tf
+++ qhub/template/stages/07-kubernetes-services/modules/kubernetes/services/kbatch/main.tf
@@ -13,6 +13,12 @@ resource "helm_release" "kbatch" {
         jupyterhub_api_url = "https://${var.external-url}/hub/api/"
         extra_env = {
           KBATCH_PREFIX = "/services/kbatch"
+          # KBATCH_JOB_EXTRA_ENV = {
+          #   "DASK_GATEWAY__AUTH__TYPE": "jupyterhub",
+          #   "DASK_GATEWAY__CLUSTER__OPTIONS__IMAGE": "{JUPYTER_IMAGE_SPEC}",
+          #   "DASK_GATEWAY__ADDRESS":  "https://<JUPYTERHUB_URL>/services/dask-gateway",
+          #   "DASK_GATEWAY__PROXY_ADDRESS": "gateway://<DASK_GATEWAY_ADDRESS>:80"
+          # }
         }
       }
       image = {
@@ -36,4 +42,42 @@ resource "helm_release" "kbatch" {
     value = var.namespace
   }
 
+}
+
+resource "kubernetes_cluster_role" "kbatch" {
+  metadata {
+    name = "${var.name}-kbatch"
+  }
+
+  rule {
+    api_groups = ["", "batch"]
+    resources  = ["*"]
+    verbs      = ["get", "watch", "list", "patch", "create"]
+  }
+
+  rule {
+    api_groups = ["gateway.dask.org"]
+    resources  = ["daskclusters"]
+    verbs      = ["*"]
+  }
+}
+
+
+resource "kubernetes_cluster_role_binding" "kbatch" {
+  metadata {
+    name = "${var.name}-kbatch"
+    namespace = var.namespace
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = kubernetes_cluster_role.kbatch.metadata.0.name
+  }
+  subject {
+    kind      = "ServiceAccount"
+    name      = kubernetes_service_account.kbatch.metadata.0.name
+    namespace = var.namespace
+    api_group = ""
+  }
 }
