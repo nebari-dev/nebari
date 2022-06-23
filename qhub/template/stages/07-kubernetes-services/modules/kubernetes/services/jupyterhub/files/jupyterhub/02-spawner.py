38e427b399512c15aca0affbcfe5273ddf5533f8
eskild <42120229+iameskild@users.noreply.github.com>
2022-06-23 14:09:59 -0700

    Add panel-serve as a CDS dashboard option (#1070)
    * Add panel-serve as a CDS dashboard option
    
    * Update pythonpath
    
    * get username from auth_state
    
    * use pre_spawn_hook to get username

--- qhub/template/stages/07-kubernetes-services/modules/kubernetes/services/jupyterhub/files/jupyterhub/02-spawner.py
+++ qhub/template/stages/07-kubernetes-services/modules/kubernetes/services/jupyterhub/files/jupyterhub/02-spawner.py
@@ -1,6 +1,9 @@
 # remove after next kubespawner release past 1/20/2022
 # https://github.com/jupyterhub/kubespawner/pull/558
+import os
+
 import kubernetes.client.models
+from tornado import gen
 
 kubernetes.client.models.V1EndpointPort = kubernetes.client.models.CoreV1EndpointPort
 
@@ -9,6 +12,21 @@ from kubespawner import KubeSpawner  # noqa: E402
 
 cdsdashboards = z2jh.get_config("custom.cdsdashboards")
 conda_store_environments = z2jh.get_config("custom.environments")
+
+
+@gen.coroutine
+def get_username_hook(spawner):
+    auth_state = yield spawner.user.get_auth_state()
+    username = auth_state["oauth_user"]["preferred_username"]
+
+    spawner.environment.update(
+        {
+            "PREFERRED_USERNAME": username,
+        }
+    )
+
+
+c.Spawner.pre_spawn_hook = get_username_hook
 
 if cdsdashboards["enabled"]:
     from cdsdashboards.app import CDS_TEMPLATE_PATHS
@@ -34,6 +52,28 @@ if cdsdashboards["enabled"]:
     c.CDSDashboardsConfig.conda_envs = [
         environment["name"] for _, environment in conda_store_environments.items()
     ]
+
+    # TODO: make timeouts configurable
+    c.VariableMixin.proxy_ready_timeout = 600
+    c.VariableMixin.proxy_request_timeout = 600
+
+    home_username = f"/home/{os.getenv('PREFERRED_USERNAME')}"
+    c.CDSDashboardsConfig.extra_presentation_types = ["panel-serve"]
+    c.VariableMixin.extra_presentation_launchers = {
+        "panel-serve": {
+            "args": [
+                "python3",
+                "{presentation_path}",
+                "{--}port",
+                "{port}",
+                "{--}address",
+                "{origin_host}",
+            ],
+            "debug_args": [],
+            "env": {"PYTHONPATH": f"{home_username}/{{presentation_dirname}}"},
+        }
+    }
+
 else:
     c.JupyterHub.allow_named_servers = False
     c.JupyterHub.spawner_class = KubeSpawner
