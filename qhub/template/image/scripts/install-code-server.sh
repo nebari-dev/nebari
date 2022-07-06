c27899f8046a595f3ba2a9bdb8d2c023107e9bca
viniciusdc <vinivdc2009@hotmail.com>
2022-07-05 22:29:52 -0300

    change install location to use local env

--- qhub/template/image/scripts/install-code-server.sh
+++ qhub/template/image/scripts/install-code-server.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 set -xe
+DEFAULT_PREFIX="${1}"; shift # path to environment yaml or lock file
 
-mkdir -p /opt/conda/envs/${DEFAULT_ENV}/share/code-server
-cd /opt/conda/envs/${DEFAULT_ENV}/share/code-server
+mkdir -p ${DEFAULT_PREFIX}/share/code-server
+cd ${DEFAULT_PREFIX}/share/code-server
 
 # Fetch the snapshot of https://code-server.dev/install.sh as of the time of writing
-curl -fsSL https://raw.githubusercontent.com/coder/code-server/326a1d1862872955cec062030df2bd103799a1eb/install.sh > install.sh
+wget --quiet https://raw.githubusercontent.com/coder/code-server/326a1d1862872955cec062030df2bd103799a1eb/install.sh
 expected_sum=ed18563871beb535130019b6c5b62206cc4a60c8bf4256aae96ce737951fc253
 
 if [[ ! $(sha256sum install.sh) == "${expected_sum}  install.sh" ]];then
@@ -13,7 +14,7 @@ if [[ ! $(sha256sum install.sh) == "${expected_sum}  install.sh" ]];then
     exit 1
 fi
 
-sh install.sh --prefix "/opt/conda/envs/${DEFAULT_ENV}/share/code-server"
+sh ./install.sh --prefix ${DEFAULT_PREFIX}/share/code-server
 
 # Install the VS code proxy
 pip install git+https://github.com/betatim/vscode-binder
