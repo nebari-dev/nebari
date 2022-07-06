4467325be7ca5ce86e8ecdd8b6de03667dc61e39
Vinicius D. Cerutti <51954708+viniciusdc@users.noreply.github.com>
2022-07-06 18:25:52 -0300

    Fix vscode bug when installing extensions (#1360)
    * rollback to code-server installation
    
    * test if build passes
    
    * bump dask-labextension to fix CI
    
    * update image again test CI
    
    * update image again test CI
    
    * rollback to code-server installation
    
    * rollback dask-lab version
    
    * install extension from conda
    
    * change install location to use local env
    
    * change install location to use local env
    
    * change install location to use local env
    
    * change install location to use local env
    
    * Update script installation
    
    * Try to fix missing path
    
    * Try to fix missing path
    
    * try something else
    
    * try something else
    
    * Trigger CI build
    
    * try something else
    
    * Rollback to previous conda install
    
    * Use jupyter proxy instead of changing installation
    
    * Update install method
    
    * rm proxy settings
    
    * add code-server to path
    
    * add code-server to path
    
    * Will fix everything
    
    * Will fix everything 2
    
    * fix dir typo
    
    * missed another typo again
    
    * Will fix everything 3
    
    * Will fix everything 4
    
    * Will fix everything 4
    
    * Will fix everything 5
    
    * rm external proxy and use jupyter builtin
    
    * rollback to using vscode proxy
    
    * Update qhub/template/image/jupyterlab/environment.yaml
    
    Co-authored-by: Vinicius D. Cerutti <51954708+viniciusdc@users.noreply.github.com>
    
    Co-authored-by: eskild <42120229+iameskild@users.noreply.github.com>

---
+++ qhub/template/image/scripts/install-code-server.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -xe
+DEFAULT_PREFIX="${1}"; shift # path to environment yaml or lock file
+
+mkdir -p ${DEFAULT_PREFIX}/code-server
+cd ${DEFAULT_PREFIX}/code-server
+
+# Fetch the snapshot of https://code-server.dev/install.sh as of the time of writing
+wget --quiet https://raw.githubusercontent.com/coder/code-server/326a1d1862872955cec062030df2bd103799a1eb/install.sh
+expected_sum=ed18563871beb535130019b6c5b62206cc4a60c8bf4256aae96ce737951fc253
+
+if [[ ! $(sha256sum install.sh) == "${expected_sum}  install.sh" ]];then
+    echo Unexpected hash from code-server install script
+    exit 1
+fi
+
+mkdir /opt/tmpdir
+sh ./install.sh --method standalone --prefix /opt/tmpdir
+
+mv /opt/tmpdir/lib/code-server-4.5.0/* ${DEFAULT_PREFIX}/code-server
+rm -rf /opt/tmpdir
