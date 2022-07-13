4f2802524ad28ea07597f7ad8dd18abe83973020
Harsh Mishra <erbeusgriffincasper@gmail.com>
2022-07-13 11:29:55 +0530

    Improvements in the QHub Cost estimate tool (#1365)
    * feat: make infracost dashboard customizable to end users
    
    * feat: make it possible to download infracost reports as json
    
    * feat: add currency codes to make customizable cost reports possible
    
    * feat: add infracost diff feature to cost estimate
    
    * make suggestions

--- qhub/cost.py
+++ qhub/cost.py
@@ -47,6 +47,19 @@ def _check_infracost_api_key():
         return False
 
 
+def _set_currency_code(currency_code):
+    """
+    Specify the currency code for infracost
+    """
+    try:
+        subprocess.check_output(
+            ["infracost", "configure", "set", "currency", currency_code]
+        )
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+
 def _run_infracost(path):
     """
     Run infracost on the given path and return the JSON output
@@ -84,28 +97,88 @@ def _enable_infracost_dashboard():
         return False
 
 
-def infracost_report(path):
+def _disable_infracost_dashboard():
+    """
+    Disable infracost dashboard
+    """
+    try:
+        subprocess.check_output(
+            ["infracost", "configure", "set", "enable_dashboard", "false"]
+        )
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+
+def infracost_diff(path, compare_to_path):
+    """
+    Compare the infracost report of the given path to a previous infracost report
+    """
+    try:
+        process = subprocess.Popen(
+            [
+                "infracost",
+                "diff",
+                "--path",
+                path,
+                "--compare-to",
+                compare_to_path,
+                "--format",
+                "json",
+            ],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        stdout, stderr = process.communicate()
+        diff_data = json.loads(
+            re.search("({.+})", stdout.decode("UTF-8"))
+            .group(0)
+            .replace("u'", '"')
+            .replace("'", '"')
+        )
+        return diff_data
+    except subprocess.CalledProcessError:
+        return None
+
+
+def infracost_report(path, dashboard, file, currency_code, compare):
     """
     Generate a report of the infracost cost of the given path
     args:
         path: path to the qhub stages directory
     """
+    console = Console()
+    # If path is not provided, use the current directory with `stages` subdirectory
     if not path:
         path = os.path.join(os.getcwd(), "stages")
 
+    # Checks if infracost is installed and an API key is configured
     if _check_infracost() and _check_infracost_api_key():
-        _enable_infracost_dashboard()
+        _set_currency_code(currency_code)
+        if not dashboard:
+            _disable_infracost_dashboard()
+        else:
+            _enable_infracost_dashboard()
+
+        # Check if the deployment is available on the given path
         if not os.path.exists(path):
             logger.error("Deployment is not available")
         else:
             data = _run_infracost(path)
+
+            # If a user has asked for a JSON file, download it
+            if file:
+                # Convert data to JSON and save it to the given file
+                with open(file, "w") as f:
+                    json.dump(data, f)
+
             if data:
                 cost_table = Table(title="Cost Breakdown")
                 cost_table.add_column(
                     "Name", justify="right", style="cyan", no_wrap=True
                 )
                 cost_table.add_column(
-                    "Cost ($)", justify="right", style="cyan", no_wrap=True
+                    "Cost", justify="right", style="cyan", no_wrap=True
                 )
 
                 cost_table.add_row("Total Monthly Cost", data["totalMonthlyCost"])
@@ -139,11 +212,37 @@ def infracost_report(path):
                     "Total Usage-Priced Resources",
                     str(data["summary"]["totalUsageBasedResources"]),
                 )
-
-                console = Console()
                 console.print(cost_table)
                 console.print(resource_table)
-                console.print(f"Access the dashboard here: {data['shareUrl']}\n")
+                # If dashboard is enabled, display the dashboard
+                if dashboard:
+                    console.print(
+                        f"Access the cost & resource breakdown dashboard here: {data['shareUrl']}\n"
+                    )
+
+                # If the user is comparing the cost of the current deployment to a previous deployment
+                if compare:
+                    diff_data = infracost_diff(path, compare)
+                    diff_table = Table(title="Diff Breakdown")
+                    diff_table.add_column(
+                        "Name", justify="right", style="cyan", no_wrap=True
+                    )
+                    diff_table.add_column(
+                        "Cost", justify="right", style="cyan", no_wrap=True
+                    )
+                    diff_table.add_row(
+                        "Diff Monthly Cost", diff_data["diffTotalHourlyCost"]
+                    )
+                    diff_table.add_row(
+                        "Diff Hourly Cost", diff_data["diffTotalMonthlyCost"]
+                    )
+                    console.print(diff_table)
+                    # If dashboard is enabled, display the diff breakdown dashboard
+                    if dashboard:
+                        console.print(
+                            f"Access the diff breakdown dashboard here: {diff_data['shareUrl']}\n"
+                        )
+
                 console.print(Markdown(INFRACOST_NOTE))
             else:
                 logger.error(
