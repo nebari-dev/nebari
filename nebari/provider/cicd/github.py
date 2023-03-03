28078ad614eacdd5627c37435142a81d63fd5b15
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2023-03-03 11:31:44 -0500

    Allow for arbitrary before and after steps in github-actions nebari deployment

--- nebari/provider/cicd/github.py
+++ nebari/provider/cicd/github.py
@@ -241,11 +241,16 @@ def gen_nebari_ops(config):
     step1 = checkout_image_step()
     step2 = setup_python_step()
     step3 = install_nebari_step(nebari_version)
+    gha_steps = [step1, step2, step3]
+
+    for step in config['ci_cd'].get('before_script'):
+        gha_steps.append(GHA_job_step(**step))
 
     step4 = GHA_job_step(
         name="Deploy Changes made in nebari-config.yaml",
         run=f"nebari deploy -c nebari-config.yaml --disable-prompt{' --skip-remote-state-provision' if os.environ.get('NEBARI_GH_BRANCH') else ''}",
     )
+    gha_steps.append(step4)
 
     step5 = GHA_job_step(
         name="Push Changes",
@@ -262,10 +267,11 @@ def gen_nebari_ops(config):
             )
         },
     )
-
-    gha_steps = [step1, step2, step3, step4]
     if commit_render:
         gha_steps.append(step5)
+
+    for step in config['ci_cd'].get('after_script'):
+        gha_steps.append(GHA_job_step(**step))
 
     job1 = GHA_job_id(name="nebari", runs_on_="ubuntu-latest", steps=gha_steps)
     jobs = GHA_jobs(__root__={"build": job1})
