de1a6ea46a667d91f5bc1843a63370109e88228c
viniciusdc <vinivdc2009@hotmail.com>
2023-03-14 00:38:04 -0300

    Initial version for helm validation/parser

--- scripts/helm-validate.py
+++ scripts/helm-validate.py
@@ -2,6 +2,7 @@ import pathlib
 import re
 
 import hcl2
+from nebari.utils import deep_merge
 
 # 1. Get a list of paths whose files contains the helm_release resource
 # 2. In each file, get the helm_release resource using the hcl2 library
@@ -9,14 +10,14 @@ import hcl2
 # { "chart_name": {"version" : "1.2.3", "repository": "https://charts.bitnami.com/bitnami"} }
 
 STAGES_DIR = "nebari/template/stages"
-
+SKIP_CHARTS = ["prefect", "clearml", "helm-extensions"]
+DEBUG = False
 
 def get_filepaths_that_contain_helm_release():
     """Get list of helm charts from nebari code-base"""
     # using pathlib to get list of files in the project root dir, look for all .tf files that
     # contain helm_release
     path = pathlib.Path(__file__).parent.parent.absolute()
-    # print(path)
     path_tree = path.glob(f"{STAGES_DIR}/**/main.tf")
     paths = []
     for file in path_tree:
@@ -24,7 +25,6 @@ def get_filepaths_that_contain_helm_release():
         # variables, so we will just do a superficial check for now
         with open(file) as f:
             contents = f.read()
-            # print(contents)
             if "helm_release" in contents:
                 paths.append(file)
             else:
@@ -40,193 +40,112 @@ def _argument_contains_variable_hook(argument):
         # we are checking this here but grabbing this value would be really difficult
         return True
 
-
-def process_chart_metadata(attrs):
-    """Process chart metadata and return a dict"""
-    chart_name = attrs.get("chart", "")
-    chart_version = attrs.get("version", "")
-    chart_repository = attrs.get("repository", "")
-
-    # if chart_version is a variable, we need to get the value from locals or variables(?)
-    for argument in [chart_version, chart_repository]:
-        if _argument_contains_variable_hook(argument):
-            print(f"Found variable in {chart_name} chart metadata")
-            return {
-                "version": chart_version,
-                "repository": chart_repository,
-                "possible_variable": argument,
-                "name": chart_name,
-            }
-    else:
-        return {"version": chart_version, "repository": chart_repository}
-
-
-def get_helm_release_contents_from_hcl(filepath):
-    """Load HCL file and parsed related helm_release resource"""
-    # {
-    #     "name": {"version": "", "repository": ""},
-    # }
-    helm_charts = {}
-    with open(filepath, "r") as file:
-        hcl = hcl2.load(file)
-        for resource in hcl.get("resource", {}):
-            try:
-                for release_name, release_attrs in resource["helm_release"].items():
-                    if release_name in ["prefect", "clearml", "custom-helm-deployment"]:
-                        continue
-                    chart_name = release_attrs.get("chart", "")
-                    helm_charts[chart_name] = {
-                        **process_chart_metadata(release_attrs),
-                        "filepath": filepath.as_posix(),
-                    }
-            except KeyError:
-                # print(f"No helm_release found for {filepath}")
-                continue
-
-    return helm_charts
-
-
 def _clean_var_name(var_name, type):
     """Clean variable name"""
     if type == "local":
         # $(local.var_name)
-        return re.match(r"\(local.(.*)\)", var_name).group(1)
+        return re.findall(r"local.(.*[a-z])", var_name)[0]
     if type == "var":
         # $(var.var_name)
-        return re.match(r"\(var.(.*)\)", var_name).group(1)
+        return re.findall(r"var.(.*[a-z])", var_name)[0]
+
+def _load_variable_value(argument, parent_contents):
+    if "local." in argument:
+        var_name = _clean_var_name(argument, "local")
+        for local in parent_contents.get("locals", {}):
+            if var_name in local:
+                return local[var_name]
+        else:
+            raise ValueError(f"Could not find local variable {var_name}")
+    if "var." in argument:
+        var_name = _clean_var_name(argument, "var")
+        for var in parent_contents.get("variable", {}):
+            if var_name in var:
+                return var[var_name]
+        else:
+            raise ValueError(f"Could not find variable {var_name}")
 
 
-def post_process_charts(charts: list):
-    """Post process charts that contain variables in their metadata"""
-    for chart in charts:
-        # name = chart["name"]
-        parent_filepath = pathlib.Path(chart["filepath"]).parent
-        parent_tree_contents = {}
-        for file in parent_filepath.glob("**/*.tf"):
-            with open(file, "r") as f:
-                parent_tree_contents.update(hcl2.load(f))
-        print(parent_tree_contents)
-        # inspect locals and variables for the value of the variable
-        # if helm_release.get(name):
-        #     helm_resource = parent_tree_contents['resource']['helm_release'][helm_release]
-        # chart_version = helm_resource['version']
-        # if 'local.' in chart_version:
-        #     local = set(item.get(_clean_var_name(chart_version, 'local'), None) for item in parent_tree_contents['locals'])[0]
-        #     chart_version = local
+def retrieve_helm_information(filepath):
+    print("###############################################")
+    _charts = {}
+    print("-- Initialize helm_release store chart resource") if DEBUG else None
 
-        # if 'var.' in chart_version:
-        #     var = set(item.get(_clean_var_name(chart_version, 'var'), None) for item in parent_tree_contents['variable'])[0]
-        #     chart_version = var
+    parent_path = pathlib.Path(filepath).parent
 
-        # chart_repository = helm_resource['repository']
-        # if 'local.' in chart_repository:
-        #     local = set(item.get(_clean_var_name(chart_repository, 'local'), None) for item in parent_tree_contents['locals'])[0]
-        #     chart_repository = local
+    if parent_path.name in SKIP_CHARTS:
+        print(f"-- Skipping {parent_path.name}") if DEBUG else None
+        return _charts
 
-        # if 'var.' in chart_repository:
-        #     var = set(item.get(_clean_var_name(chart_repository, 'var'), None) for item in parent_tree_contents['variable'])[0]
-        #     chart_repository = var
+    print(f"-- Processing {parent_path.name}") if DEBUG else None
+    parent_contents = {}
+    for file in parent_path.glob("**/*.tf"):
+        with open(file, "r") as f:
+            parent_contents = deep_merge(parent_contents, hcl2.load(f))
+        # loading all the relatives will give us the locals and variables in case we need them
 
-        # print(f"Found {name} chart version: {chart_version} and repository: {chart_repository}")
+    for resource in parent_contents.get("resource", {}):
+        if "helm_release" not in resource:
+            print(f"-- Skipping {resource.keys()} (not helm_release)") if DEBUG else None
+            continue
+        try:
+            for release_name, release_attrs in resource["helm_release"].items():
+                print(f"-- Processing helm_release {release_name}") if DEBUG else None
+                chart_name = release_attrs.get("chart", "")
+                chart_version = release_attrs.get("version", "")
+                chart_repository = release_attrs.get("repository", "")
+                # if chart_version is a variable, we need to get the value from locals or variables(?)
+                if _argument_contains_variable_hook(chart_version):
+                    print(f"-- Spotted {chart_version} in {chart_name} chart metadata") if DEBUG else None
+                    chart_version = _load_variable_value(chart_version, parent_contents)
 
+                if _argument_contains_variable_hook(chart_repository):
+                    print(f"-- Spotted {chart_repository} in {chart_name} chart metadata") if DEBUG else None
+                    chart_repository = _load_variable_value(chart_repository, parent_contents)
+
+                print(f" Name: {chart_name} \n Version: {chart_version} \n Repository: {chart_repository}")
+
+                _charts.update({chart_name : {"version": chart_version, "repository": chart_repository}})
+        except KeyError:
+            print(f"Could not find helm_release in {filepath} for")
+            continue
+
+    if not _charts:
+        print(f"-- Could not find any helm_release under module resources") if DEBUG else None
+
+    return _charts
+
+def generate_index_of_helm_charts():
+    """Generate index of helm charts"""
+    paths = get_filepaths_that_contain_helm_release()
+    helm_charts = {}
+    for path in paths:
+        helm_information = retrieve_helm_information(path)
+        helm_charts.update(helm_information)
+    with open("helm_charts.json", "w") as f:
+        f.write(str(helm_charts))
+    return helm_charts
+
+def pull_helm_chart(chart_index : dict):
+    """Pull helm chart"""
+    import os
+    print("Creating helm_charts directory")
+    os.system("mkdir helm_charts && cd helm_charts")
+    for chart_name, chart_metadata in chart_index.items():
+        chart_version = chart_metadata["version"]
+        chart_repository = chart_metadata["repository"]
+        print(f"Adding {chart_name} repository")
+        os.system(f"helm repo add {chart_name} {chart_repository}")
+
+        print(f"Pulling {chart_name} chart")
+        os.system(f"helm pull {chart_name} --version {chart_version} --repo {chart_repository} --untar --untardir helm_charts")
+
+        print("Inspecting downloaded chart")
+        if not os.path.exists(f"helm_charts/{chart_name}-{chart_version}.tgz"):
+            raise f"Could not find {chart_name}:{chart_version} directory in helm_charts."
+    print("All charts downloaded successfully, removing helm_charts directory")
+    os.system("cd .. && rm -rf helm_charts")
 
 if __name__ == "__main__":
-    paths = get_filepaths_that_contain_helm_release()
-    charts = {}
-    for path in paths:
-        charts.update(get_helm_release_contents_from_hcl(path))
-    needs_post_processing = []
-    for chart_name, chart_attrs in charts.items():
-        if "possible_variable" in chart_attrs:
-            needs_post_processing.append(chart_attrs)
-    post_process_charts(needs_post_processing)
-
-# def get_filepaths_that_contain_helm_release():
-#     """Get list of helm charts from nebari code-base"""
-#     # using pathlib to get list of files in the project root dir, look for all .tf files that contain helm_release
-#     path = pathlib.Path(__file__).parent.parent.absolute()
-#     # print(path)
-#     path_tree = path.glob('nebari/template/stages/**/*.tf')
-#     paths = []
-#     for file in path_tree:
-#         load_hcl_file(file)
-#         # with open(file) as f:
-#         #     contents = f.read()
-#         # if "helm_release" in contents:
-#         #     # print(file)
-#         #     # print("###############################################")
-#         #     paths.append(file)
-#     print(f"Found {len(paths)} files that contain helm_release")
-#     return paths
-
-# def get_parent_dir(filepath):
-#     """Get parent directory of a file"""
-#     return pathlib.Path(filepath).parent
-
-# def filter_paths_retrieve_parent_dir(paths):
-#     """Filter paths and retrieve parent directory"""
-#     filtered_paths = set()
-#     for path in paths:
-#         filtered_paths.add(get_parent_dir(path))
-#     return filtered_paths
-
-# def load_hcl_file(filepath):
-#     with open(filepath, 'r') as file:
-#         hcl = hcl2.load(file)
-#         local = hcl.get('locals', {})
-#         resource = hcl.get('resource', {})
-#         for resource in hcl['resource']:
-#             # print(resource)
-#             try:
-#                 if 'helm_release' in resource.keys():
-#                     helm_release = resource['helm_release']
-#                     for release_name, values in helm_release.items():
-#                         # print("###############################################")
-#                         print(f"Release name: {release_name}")
-#                         print(f"Chart name: {values.get('chart', '')}")
-#                         print(f"Chart version: {values.get('version', '')}")
-#                         print(f"Chart repository: {values.get('repository', '')}")
-#                         print("###############################################")
-#                 else:
-#                     return
-#             except KeyError:
-#                 print("No helm_release found in this file")
-#                 print(filepath)
-#                 print(hcl)
-
-# def main():
-#     """Main function"""
-#     paths = get_filepaths_that_contain_helm_release()
-# filtered_paths = filter_paths_retrieve_parent_dir(paths)
-# for path in paths:
-#     print(path)
-#     load_hcl_file(path)
-
-
-# def get_helm_chart_list():
-#     """Get list of helm charts from nebari code-base"""
-#     # using pathlib to get list of files in the project root dir, look for all .tf files that contain helm_release
-#     path = pathlib.Path(__file__).parent.parent.absolute()
-#     # print(path)
-#     path_tree = path.glob('nebari/template/stages/**/*.tf')
-#     with open(Path('/home/vcerutti/Quansight/Projects/Nebari/nebari/nebari/template/stages/07-kubernetes-services/modules/kubernetes/services/argo-workflows/main.tf'), 'r') as file:
-#         hcl = hcl2.load(file)
-#         local = hcl.get('locals', {})
-#         resource = hcl.get('resource', {})
-#         for resource in hcl['resource']:
-#             pass
-# if resource['helm_release']:
-#     # print(resource['helm_release'])
-# print("###############################################")
-# for file in path_tree:
-#     with open(file) as f:
-#         contents = f.read()
-#         if "helm_release" in contents:
-#             print(file)
-#             print(contents)
-#             print("###############################################")
-#             #matches = re.match(r"default = \"(?<currentValue>([0-9]+)\\.([0-9]+)\\.([0-9]+))\"\\n}\\n\\nvariable \".+_helm_chart_name\" {\\n.+\\n  default = \"(?<depName>[a-z].+)\"\\n}\\n\\n.+\\n.+\\n.+\\n}\\n\\nvariable \".+_helm_repository\" {\\n.+\\n  default = \"(?<registryUrl>.*?)\"", f.read())
-#             # print(matches.groupdict())
-
-# if __name__ == "__main__":
-#     main()
+    charts = generate_index_of_helm_charts()
+    pull_helm_chart(charts)
