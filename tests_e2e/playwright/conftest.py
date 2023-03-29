c755179e90c09dd3701887078fe7e8eb13ffd584
Kim Pevey <kcpevey@gmail.com>
2023-03-29 12:33:46 -0500

    try/except on the nav fixture- temp

--- tests_e2e/playwright/conftest.py
+++ tests_e2e/playwright/conftest.py
@@ -11,21 +11,25 @@ def _navigator_session(browser_name):
     a server. Teardown when session is complete. Use `navigator` fixture
     for individual tests."""
     dotenv.load_dotenv()
-    nav = Navigator(
-        nebari_url=os.environ["NEBARI_FULL_URL"],
-        username=os.environ["USERNAME"],
-        password=os.environ["PASSWORD"],
-        headless=False,
-        browser=browser_name,
-        auth="password",
-        instance_name=os.environ["INSTANCE_NAME"],
-        video_dir="videos/",
-    )
-    nav.login_password()
-    nav.start_server()
-    yield nav
-
-    nav.teardown()
+    # TODO: try/except added here in attempt to reach teardown after error in
+    # order to close the browser context which will save the video so I debug
+    # the error.
+    try:
+        nav = Navigator(
+            nebari_url=os.environ["NEBARI_FULL_URL"],
+            username=os.environ["USERNAME"],
+            password=os.environ["PASSWORD"],
+            headless=False,
+            browser=browser_name,
+            auth="password",
+            instance_name=os.environ["INSTANCE_NAME"],
+            video_dir="videos/",
+        )
+        nav.login_password()
+        nav.start_server()
+        yield nav
+    except Exception:
+        nav.teardown()
 
 
 @pytest.fixture(scope="function")
