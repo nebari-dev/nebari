7da6cc2eeaa815e07834a6d8b44d2a88ae56385e
Kim Pevey <kcpevey@gmail.com>
2023-04-13 12:05:38 -0500

    extend notebook runtime

--- tests_e2e/playwright/test_playwright.py
+++ tests_e2e/playwright/test_playwright.py
@@ -12,4 +12,5 @@ def test_notebook(navigator):
         path="nebari/tests_e2e/playwright/test_data/test_notebook_output.ipynb",
         expected_output_text="success: 6",
         conda_env="conda-env-default-py",
+        runtime=60000,
     )
