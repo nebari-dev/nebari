f590a0312d9f53f9ca47fa6ac63754d5ddbbe877
Adam Lewis <23342526+Adam-D-Lewis@users.noreply.github.com>
2023-04-14 09:16:16 -0500

    Fix Existing dashboards (#1723)
    Co-authored-by: Adam-D-Lewis <>
    Co-authored-by: eskild <42120229+iameskild@users.noreply.github.com>

--- nebari/template/stages/07-kubernetes-services/modules/kubernetes/services/monitoring/main.tf
+++ nebari/template/stages/07-kubernetes-services/modules/kubernetes/services/monitoring/main.tf
@@ -7,7 +7,7 @@ resource "helm_release" "prometheus-grafana" {
 
   values = concat([
     file("${path.module}/values.yaml"),
-    # https://github.com/prometheus-community/helm-charts/blob/main/charts/kube-prometheus-stack/values.yaml
+    # https://github.com/prometheus-community/helm-charts/blob/kube-prometheus-stack-30.1.0/charts/kube-prometheus-stack/values.yaml
     jsonencode({
       alertmanager = {
         alertmanagerSpec = {
@@ -32,6 +32,9 @@ resource "helm_release" "prometheus-grafana" {
       }
 
       kube-state-metrics = {
+        # kube-state-metrics does not collect pod labels by default.
+        # This tells kube-state-metrics to collect app and component labels which are used by the jupyterhub grafana dashboards.
+        metricLabelsAllowlist = ["pods=[app,component,hub.jupyter.org/username,app.kubernetes.io/component,gateway.dask.org/cluster]", "nodes=[*]"] # ["pods=[*]"] would collect all pod labels, but is not recommended.
         nodeSelector = {
           "${var.node-group.key}" = var.node-group.value
         }
@@ -42,6 +45,110 @@ resource "helm_release" "prometheus-grafana" {
           nodeSelector = {
             "${var.node-group.key}" = var.node-group.value
           }
+          additionalScrapeConfigs = [
+            {
+              job_name     = "Jupyterhub"
+              metrics_path = "/hub/metrics"
+              static_configs = [
+                { targets = [
+                  "hub.${var.namespace}.svc:8081",
+                  ]
+                }
+              ]
+              authorization = {
+                type        = "Bearer"
+                credentials = var.jupyterhub_api_token
+              }
+            },
+            {
+              "job_name" = "Kubernetes Services"
+
+              "honor_labels" = true
+
+              "kubernetes_sd_configs" = [{
+                "role" = "endpoints"
+              }]
+
+              "relabel_configs" = [
+                {
+                  "action" = "keep"
+
+                  "regex" = true
+
+                  "source_labels" = ["__meta_kubernetes_service_annotation_prometheus_io_scrape"]
+                },
+                {
+                  "action" = "drop"
+
+                  "regex" = true
+
+                  "source_labels" = ["__meta_kubernetes_service_annotation_prometheus_io_scrape_slow"]
+                },
+                {
+                  "action" = "replace"
+
+                  "regex" = "(https?)"
+
+                  "source_labels" = ["__meta_kubernetes_service_annotation_prometheus_io_scheme"]
+
+                  "target_label" = "__scheme__"
+                },
+                {
+                  "action" = "replace"
+
+                  "regex" = "(.+)"
+
+                  "source_labels" = ["__meta_kubernetes_service_annotation_prometheus_io_path"]
+
+                  "target_label" = "__metrics_path__"
+                },
+                {
+                  "action" = "replace"
+
+                  "regex" = "(.+?)(?::\\d+)?;(\\d+)"
+
+                  "replacement" = "$1:$2"
+
+                  "source_labels" = ["__address__", "__meta_kubernetes_service_annotation_prometheus_io_port"]
+
+                  "target_label" = "__address__"
+                },
+                {
+                  "action" = "labelmap"
+
+                  "regex" = "__meta_kubernetes_service_annotation_prometheus_io_param_(.+)"
+
+                  "replacement" = "__param_$1"
+                },
+                {
+                  "action" = "labelmap"
+
+                  "regex" = "__meta_kubernetes_service_label_(.+)"
+                },
+                {
+                  "action" = "replace"
+
+                  "source_labels" = ["__meta_kubernetes_namespace"]
+
+                  "target_label" = "namespace"
+                },
+                {
+                  "action" = "replace"
+
+                  "source_labels" = ["__meta_kubernetes_service_name"]
+
+                  "target_label" = "service"
+                },
+                {
+                  "action" = "replace"
+
+                  "source_labels" = ["__meta_kubernetes_pod_node_name"]
+
+                  "target_label" = "node"
+                }
+              ]
+            }
+          ]
         }
       }
 
@@ -49,6 +156,20 @@ resource "helm_release" "prometheus-grafana" {
       grafana = {
         nodeSelector = {
           "${var.node-group.key}" = var.node-group.value
+        }
+
+        sidecar = {
+          dashboards = {
+            annotations = {
+              "dashboard/subdirectory" = "Supplementary"
+            }
+            provider = {
+              foldersFromFilesStructure : true
+            }
+            # If specified, the sidecar will look for annotation with this name to create folder and put graph here.
+            # You can use this parameter together with `provider.foldersFromFilesStructure`to annotate configmaps and create folder structure.
+            folderAnnotation : "dashboard/subdirectory"
+          }
         }
 
         "grafana.ini" : {
@@ -102,18 +223,86 @@ module "grafana-client-id" {
 
 
 resource "kubernetes_config_map" "dashboard" {
+  for_each = var.dashboards
   metadata {
-    name      = "nebari-grafana-dashboards"
+    name      = "nebari-grafana-dashboards-${lower(each.value)}"
     namespace = var.namespace
     labels = {
       # grafana_dashboard label needed for grafana to pick it up
       # automatically
       grafana_dashboard = "1"
     }
+    annotations = {
+      "dashboard/subdirectory" = "${each.value}"
+    }
   }
 
   data = {
-    for dashboard in var.dashboards : dashboard => file("${path.module}/dashboards/${dashboard}")
+    for dashboard_file in fileset("${path.module}/dashboards/${each.value}", "*.json") :
+    dashboard_file => file("${path.module}/dashboards/${each.value}/${dashboard_file}")
+  }
+}
+
+resource "kubernetes_manifest" "keycloak-service-monitor" {
+  manifest = {
+    "apiVersion" : "monitoring.coreos.com/v1",
+    "kind" : "ServiceMonitor",
+    "metadata" : {
+      "annotations" : null,
+      "labels" : {
+        "release" : "nebari"
+      },
+      "name" : "keycloak-sm",
+      "namespace" : "dev"
+    },
+    "spec" : {
+      "endpoints" : [
+        {
+          "interval" : "10s",
+          "path" : "/auth/realms/master/metrics",
+          "port" : "http",
+          "scrapeTimeout" : "10s"
+        }
+      ],
+      "selector" : {
+        "matchLabels" : {
+          "app.kubernetes.io/component" : "http",
+          "app.kubernetes.io/instance" : "keycloak",
+          "app.kubernetes.io/name" : "keycloak"
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_manifest" "conda-store-service-monitor" {
+  manifest = {
+    "apiVersion" : "monitoring.coreos.com/v1",
+    "kind" : "ServiceMonitor",
+    "metadata" : {
+      "annotations" : null,
+      "labels" : {
+        "release" : "nebari"
+      },
+      "name" : "conda-store-sm",
+      "namespace" : "dev"
+    },
+    "spec" : {
+      "endpoints" : [
+        {
+          "interval" : "10s",
+          "path" : "/conda-store/metrics",
+          "port" : "conda-store-server",
+          "scrapeTimeout" : "10s"
+        }
+      ],
+      "selector" : {
+        "matchLabels" : {
+          app       = "conda-store"
+          component = "conda-store-server"
+        }
+      }
+    }
   }
 }
 
