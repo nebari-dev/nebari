e0876513c26c1ffaf46182524adacd3d4c0aebb2
iameskild <eskild.eriksen122@gmail.com>
2023-05-01 18:08:39 -0400

    Add wait for cypress check

--- tests_e2e/cypress/integration/main.js
+++ tests_e2e/cypress/integration/main.js
@@ -56,10 +56,10 @@ describe('First Test', () => {
         .should('have.attr', 'value', 'Start').click();
 
       // Minimal check that JupyterLab has opened
-      cy.get('div#jp-MainLogo', { timeout: 60000 }).should('exist').wait(2000);
+      cy.get('div#jp-MainLogo', { timeout: 60000 }).should('exist').wait(4000);
 
       // Click VS Code Launcher exists
-      cy.get('div.jp-LauncherCard[title="VS Code [↗]"]').should('exist').wait(2000);
+      cy.get('div.jp-LauncherCard[title="VS Code [↗]"]').should('exist');
 
       // Stop my Jupyter server - must do this so PVC can be destroyed on Minikube
       cy.visit('/hub/home');
