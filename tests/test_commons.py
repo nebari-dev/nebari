78df515d18a77b00df08995382d3c621eca8978b
eskild <42120229+iameskild@users.noreply.github.com>
2023-05-04 08:46:37 -0400

    Upgrade to k8s 1.24 (#1760)
    Co-authored-by: Adam Lewis <23342526+Adam-D-Lewis@users.noreply.github.com>

--- tests/test_commons.py
+++ tests/test_commons.py
@@ -2,6 +2,8 @@ from nebari.provider.cloud.commons import filter_by_highest_supported_k8s_versio
 
 
 def test_filter_by_highest_supported_k8s_version():
+    version_to_filter = "99.99"
+
     k8s_versions = [
         "1.21.7",
         "1.21.9",
@@ -10,7 +12,8 @@ def test_filter_by_highest_supported_k8s_version():
         "1.23.3",
         "1.23.5",
         "1.24.0",
+        version_to_filter,
     ]
     actual = filter_by_highest_supported_k8s_version(k8s_versions)
-    expected = sorted(list(set(k8s_versions) - {"1.24.0"}))
+    expected = sorted(list(set(k8s_versions) - {version_to_filter}))
     assert actual == expected
