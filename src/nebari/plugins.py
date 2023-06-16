2fa3839017bff6ecd608f9191a25910d9967b7d8
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2023-06-16 16:28:49 -0400

    Ensure that plugy extensions can be loaded via entrypoints

--- src/nebari/plugins.py
+++ src/nebari/plugins.py
@@ -33,7 +33,7 @@ pm.add_hookspecs(hookspecs)
 
 if not hasattr(sys, "_called_from_test"):
     # Only load plugins if not running tests
-    pm.load_setuptools_entrypoints("datasette")
+    pm.load_setuptools_entrypoints("nebari")
 
 # Load default plugins
 for plugin in DEFAULT_PLUGINS:
