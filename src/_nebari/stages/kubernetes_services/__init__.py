f3826e5e07d2a105c85db90faf9e6fa14038adee
eskild <42120229+iameskild@users.noreply.github.com>
2023-06-20 17:55:19 +0200

    Add InputVar schema to kubernetes_services stage (#1837)

--- src/_nebari/stages/kubernetes_services/__init__.py
+++ src/_nebari/stages/kubernetes_services/__init__.py
@@ -3,6 +3,8 @@ import sys
 from typing import Any, Dict, List
 from urllib.parse import urlencode
 
+from pydantic import Field
+
 from _nebari.stages.base import NebariTerraformStage
 from _nebari.stages.tf_objects import (
     NebariHelmProvider,
@@ -15,6 +17,87 @@ from nebari.hookspecs import NebariStage, hookimpl
 # check and retry settings
 NUM_ATTEMPTS = 10
 TIMEOUT = 10  # seconds
+
+
+# variables shared by multiple services
+class KubernetesServicesInputVars(schema.Base):
+    name: str
+    environment: str
+    endpoint: str
+    realm_id: str
+    node_groups: Dict[str, Dict[str, str]]
+    conda_store_default_namespace: str
+    jupyterhub_logout_redirect_url: str = Field(alias="jupyterhub-logout-redirect-url")
+
+
+class CondaStoreInputVars(schema.Base):
+    conda_store_environments: Dict[str, Dict[str, Any]] = Field(
+        alias="conda-store-environments"
+    )
+    conda_store_filesystem_storage: Dict[str, Any] = Field(
+        alias="conda-store-filesystem-storage"
+    )
+    conda_store_object_storage: Dict[str, Any] = Field(
+        alias="conda-store-object-storage"
+    )
+    conda_store_extra_settings: Dict[str, Any] = Field(
+        alias="conda-store-extra-settings"
+    )
+    conda_store_extra_config: Dict[str, Any] = Field(alias="conda-store-extra-config")
+    conda_store_image = Dict[str, Any] = Field(alias="conda-store-image")
+    conda_store_image_tag: str = Field(alias="conda-store-image-tag")
+    conda_store_service_token_scopes: Dict[str, Dict[str, Any]] = Field(
+        alias="conda-store-service-token-scopes"
+    )
+
+
+class JupyterhubInputVars(schema.Base):
+    cdsdashboards: Dict[str, Any]
+    jupyterhub_theme: Dict[str, Any] = Field(alias="jupyterhub-theme")
+    jupyterlab_image: Dict[str, Any] = Field(alias="jupyterlab-image")
+    jupyterhub_overrides: List[str] = Field(alias="jupyterhub-overrides")
+    jupyterhub_stared_storage: Dict[str, Any] = Field(alias="jupyterhub-shared-storage")
+    jupyterhub_shared_endpoint: str = Field(alias="jupyterhub-shared-endpoint")
+    jupyterhub_profiles = Dict[str, Any] = Field(alias="jupyterlab-profiles")
+    jupyterhub_image: Dict[str, Any] = Field(alias="jupyterhub-image")
+    jupyterhub_hub_extraEnv: str = Field(alias="jupyterhub-hub-extraEnv")
+    idle_culler_settings: Dict[str, Any] = Field(alias="idle-culler-settings")
+
+
+class DaskGatewayInputVars(schema.Base):
+    dask_worker_image: Dict[str, Any] = Field(alias="dask-worker-image")
+    dask_gateway_profiles: Dict[str, Any] = Field(alias="dask-gateway-profiles")
+
+
+class MonitoringInputVars(schema.Base):
+    monitoring_enabled: bool = Field(alias="monitoring-enabled")
+
+
+class ArgoWorkflowsInputVars(schema.Base):
+    argo_workflows_enabled: bool = Field(alias="argo-workflows-enabled")
+    argo_workflows_overrides: List[str] = Field(alias="argo-workflows-overrides")
+    nebari_workflow_controller: bool = Field(alias="nebari-workflow-controller")
+    workflow_controller_image_tag: str = Field(alias="workflow-controller-image-tag")
+    keycloak_read_only_user_credentials: Dict[str, Any] = Field(
+        alias="keycloak-read-only-user-credentials"
+    )
+
+
+class KBatchInputVars(schema.Base):
+    kbatch_enabled: bool = Field(alias="kbatch-enabled")
+
+
+class PrefectInputVars(schema.Base):
+    prefect_enabled: bool = Field(alias="prefect-enabled")
+    prefect_token: str = Field(alias="prefect-token")
+    prefect_image: str = Field(alias="prefect-image")
+    prefect_overrides: List[str] = Field(alias="prefect-overrides")
+
+
+class ClearMLInputVars(schema.Base):
+    clearml_enabled: bool = Field(alias="clearml-enabled")
+    clearml_enable_forwardauth: bool = Field(alias="clearml-enable-forwardauth")
+    clearml_overrides: List[str] = Field(alias="clearml-overrides")
 
 
 def _split_docker_image_name(image_name):
@@ -63,6 +146,33 @@ class KubernetesServicesStage(NebariTerraformStage):
     def input_vars(self, stage_outputs: Dict[str, Dict[str, Any]]):
         final_logout_uri = f"https://{self.config.domain}/hub/login"
 
+        realm_id = stage_outputs["stages/06-kubernetes-keycloak-configuration"][
+            "realm_id"
+        ]["value"]
+        jupyterhub_shared_endpoint = (
+            stage_outputs["stages/02-infrastructure"]
+            .get("nfs_endpoint", {})
+            .get("value")
+        )
+        keycloak_read_only_user_credentials = stage_outputs[
+            "stages/06-kubernetes-keycloak-configuration"
+        ]["keycloak-read-only-user-credentials"]["value"]
+
+        conda_store_token_scopes = {
+            "cdsdashboards": {
+                "primary_namespace": "cdsdashboards",
+                "role_bindings": {
+                    "*/*": ["viewer"],
+                },
+            },
+            "dask-gateway": {
+                "primary_namespace": "",
+                "role_bindings": {
+                    "*/*": ["viewer"],
+                },
+            },
+        }
+
         # Compound any logout URLs from extensions so they are are logged out in succession
         # when Keycloak and JupyterHub are logged out
         for ext in self.config.tf_extensions:
@@ -78,86 +188,93 @@ class KubernetesServicesStage(NebariTerraformStage):
         ):
             jupyterhub_theme.update({"version": f"v{self.config.nebari_version}"})
 
-        return {
-            "name": self.config.project_name,
-            "environment": self.config.namespace,
-            "endpoint": self.config.domain,
-            "realm_id": stage_outputs["stages/06-kubernetes-keycloak-configuration"][
-                "realm_id"
-            ]["value"],
-            "node_groups": _calculate_node_groups(self.config),
-            # conda-store
-            "conda-store-environments": {
+        kubernetes_services_vars = KubernetesServicesInputVars(
+            name=self.config.project_name,
+            environment=self.config.namespace,
+            endpoint=self.config.domain,
+            realm_id=realm_id,
+            node_groups=_calculate_node_groups(self.config),
+            conda_store_default_namespace=self.config.conda_store.default_namespace,
+        )
+
+        conda_store_vars = CondaStoreInputVars(
+            conda_store_environments={
                 k: v.dict() for k, v in self.config.environments.items()
             },
-            "conda-store-filesystem-storage": self.config.storage.conda_store,
-            "conda-store-service-token-scopes": {
-                "cdsdashboards": {
-                    "primary_namespace": "cdsdashboards",
-                    "role_bindings": {
-                        "*/*": ["viewer"],
-                    },
-                },
-                "dask-gateway": {
-                    "primary_namespace": "",
-                    "role_bindings": {
-                        "*/*": ["viewer"],
-                    },
-                },
-            },
-            "conda-store-default-namespace": self.config.conda_store.default_namespace,
-            "conda-store-extra-settings": self.config.conda_store.extra_settings,
-            "conda-store-extra-config": self.config.conda_store.extra_config,
-            "conda-store-image-tag": self.config.conda_store.image_tag,
-            # jupyterhub
-            "cdsdashboards": self.config.cdsdashboards.dict(),
-            "jupyterhub-theme": self.config.theme.jupyterhub.dict(),
-            "jupyterhub-image": _split_docker_image_name(
-                self.config.default_images.jupyterhub
-            ),
-            "jupyterhub-shared-storage": self.config.storage.shared_filesystem,
-            "jupyterhub-shared-endpoint": stage_outputs["stages/02-infrastructure"]
-            .get("nfs_endpoint", {})
-            .get("value"),
-            "jupyterlab-profiles": self.config.profiles.dict()["jupyterlab"],
-            "jupyterlab-image": _split_docker_image_name(
+            conda_store_filesystem_storage=self.config.storage.conda_store,
+            conda_store_object_storage=self.config.conda_store.object_storage,
+            conda_store_service_token_scopes=conda_store_token_scopes,
+            conda_store_extra_settings=self.config.conda_store.extra_settings,
+            conda_store_extra_config=self.config.conda_store.extra_config,
+            conda_store_image=self.config.conda_store.image,
+            conda_store_image_tag=self.config.conda_store.image_tag,
+        )
+
+        jupyterhub_vars = JupyterhubInputVars(
+            cdsdashboards=self.config.cdsdashboards.dict(),
+            jupyterhub_theme=jupyterhub_theme.dict(),
+            jupyterlab_image=_split_docker_image_name(
                 self.config.default_images.jupyterlab
             ),
-            "jupyterhub-overrides": [json.dumps(self.config.jupyterhub.overrides)],
-            "jupyterhub-hub-extraEnv": json.dumps(
+            jupyterhub_stared_storage=self.config.storage.shared_filesystem,
+            jupyterhub_shared_endpoint=jupyterhub_shared_endpoint,
+            jupyterhub_profiles=self.config.profiles.dict()["jupyterlab"],
+            jupyterhub_image=_split_docker_image_name(
+                self.config.default_images.jupyterhub
+            ),
+            jupyterhub_overrides=[json.dumps(self.config.jupyterhub.overrides)],
+            jupyterhub_hub_extraEnv=json.dumps(
                 self.config.jupyterhub.overrides.get("hub", {}).get("extraEnv", [])
             ),
-            # jupyterlab
-            "idle-culler-settings": self.config.jupyterlab.idle_culler.dict(),
-            # dask-gateway
-            "dask-worker-image": _split_docker_image_name(
+            idle_culler_settings=self.config.jupyterlab.idle_culler.dict(),
+        )
+
+        dask_gateway_vars = DaskGatewayInputVars(
+            dask_worker_image=_split_docker_image_name(
                 self.config.default_images.dask_worker
             ),
-            "dask-gateway-profiles": self.config.profiles.dict()["dask_worker"],
-            # monitoring
-            "monitoring-enabled": self.config.monitoring.enabled,
-            # argo-worfklows
-            "argo-workflows-enabled": self.config.argo_workflows.enabled,
-            "argo-workflows-overrides": [
-                json.dumps(self.config.argo_workflows.overrides)
-            ],
-            "nebari-workflow-controller": self.config.argo_workflows.nebari_workflow_controller.enabled,
-            "keycloak-read-only-user-credentials": stage_outputs[
-                "stages/06-kubernetes-keycloak-configuration"
-            ]["keycloak-read-only-user-credentials"]["value"],
-            "workflow-controller-image-tag": self.config.argo_workflows.nebari_workflow_controller.image_tag,
-            # kbatch
-            "kbatch-enabled": self.config.kbatch.enabled,
-            # prefect
-            "prefect-enabled": self.config.prefect.enabled,
-            "prefect-token": self.config.prefect.token,
-            "prefect-image": self.config.prefect.image,
-            "prefect-overrides": self.config.prefect.overrides,
-            # clearml
-            "clearml-enabled": self.config.clearml.enabled,
-            "clearml-enable-forwardauth": self.config.clearml.enable_forward_auth,
-            "clearml-overrides": [json.dumps(self.config.clearml.overrides)],
-            "jupyterhub-logout-redirect-url": final_logout_uri,
+            dask_gateway_profiles=self.config.profiles.dict()["dask_worker"],
+        )
+
+        monitoring_vars = MonitoringInputVars(
+            enabled=self.config.monitoring.enabled,
+        )
+
+        argo_workflows_vars = ArgoWorkflowsInputVars(
+            argo_workflows_enabled=self.config.argo_workflows.enabled,
+            argo_workflows_overrides=[json.dumps(self.config.argo_workflows.overrides)],
+            nebari_workflow_controller=self.config.argo_workflows.nebari_workflow_controller.enabled,
+            workflow_controller_image_tag=self.config.argo_workflows.nebari_workflow_controller.image_tag,
+            keycloak_read_only_user_credentials=keycloak_read_only_user_credentials,
+        )
+
+        kbatch_vars = KBatchInputVars(
+            kbatch_enabled=self.config.kbatch.enabled,
+        )
+
+        prefect_vars = PrefectInputVars(
+            prefect_enabled=self.config.prefect.enabled,
+            prefect_token=self.config.prefect.token,
+            prefect_image=self.config.prefect.image,
+            prefect_overrides=[json.dumps(self.config.prefect.overrides)],
+        )
+
+        clearml_vars = ClearMLInputVars(
+            clearml_enabled=self.config.clearml.enabled,
+            clearml_enable_forwardauth=self.config.clearml.enable_forward_auth,
+            clearml_overrides=[json.dumps(self.config.clearml.overrides)],
+        )
+
+        return {
+            **kubernetes_services_vars.dict(),
+            **conda_store_vars.dict(),
+            **jupyterhub_vars.dict(),
+            **dask_gateway_vars.dict(),
+            **monitoring_vars.dict(),
+            **argo_workflows_vars.dict(),
+            **kbatch_vars.dict(),
+            **prefect_vars.dict(),
+            **clearml_vars.dict(),
         }
 
     def check(self, stage_outputs: Dict[str, Dict[str, Any]]):
--- src/_nebari/stages/kubernetes_services/template/conda-store.tf
+++ src/_nebari/stages/kubernetes_services/template/conda-store.tf
@@ -1,7 +1,6 @@
 # ======================= VARIABLES ======================
 variable "conda-store-environments" {
   description = "Conda-Store managed environments"
-  default     = {}
 }
 
 variable "conda-store-filesystem-storage" {
@@ -12,31 +11,31 @@ variable "conda-store-filesystem-storage" {
 variable "conda-store-object-storage" {
   description = "Conda-Store storage in GB for object storage. Conda-Store uses minio for object storage to be cloud agnostic. If empty default is var.conda-store-filesystem-storage value"
   type        = string
-  default     = null
 }
 
 variable "conda-store-extra-settings" {
   description = "Conda-Store extra traitlet settings to apply in `c.Class.key = value` form"
   type        = map(any)
-  default     = {}
 }
 
 variable "conda-store-extra-config" {
   description = "Additional traitlets configuration code to be ran"
   type        = string
-  default     = ""
 }
 
 variable "conda-store-image" {
   description = "Conda-Store image"
   type        = string
-  default     = "quansight/conda-store-server"
 }
 
 variable "conda-store-image-tag" {
   description = "Version of conda-store to use"
   type        = string
-  default     = "v0.4.14"
+}
+
+variable "conda-store-service-token-scopes" {
+  description = "Map of services tokens and scopes for conda-store"
+  type        = map(any)
 }
 
 # ====================== RESOURCES =======================
