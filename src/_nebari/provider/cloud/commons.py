c648e507e7be8a6c2a684f19ad28ae7bcfbe6480
iameskild <eskild.eriksen122@gmail.com>
2023-06-23 14:27:30 +0200

    Fix test-provider CI

--- src/_nebari/provider/cloud/commons.py
+++ src/_nebari/provider/cloud/commons.py
@@ -1,7 +1,7 @@
 from _nebari.constants import HIGHEST_SUPPORTED_K8S_VERSION
 
 
-def filter_by_highest_supported_k8s_version(k8s_versions_list: list[str]) -> list[str]:
+def filter_by_highest_supported_k8s_version(k8s_versions_list):
     filtered_k8s_versions_list = []
     for k8s_version in k8s_versions_list:
         if k8s_version.split("-")[0] <= HIGHEST_SUPPORTED_K8S_VERSION:
