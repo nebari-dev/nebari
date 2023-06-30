39c5c3c522cd3aa74ab2fadb14ff5a84bb831577
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2023-06-30 00:18:06 -0400

    Making docs links in constants module

--- src/_nebari/constants.py
+++ src/_nebari/constants.py
@@ -15,3 +15,10 @@ DEFAULT_NEBARI_WORKFLOW_CONTROLLER_IMAGE_TAG = CURRENT_RELEASE
 DEFAULT_CONDA_STORE_IMAGE_TAG = "v0.4.14"
 
 LATEST_SUPPORTED_PYTHON_VERSION = "3.10"
+
+
+# DOCS
+DO_ENV_DOCS = "https://www.nebari.dev/docs/how-tos/nebari-do"
+AZURE_ENV_DOCS = "https://www.nebari.dev/docs/how-tos/nebari-azure"
+AWS_ENV_DOCS = "https://www.nebari.dev/docs/how-tos/nebari-aws"
+GCP_ENV_DOCS = "https://www.nebari.dev/docs/how-tos/nebari-gcp"
