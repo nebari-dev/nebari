79e6c8b8289bfe1261e7e68cf7d1f4ed1e865b6e
Amit Kumar <dtu.amit@gmail.com>
2023-07-20 19:31:28 +0100

    add a trivial keycloak test

--- tests_integration/deployment_fixtures.py
+++ tests_integration/deployment_fixtures.py
@@ -1,23 +1,31 @@
 import logging
 import pytest
 import os
+import warnings
+
 from pathlib import Path
-from urllib3.exceptions import InsecureRequestWarning
 
 import yaml
 
 from _nebari.deploy import deploy_configuration
 from _nebari.destroy import destroy_configuration
 from _nebari.render import render_template
+from urllib3.exceptions import InsecureRequestWarning
 from tests.utils import render_config_partial
 
 import random
 import string
-import warnings
 
 DEPLOYMENT_DIR = '_test_deploy'
 
 logger = logging.getLogger(__name__)
+
+
+def ignore_warnings():
+    # Ignore this for now, as test is failing due to a
+    # DeprecationWarning and InsecureRequestWarning
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
+    warnings.filterwarnings("ignore", category=InsecureRequestWarning)
 
 
 def random_letters(length=5):
@@ -37,15 +45,10 @@ def get_or_create_deployment_directory(cloud):
     return deployment_dir
 
 
-@pytest.fixture
-def deploy(
-        request,
-):
-    # Ignore this for now, as test is failing due to a
-    # DeprecationWarning
+@pytest.fixture(scope="session")
+def deploy(request):
+    ignore_warnings()
     cloud = request.param
-    warnings.filterwarnings("ignore", category=DeprecationWarning)
-    warnings.filterwarnings("ignore", category=InsecureRequestWarning)
     deployment_dir = get_or_create_deployment_directory(cloud)
     config = render_config_partial(
         project_name=deployment_dir.name,
@@ -57,12 +60,12 @@ def deploy(
     )
     deployment_dir_abs = deployment_dir.absolute()
     os.chdir(deployment_dir)
-    print(f"Temporary directory: {deployment_dir}")
+    logger.info(f"Temporary directory: {deployment_dir}")
     with open(Path("nebari-config.yaml"), "w") as f:
         yaml.dump(config, f)
     render_template(deployment_dir_abs, Path("nebari-config.yaml"))
     try:
-        return deploy_configuration(
+        yield deploy_configuration(
             config=config,
             dns_provider="cloudflare",
             dns_auto_provision=True,
@@ -71,15 +74,17 @@ def deploy(
             skip_remote_state_provision=False,
         )
     except Exception as e:
-        print(f"Deploy Failed, Exception: {e}")
+        logger.info(f"Deploy Failed, Exception: {e}")
         logger.exception(e)
         raise
+    logger.info("Teardown")
+    return destroy(cloud)
 
 
 def destroy(cloud):
     deployment_dirs = list(Path(Path(DEPLOYMENT_DIR) / cloud).glob(f"{cloud}*"))
     if not deployment_dirs:
-        print("Configuration not found")
+        logger.info("Configuration not found")
         destroy_configuration(deployment_dirs[0] / Path("nebari-config.yaml"))
 
 
