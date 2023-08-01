e810919bc6442b0f5f8a7f1ab0e8e133d98cc97c
Amit Kumar <dtu.amit@gmail.com>
2023-08-01 22:56:16 +0100

    cleanup test data

--- tests/common/config_mod_utils.py
+++ tests/common/config_mod_utils.py
@@ -1,4 +1,55 @@
+import dataclasses
+import typing
+
 PREEMPTIBLE_NODE_GROUP_NAME = "preemptible-node-group"
+
+
+@dataclasses.dataclass
+class GPUConfig:
+    cloud: str
+    gpu_name: str
+    node_selector: str
+    node_selector_val: str
+    extra_config: dict
+    min_nodes: typing.Optional[int] = 0
+    max_nodes: typing.Optional[int] = 2
+    node_group_name: typing.Optional[str] = "gpu-node"
+    docker_image: typing.Optional[str] = "quay.io/nebari/nebari-jupyterlab-gpu:2023.7.1"
+
+    def node(self):
+        return {
+            "instance": self.gpu_name,
+            "min_nodes": self.min_nodes,
+            "max_nodes": self.max_nodes,
+            **self.extra_config,
+        }
+
+
+AWS_GPU_CONF = GPUConfig(
+    cloud="amazon_web_services",
+    gpu_name="g4dn.xlarge",
+    node_selector="beta.kubernetes.io/instance-type",
+    node_selector_val="g4dn.xlarge",
+    extra_config={
+        "single_subnet": False,
+        "gpu": True,
+    },
+)
+
+
+GCP_GPU_CONF = GPUConfig(
+    cloud="google_cloud_platform",
+    gpu_name="n1-standard-16",
+    node_selector="cloud.google.com/gke-nodepool",
+    node_selector_val="g4dn.xlarge",
+    extra_config={"guest_accelerators": [{"name": "nvidia-tesla-t4", "count": 1}]},
+)
+
+
+GPU_CONFIG = {
+    "aws": AWS_GPU_CONF,
+    "gcp": GCP_GPU_CONF,
+}
 
 
 def _create_gpu_environment():
@@ -20,27 +71,12 @@ def _create_gpu_environment():
 
 def add_gpu_config(config, cloud="aws"):
 
-    gpu_node_group = "gpu-node"
-    if cloud == "aws":
-        cloud_name = "amazon_web_services"
-        gpu_name = "g4dn.xlarge"
-        node_selector = "beta.kubernetes.io/instance-type"
-        extra_config = {
-            "single_subnet": False,
-            "gpu": True,
-        }
-        node_selector_val = "g4dn.xlarge"
-    elif cloud == "gcp":
-        cloud_name = "google_cloud_platform"
-        gpu_name = "n1-standard-16"
-        node_selector = "cloud.google.com/gke-nodepool"
-        extra_config = {"guest_accelerators": [{"name": "nvidia-tesla-t4", "count": 1}]}
-        node_selector_val = gpu_node_group
-    else:
+    gpu_config = GPU_CONFIG.get(cloud)
+    if not gpu_config:
         raise ValueError(f"GPU not supported/tested on {cloud}")
 
-    gpu_node = {"instance": gpu_name, "min_nodes": 0, "max_nodes": 4, **extra_config}
-    gpu_docker_image = "quay.io/nebari/nebari-jupyterlab-gpu:2023.7.1"
+    gpu_node = gpu_config.node()
+    gpu_docker_image = gpu_config.docker_image
     jupyterlab_profile = {
         "display_name": "GPU Instance",
         "description": "4 CPU / 16GB RAM / 1 NVIDIA T4 GPU (16 GB GPU RAM)",
@@ -53,13 +89,12 @@ def add_gpu_config(config, cloud="aws"):
             "mem_guarantee": "10G",
             "extra_resource_limits": {"nvidia.com/gpu": 1},
             "node_selector": {
-                node_selector: node_selector_val,
+                gpu_config.node_selector: gpu_config.node_selector_val,
             },
         },
     }
-    config[cloud_name]["node_groups"][gpu_node_group] = gpu_node
+    config[gpu_config.cloud]["node_groups"][gpu_config.node_group_name] = gpu_node
     config["profiles"]["jupyterlab"].append(jupyterlab_profile)
-
     config["environments"]["environment-gpu.yaml"] = _create_gpu_environment()
     return config
 
