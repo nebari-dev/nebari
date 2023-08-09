5577d373f0a55b0f7a67b9479b3234e58c24216f
iameskild <eskild.eriksen122@gmail.com>
2023-08-09 10:39:19 -0700

    fix jhub_ssh test

--- tests/tests_deployment/test_jupyterhub_ssh.py
+++ tests/tests_deployment/test_jupyterhub_ssh.py
@@ -4,13 +4,10 @@ import uuid
 
 import paramiko
 import pytest
+from tests_deployment import constants
+from tests_deployment.utils import get_jupyterhub_token, monkeypatch_ssl_context
 
-from tests.tests_deployment import constants
-from tests.tests_deployment.utils import (
-    escape_string,
-    get_jupyterhub_token,
-    monkeypatch_ssl_context,
-)
+from _nebari.utils import escape_string
 
 monkeypatch_ssl_context()
 
