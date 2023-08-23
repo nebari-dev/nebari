21c30e4ba1b40c74c9083c79f26549f587acc9dc
Scott Blair <sblair@metrostar.com>
2023-08-23 16:47:10 +0000

    additional init tests

--- tests/tests_unit/test_cli_init.py
+++ tests/tests_unit/test_cli_init.py
@@ -4,89 +4,129 @@ import tempfile
 import yaml
 
 from collections.abc import MutableMapping
-from _nebari.cli import create_cli
 from typer import Typer
 from typer.testing import CliRunner
-
 from typing import List
+
+from _nebari.cli import create_cli
+from _nebari.provider.cloud import amazon_web_services, azure_cloud, digital_ocean, google_cloud
 
 runner = CliRunner()
 
-MOCK_ENV = [
+MOCK_KUBERNETES_VERSIONS = ["1.24"]
+MOCK_ENV = {k: "test" for k in [
     "AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY", # aws
     "GOOGLE_CREDENTIALS", "PROJECT_ID", # gcp
     "ARM_SUBSCRIPTION_ID", "ARM_TENANT_ID", "ARM_CLIENT_ID", "ARM_CLIENT_SECRET", # azure
     "DIGITALOCEAN_TOKEN", "SPACES_ACCESS_KEY_ID", "SPACES_SECRET_ACCESS_KEY", # digital ocean
     "GITHUB_CLIENT_ID", "GITHUB_CLIENT_SECRET", "GITHUB_USERNAME", "GITHUB_TOKEN", # github
     "AUTH0_CLIENT_ID", "AUTH0_CLIENT_SECRET", "AUTH0_DOMAIN", # auth0
-]
+]}
 
-@pytest.mark.parametrize('args, exit_code, content', [
+@pytest.mark.parametrize("args, exit_code, content", [
     # --help
-    (['--help'], 0, ['init']),
-    (['-h'], 0, ['init']),
-    (['init', '--help'], 0, ['Create and initialize your nebari-config.yaml file']),
-    (['init', '-h'], 0, ['Create and initialize your nebari-config.yaml file']),
+    (["--help"], 0, ["Create and initialize your nebari-config.yaml file"]),
+    (["-h"], 0, ["Create and initialize your nebari-config.yaml file"]),
 
     # error, missing args
-    (['init'], 2, ['Missing option']),
-    (['init', '--no-guided-init'], 2, ['Missing option']),
-    (['init', '--project-name'], 2, ['requires an argument']),
-    (['init', '--project'], 2, ['requires an argument']),
-    (['init', '-p'], 2, ['requires an argument']),
-    (['init', '--domain-name'], 2, ['requires an argument']),
-    (['init', '--domain'], 2, ['requires an argument']),
-    (['init', '--namespace'], 2, ['requires an argument']),
-    (['init', '--auth-provider'], 2, ['requires an argument']),
-    #(['init', '--auth-auto-provision'], 2, ['requires an argument']),
-    #(['init', '--no-auth-auto-provision'], 2, ['requires an argument']),
-    (['init', '--repository'], 2, ['requires an argument']),
-    #(['init', '--repository-auto-provision'], 2, ['requires an argument']),
-    #(['init', '--no-repository-auto-provision'], 2, ['requires an argument']),
-    (['init', '--ci-provider'], 2, ['requires an argument']),
-    (['init', '--terraform-state'], 2, ['requires an argument']),
-    (['init', '--kubernetes-version'], 2, ['requires an argument']),
-    (['init', '--ssl-cert-email'], 2, ['requires an argument']),
-    # (['init', '--disable-prompt'], 2, ['requires an argument']),
-    # (['init', '--no-disable-prompt'], 2, ['requires an argument']),
-    (['init', '--output'], 2, ['requires an argument']),
-    (['init', '-o'], 2, ['requires an argument']),
+    ([], 2, ["Missing option"]),
+    (["--no-guided-init"], 2, ["Missing option"]),
+    (["--project-name"], 2, ["requires an argument"]),
+    (["--project"], 2, ["requires an argument"]),
+    (["-p"], 2, ["requires an argument"]),
+    (["--domain-name"], 2, ["requires an argument"]),
+    (["--domain"], 2, ["requires an argument"]),
+    (["--namespace"], 2, ["requires an argument"]),
+    (["--auth-provider"], 2, ["requires an argument"]),
+    (["--repository"], 2, ["requires an argument"]),
+    (["--ci-provider"], 2, ["requires an argument"]),
+    (["--terraform-state"], 2, ["requires an argument"]),
+    (["--kubernetes-version"], 2, ["requires an argument"]),
+    (["--ssl-cert-email"], 2, ["requires an argument"]),
+    (["--output"], 2, ["requires an argument"]),
+    (["-o"], 2, ["requires an argument"]),
 ])
 def test_init_stdout(args: List[str], exit_code: int, content: List[str]):
     app = create_cli()
-    result = runner.invoke(app, args)
+    result = runner.invoke(app, ["init"] + args)
     assert result.exit_code == exit_code
     for c in content:
         assert c in result.stdout
 
 
+###
+# NOTES:
+# - --output doesn't appear to work with --guided-init, always hard codes to ./nebari-config.yaml
+###
 # def test_init_guided_init():
 #     app = create_cli()
-#     result = runner.invoke(app, ['init', '--guided-init'], input = "\ntestprojectname\ntestprojectname.example.com\n\nN\ny\nnotreal@example.com\nn\n")
-#     assert result.exit_code == 0
 
-def test_all_init_no_guided_happy(monkeypatch, provider: str, project_name: str, domain_name: str, namespace: str, auth_provider: str, ci_provider: str, terraform_state: str, email: str):
-    mock_cloud_provider_env(monkeypatch)
+#     expected_yaml = f"""
+#     provider: local
+#     namespace: dev
+#     project_name: testproject
+#     domain: testproject.example.com
+#     certificate:
+#         type: lets-encrypt
+#         acme_email: noreply@example.com
+#     """
+
+#     # provider (arrow), project_name, domain, auth_provider (arrow), ci_cd (y/n), lets encrypt (y/n), email (if yes to lets encrypt, skip otherwise), advanced (y/n)
+#     input = ["", "testproject", "testproject.example.com", "", "n", "n", "n"]
+#     assert_nebari_init_args(app, ["init", "--guided-init"], expected_yaml, input = "\n".join(input) + "\n")
+
+def generate_test_data_test_all_init_happy_path():
+    """
+    Generate inputs to test_all_init_happy_path representing all valid combinations of options
+    available to nebari init
+    """
+
+    test_data = []
+    for provider in ["local", "aws", "azure", "gcp", "do", "existing"]:
+        for project_name in ["testproject"]:
+            for domain_name in [f"{project_name}.example.com"]:
+                for namespace in ["test-ns"]:
+                    for auth_provider in ["password"]: # ["password", "Auth0", "GitHub", "custom"] # Auth0, Github and custom failing as of 2023-08-23
+                        for repository in ["github.com", "gitlab.com"]:
+                            for ci_provider in ["none", "github-actions", "gitlab-ci"]:
+                                for terraform_state in ["local", "remote", "existing"]:
+                                    for email in ["noreply@example.com"]:
+                                        for kubernetes_version in MOCK_KUBERNETES_VERSIONS + ["latest"]:
+                                            test_data.append((provider, project_name, domain_name, namespace, auth_provider, repository, ci_provider, terraform_state, email, kubernetes_version))
+
+    keys = ["provider", "project_name", "domain_name", "namespace", "auth_provider", "repository", "ci_provider", "terraform_state", "email", "kubernetes_version"]
+    return { "keys": keys, "test_data": test_data }
+
+def test_all_init_happy_path(monkeypatch, provider: str, project_name: str, domain_name: str, namespace: str, auth_provider: str, repository: str, ci_provider: str, terraform_state: str, email: str, kubernetes_version: str):
+    # the kubernetes-version parameter can trigger calls out to AWS, Azure, etc... to validate, mocking
+    monkeypatch.setattr(amazon_web_services, "kubernetes_versions", lambda: MOCK_KUBERNETES_VERSIONS)
+    monkeypatch.setattr(azure_cloud, "kubernetes_versions", lambda: MOCK_KUBERNETES_VERSIONS)
+    monkeypatch.setattr(digital_ocean, "kubernetes_versions", lambda _: MOCK_KUBERNETES_VERSIONS)
+    monkeypatch.setattr(google_cloud, "kubernetes_versions", lambda _: MOCK_KUBERNETES_VERSIONS)
 
     app = create_cli()
     args = [
         "init", provider,
+        "--no-guided-init", # default
+        "--no-auth-auto-provision", # default
+        "--no-repository-auto-provision", # default
+        "--disable-prompt",
         "--project-name", project_name,
         "--domain-name", domain_name,
         "--namespace", namespace,
         "--auth-provider", auth_provider,
+        "--repository", repository, # TODO: doesn't show up in the output anywhere, how do I verify this?
         "--ci-provider", ci_provider,
         "--terraform-state", terraform_state,
         "--ssl-cert-email", email,
+        "--kubernetes-version", kubernetes_version,
     ]
+
     expected_yaml = f"""
     provider: {provider}
     namespace: {namespace}
     project_name: {project_name}
     domain: {domain_name}
-    certificate:
-        type: lets-encrypt
-        acme_email: {email}
     ci_cd:
         type: {ci_provider}
     terraform_state:
@@ -94,21 +134,38 @@ def test_all_init_no_guided_happy(monkeypatch, provider: str, project_name: str,
     security:
         authentication:
             type: {auth_provider}
+    certificate:
+        type: lets-encrypt
+        acme_email: {email}
+    """
+
+    provider_section = get_provider_section_header(provider)
+    if provider_section != "" and kubernetes_version != "latest":
+        expected_yaml += f"""
+    {provider_section}:
+        kubernetes_version: '{kubernetes_version}'
     """
 
     assert_nebari_init_args(app, args, expected_yaml)
 
 
-def assert_nebari_init_args(app: Typer, args: List[str], expected_yaml: str):
+def assert_nebari_init_args(app: Typer, args: List[str], expected_yaml: str, input: str = None):
+    """
+    Run nebari init with happy path assertions and verify the generated yaml contains
+    all values in expected_yaml.
+    """
     with tempfile.TemporaryDirectory() as tmp:
         tmp_file = os.path.join(tmp, 'nebari-config.yaml')
         print(f"\n>>>> Using tmp file {tmp_file}")
         assert os.path.exists(tmp_file) == False
 
-        print(f"\n>>>> Testing nebari {args}")
+        print(f"\n>>>> Testing nebari {args} -- input {input}")
 
-        result = runner.invoke(app, args + ["--output", tmp_file, "--disable-prompt"])
-        assert result.exit_code == 0
+        result = runner.invoke(app, args + ["--output", tmp_file], input = input, env = MOCK_ENV)
+        print(f"\n>>> runner.stdout == {result.stdout}")
+
+        assert not result.exception
+        assert 0 == result.exit_code
         assert os.path.exists(tmp_file) == True
 
         with open(tmp_file, 'r') as config_yaml:
@@ -116,6 +173,19 @@ def assert_nebari_init_args(app: Typer, args: List[str], expected_yaml: str):
             expected = flatten_dict(yaml.safe_load(expected_yaml))
             assert expected.items() <= config.items()
 
+
+def pytest_generate_tests(metafunc):
+    """
+    Dynamically generate test data parameters for test functions by looking for
+    and executing an associated generate_test_data_{function_name} if one exists.
+    """
+
+    try:
+        td = eval(f"generate_test_data_{metafunc.function.__name__}")()
+        metafunc.parametrize(",".join(td["keys"]), td["test_data"])
+    except:
+        # expected when a generate_test_data_ function doesn't exist
+        pass
 
 # https://stackoverflow.com/a/62186053
 def flatten_dict(dictionary, parent_key=False, separator='.'):
@@ -139,24 +209,15 @@ def flatten_dict(dictionary, parent_key=False, separator='.'):
             items.append((new_key, value))
     return dict(items)
 
-###
-# TODO:
-# auth_provider - Auth0 and GitHub both prompt for credentials even when supplied by environment variable, custom throws an error that it doesn't exist
-###
-def pytest_generate_tests(metafunc):
-    param_names = ["provider", "project_name", "domain_name", "namespace", "auth_provider", "ci_provider", "terraform_state", "email"]
-    #if all(x in metafunc.fixturenames for x in param_names): # inject fixtures by matching argument names
-    if metafunc.function.__name__.startswith("test_all_init_"): # inject by function naming convention
-        test_data = []
-        for provider in ["local", "aws", "azure", "gcp", "do", "existing"]: # ["local", "aws", "azure", "gcp", "do", "existing"]
-            for project_name in ["testproject"]:
-                for auth_provider in ["password"]: # ["password", "Auth0", "GitHub", "custom"]
-                    for ci_provider in ["none", "github-actions", "gitlab-ci"]: # ["none", "github-actions", "gitlab-ci"]
-                        for terraform_state in ["local", "remote", "existing"]:
-                            test_data.append((provider, project_name, f"{project_name}.example.com", "test", auth_provider, ci_provider, terraform_state, "noreply@example.com"))
 
-        metafunc.parametrize(",".join(param_names), test_data)
-        
-def mock_cloud_provider_env(monkeypatch):
-    for e in MOCK_ENV:
-        monkeypatch.setenv(e, "test")
+def get_provider_section_header(provider: str):
+    if provider == "aws":
+        return "amazon_web_services"
+    if provider == "gcp":
+        return "google_cloud_platform"
+    if provider == "azure":
+        return "azure"
+    if provider == "do":
+        return "digital_ocean"
+    
+    return ""
