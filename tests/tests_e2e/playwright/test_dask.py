deb7410d2a3a654b6341aff81e091c6316bf932d
Fangchen Li <fangchen.li@outlook.com>
2023-09-06 16:25:20 -0700

    TST: playwright test for dask gateway

---
+++ tests/tests_e2e/playwright/test_dask.py
@@ -0,0 +1,24 @@
+import re
+
+from tests.common.playwright_fixtures import navigator_parameterized
+from tests.common.run_notebook import Notebook
+
+
+@navigator_parameterized(instance_name="small-instance")
+def test_dask_gateway(navigator):
+
+    input_output = [
+        ("from dask_gateway import Gateway", ""),
+        ("gateway = Gateway(); gateway", re.compile(r"Gateway*")),
+        ("cluster = gateway.new_cluster(); cluster", re.compile(r"GatewayCluster")),
+        ("client = cluster.get_client(); client", re.compile(r"Client")),
+    ]
+
+    test_app = Notebook(navigator=navigator)
+    conda_env = "dask"
+    test_app.create_notebook(
+        conda_env=f"conda-env-nebari-git-nebari-git-{conda_env}-py"
+    )
+
+    for input, output in input_output:
+        test_app.assert_code_output(input, output)
