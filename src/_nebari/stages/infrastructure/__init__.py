d09ea3459bafd556495aae24dc1eeebc77ae80e6
iameskild <eskild.eriksen122@gmail.com>
2023-09-19 09:36:51 -0700

    Minor fix for AzureInputVars

--- src/_nebari/stages/infrastructure/__init__.py
+++ src/_nebari/stages/infrastructure/__init__.py
@@ -116,6 +116,8 @@ class AzureInputVars(schema.Base):
     vnet_subnet_id: str = None
     private_cluster_enabled: bool
     tags: Dict[str, str] = {}
+    max_pods: int = None
+    network_profile: Dict[str, str] = None
 
 
 class AWSNodeGroupInputVars(schema.Base):
