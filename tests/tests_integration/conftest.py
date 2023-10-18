2aa8ebd762b424b5690073d93d11b8f4c512c6f6
Fangchen Li <fangchen.li@outlook.com>
2023-10-17 21:54:49 -0700

    TST: migrate from cypress to playwright

--- tests/tests_integration/conftest.py
+++ tests/tests_integration/conftest.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 pytest_plugins = [
     "tests.tests_integration.deployment_fixtures",
     "tests.common.playwright_fixtures",
@@ -9,3 +12,18 @@ def pytest_addoption(parser):
     parser.addoption(
         "--cloud", action="store", help="Cloud to deploy on: aws/do/gcp/azure"
     )
+
+
+@pytest.fixture
+def local_test_credential():
+    import os
+
+    username = os.environ.get("EXAMPLE_USER_NAME", None)
+    password = os.environ.get("EXAMPLE_USER_PASSWORD", None)
+    domain = os.environ.get("TEST_DOMAIN", None)
+    assert None not in [
+        username,
+        password,
+        domain,
+    ], f"Missing required environment variables:\nEXAMPLE_USER_NAME: {username}, EXAMPLE_USER_PASSWORD: {password}, TEST_DOMAIN: {domain}"
+    return username, password, domain
---
+++ tests/tests_integration/test_login.py
@@ -0,0 +1,32 @@
+from playwright.sync_api import Page, expect
+import pytest
+
+@pytest.mark.local
+def test_login(page: Page, local_test_credential):
+    _, _, domain = local_test_credential
+    page.goto(f"https://{domain}")
+    # page.goto("https://nebari.quansight.dev/")
+
+    page.screenshot(path="screenshots/homepage.png")
+    locator = page.locator("#login-main > div > a")
+    expect(locator).to_be_visible()
+    locator.click()
+
+    page.screenshot(path="screenshots/login.png")
+    locator = page.locator("#social-auth0")
+    expect(locator).to_be_visible()
+    locator.click()
+
+    page.screenshot(path="screenshots/auth0.png")
+    expect(page.get_by_placeholder("yours@example.com")).to_be_visible()
+
+
+@pytest.mark.local
+def test_password_login(page: Page, local_test_credential):
+    username, password, domain = local_test_credential
+    page.goto(f"https://{domain}")
+    page.locator("#login-main > div > a").click()
+    page.locator("#username").fill(username)
+    page.locator("#password").fill(password)
+    page.locator("#kc-login").click()
+    page.screenshot(path="screenshots/password_login.png")
