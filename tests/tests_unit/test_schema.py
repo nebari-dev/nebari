7323e868645f0802c2e282b6d39a446e899e6bc0
Fangchen Li <fangchen.li@outlook.com>
2023-10-21 21:36:24 -0700

    add test

--- tests/tests_unit/test_schema.py
+++ tests/tests_unit/test_schema.py
@@ -5,6 +5,7 @@ from pydantic.error_wrappers import ValidationError
 
 from nebari import schema
 from nebari.plugins import nebari_plugin_manager
+from _nebari.provider.terraform import deploy
 
 
 def test_minimal_schema():
@@ -137,3 +138,19 @@ def test_multiple_providers(config_schema):
     msg = r"Multiple providers set: \['local', 'existing'\]"
     with pytest.raises(ValidationError, match=msg):
         config_schema(**config_dict)
+
+
+def test_aws_premissions_boundary(config_schema):
+    permissions_boundary = "arn:aws:iam::123456789012:policy/MyBoundaryPolicy"
+    config_dict = {
+        "project_name": "test",
+        "provider": "aws",
+        "amazon_web_services": {
+            "region": "us-east-1",
+            "kubernetes_version": "1.19",
+            "permissions_boundary": f"{permissions_boundary}",
+        },
+    }
+    config = config_schema(**config_dict)
+    assert config.provider == "aws"
+    assert config.amazon_web_services.permissions_boundary == permissions_boundary
