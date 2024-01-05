568ff67dcf6f020807a0f4eced4e0a2a9fec67d1
Amit Kumar <dtu.amit@gmail.com>
2024-01-05 23:49:27 +0000

    add jhub apps config

--- src/_nebari/stages/kubernetes_services/template/modules/kubernetes/services/jupyterhub/files/jupyterhub/02-spawner.py
+++ src/_nebari/stages/kubernetes_services/template/modules/kubernetes/services/jupyterhub/files/jupyterhub/02-spawner.py
@@ -6,6 +6,7 @@ kubernetes.client.models.V1EndpointPort = (
 )  # noqa: E402
 
 from kubespawner import KubeSpawner  # noqa: E402
+from jhub_apps.configuration import install_jhub_apps
 
 
 @gen.coroutine
@@ -24,3 +25,19 @@ c.Spawner.pre_spawn_hook = get_username_hook
 
 c.JupyterHub.allow_named_servers = False
 c.JupyterHub.spawner_class = KubeSpawner
+c.JupyterHub.bind_url = "https://jhub.quansight.dev/"
+# c.JAppsConfig.python_exec = "/home/conda/aktech/envs/aktech-japps/bin/python"
+c.JAppsConfig.conda_envs = [
+    "aktech-japps",
+    "nebari-git-dashboard",
+    "nebari-git-dask",
+    "base",
+    "default",
+]
+
+c.JAppsConfig.jupyterhub_config_path = "/usr/local/etc/jupyterhub/jupyterhub_config.py"
+
+c = install_jhub_apps(c, spawner_to_subclass=KubeSpawner)
+c.JupyterHub.log_level = 10
+c.JupyterHub.log_level = "DEBUG"
+c.Spawner.debug = True
