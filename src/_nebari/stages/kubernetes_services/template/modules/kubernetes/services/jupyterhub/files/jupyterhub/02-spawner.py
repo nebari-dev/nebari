9e90ea6daa4cb23bdd73893723a08b484b3c9b15
Amit Kumar <dtu.amit@gmail.com>
2024-01-06 00:13:54 +0000

    add template themes

--- src/_nebari/stages/kubernetes_services/template/modules/kubernetes/services/jupyterhub/files/jupyterhub/02-spawner.py
+++ src/_nebari/stages/kubernetes_services/template/modules/kubernetes/services/jupyterhub/files/jupyterhub/02-spawner.py
@@ -10,6 +10,7 @@ kubernetes.client.models.V1EndpointPort = (
 
 from kubespawner import KubeSpawner  # noqa: E402
 from jhub_apps.configuration import install_jhub_apps
+from jhub_apps import theme_template_paths
 
 
 @gen.coroutine
@@ -28,7 +29,8 @@ c.Spawner.pre_spawn_hook = get_username_hook
 
 c.JupyterHub.allow_named_servers = False
 c.JupyterHub.spawner_class = KubeSpawner
-c.JupyterHub.bind_url = "https://jamit.quansight.dev/"
+hub_url = "https://jamit.quansight.dev/"
+c.JupyterHub.bind_url = hub_url
 # c.JAppsConfig.python_exec = "/home/conda/aktech/envs/aktech-japps/bin/python"
 c.JAppsConfig.conda_envs = [
     "aktech-japps",
@@ -45,3 +47,95 @@ c = install_jhub_apps(c, spawner_to_subclass=KubeSpawner)
 c.JupyterHub.log_level = 10
 c.JupyterHub.log_level = "DEBUG"
 c.Spawner.debug = True
+
+c.JupyterHub.template_paths = theme_template_paths
+c.JupyterHub.services.extend(
+    [
+        {
+            "name": "JuypterLab",
+            "url": hub_url,
+            "display": True,
+            "info": {
+                "name": "JupyterLab",
+                "url": "/user/[USER]/lab",
+                "external": True,
+            },
+            "oauth_no_confirm": True,
+        },
+        {
+            "name": "Argo",
+            "url": hub_url,
+            "display": True,
+            "info": {
+                "name": "Argo Workflows",
+                "url": "/hub/argo",
+                "external": True,
+            },
+            "oauth_no_confirm": True,
+        },
+        {
+            "name": "Users",
+            "url": hub_url,
+            "display": True,
+            "info": {
+                "name": "User Management",
+                "url": "/auth/admin/nebari/console/",
+                "external": True,
+            },
+            "oauth_no_confirm": True,
+        },
+        {
+            "name": "Environments",
+            "url": hub_url,
+            "display": True,
+            "info": {
+                "name": "Environments",
+                "url": "/hub/conda-store",
+                "external": True,
+            },
+            "oauth_no_confirm": True,
+        },
+        {
+            "name": "Monitoring",
+            "url": hub_url,
+            "display": True,
+            "info": {
+                "name": "Monitoring",
+                "url": "/hub/monitoring",
+                "external": True,
+            },
+            "oauth_no_confirm": True,
+        },
+        {
+            "name": "MLflow",
+            "url": "http://mlflow.mlflow:5000",
+            "display": True,
+            "info": {
+                "name": "MLflow",
+                "url": "http://mlflow.mlflow:5000",
+                "external": True,
+            },
+            "oauth_no_confirm": True,
+        },
+    ]
+)
+
+# nebari will control these as ways to customize the template
+c.JupyterHub.template_vars = {
+    "hub_title": "Welcome to Nebari",
+    "hub_subtitle": "your open source data science platform",
+    "welcome": "Running in dev mode",
+    "logo": "/services/japps/static/img/Nebari-Logo-Horizontal-Lockup-White-text.svg",
+    "primary_color": "#C316E9",
+    "primary_color_dark": "#79158a",
+    "secondary_color": "#18817A",
+    "secondary_color_dark": "#12635E",
+    "accent_color": "#eda61d",
+    "accent_color_dark": "#a16d14",
+    "text_color": "#1c1d26",
+    "h1_color": "#0f1015",
+    "h2_color": "#0f1015",
+    "navbar_text_color": "#ffffff",
+    "navbar_hover_color": "#20b1a8",
+    "navbar_color": "#1c1d26",
+}
