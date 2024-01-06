30c0025f0956209b90883b40cdf78f9db3d30331
Prashant Tiwari <prashant.tiwari@genesisoilandgas.com>
2024-01-06 14:21:24 +0000

    Revert aws version change.

--- src/_nebari/stages/kubernetes_initialize/template/versions.tf
+++ src/_nebari/stages/kubernetes_initialize/template/versions.tf
@@ -8,10 +8,6 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "2.20.0"
     }
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.73.0"
-    }
   }
   required_version = ">= 1.0"
 }
