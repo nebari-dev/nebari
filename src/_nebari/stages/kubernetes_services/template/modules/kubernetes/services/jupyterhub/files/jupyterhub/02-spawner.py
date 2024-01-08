a6405ee28c542e3dda9f01ce3c24b7f315f04903
Amit Kumar <dtu.amit@gmail.com>
2024-01-08 15:22:02 +0000

    add vscode service

--- src/_nebari/stages/kubernetes_services/template/modules/kubernetes/services/jupyterhub/files/jupyterhub/02-spawner.py
+++ src/_nebari/stages/kubernetes_services/template/modules/kubernetes/services/jupyterhub/files/jupyterhub/02-spawner.py
@@ -84,6 +84,7 @@ if z2jh.get_config("custom.jhub-apps-enabled"):
             service_for_jhub_apps(name="Users", url="/auth/admin/nebari/console/"),
             service_for_jhub_apps(name="Environments", url="/conda-store"),
             service_for_jhub_apps(name="Monitoring", url="/monitoring"),
+            service_for_jhub_apps(name="VSCode", url="/user/[USER]/vscode"),
         ]
     )
 
