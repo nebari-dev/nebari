1a00157d93ba423ae2a6753722fb4ced6af27666
Prashant Tiwari <prashant.tiwari@genesisoilandgas.com>
2024-01-08 19:02:00 +0000

    Remove aws filter from stage level and add it to module level.

--- src/_nebari/stages/kubernetes_initialize/template/modules/tagging/variables.tf
+++ src/_nebari/stages/kubernetes_initialize/template/modules/tagging/variables.tf
@@ -6,3 +6,8 @@ variable "aws_region" {
   description = "AWS Region that cluster autoscaler is running"
   type        = string
 }
+
+variable "cloud_provider" {
+  description = "Cloud provider being used in deployment"
+  type        = string
+}
