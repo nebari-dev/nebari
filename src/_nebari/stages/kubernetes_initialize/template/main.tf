560fc1a0671c0741b5499d8fd717dbd8fef521ba
Prashant Tiwari <prashant.tiwari@genesisoilandgas.com>
2024-01-09 10:27:34 +0000

    Get aws_get_asg_node_group_mapping from stage 2 output.

--- src/_nebari/stages/kubernetes_initialize/template/main.tf
+++ src/_nebari/stages/kubernetes_initialize/template/main.tf
@@ -25,6 +25,7 @@ module "nvidia-driver-installer" {
 }
 
 module "tagging" {
+  count              = length(var.asg_node_group_map) > 0 ? 1 : 0
   source             = "./modules/tagging"
   asg_node_group_map = var.asg_node_group_map
 }
