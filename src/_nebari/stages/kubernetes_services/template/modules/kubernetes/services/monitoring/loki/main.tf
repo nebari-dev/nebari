ef8edecc9c1b1f06b4551e70c2ae4e4f767d27f5
Amit Kumar <dtu.amit@gmail.com>
2024-02-29 22:22:43 +0000

    install promtail

--- src/_nebari/stages/kubernetes_services/template/modules/kubernetes/services/monitoring/loki/main.tf
+++ src/_nebari/stages/kubernetes_services/template/modules/kubernetes/services/monitoring/loki/main.tf
@@ -1,12 +1,27 @@
-resource "helm_release" "loki-grafana" {
-  name       = "loki-grafana"
+resource "helm_release" "grafana-loki" {
+  name       = "grafana-loki"
   namespace  = var.namespace
   repository = "https://grafana.github.io/helm-charts"
   chart      = "loki"
   version    = var.loki-helm-chart-version
 
   values = concat([
-    file("${path.module}/values.yaml"),
+    file("${path.module}/values_loki.yaml"),
+    jsonencode({
+    })
+  ], var.overrides)
+}
+
+
+resource "helm_release" "grafana-promtail" {
+  name       = "grafana-promtail"
+  namespace  = var.namespace
+  repository = "https://grafana.github.io/helm-charts"
+  chart      = "promtail"
+  version    = var.promtail-helm-chart-version
+
+  values = concat([
+    file("${path.module}/values_promtail.yaml"),
     jsonencode({
     })
   ], var.overrides)
--- src/_nebari/stages/kubernetes_services/template/modules/kubernetes/services/monitoring/loki/variables.tf
+++ src/_nebari/stages/kubernetes_services/template/modules/kubernetes/services/monitoring/loki/variables.tf
@@ -15,6 +15,12 @@ variable "loki-helm-chart-version" {
   default     = "5.43.3"
 }
 
+variable "promtail-helm-chart-version" {
+  description = "version to deploy for the promtail helm chart"
+  type        = string
+  default     = "6.15.5"
+}
+
 variable "overrides" {
   description = "Grafana Loki helm chart overrides"
   type        = list(string)
