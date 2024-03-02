d0e54fc656bc4731fb60faeebcad7d1ba2f50b1c
Amit Kumar <dtu.amit@gmail.com>
2024-03-02 21:42:39 +0000

    refactor port forward fixture

--- tests/tests_deployment/test_loki_deployment.py
+++ tests/tests_deployment/test_loki_deployment.py
@@ -19,25 +19,22 @@ MINIO_POD_LABELS = {
 
 
 @pytest.fixture(scope="module")
-def port_forward():
+def port_forward_fixture(request):
     """Pytest fixture to port forward loki backend pod to make it accessible
     on localhost so that we can run some tests on it.
     """
     return kubernetes_port_forward(
-        pod_labels=LOKI_BACKEND_POD_LABELS,
-        port=LOKI_BACKEND_PORT
+        pod_labels=request.param["labels"],
+        port=request.param["port"]
     )
 
 
-@pytest.fixture(scope="module")
-def port_forward_minio():
-    """Pytest fixture to port forward loki backend pod to make it accessible
-    on localhost so that we can run some tests on it.
-    """
-    return kubernetes_port_forward(
-        pod_labels=MINIO_POD_LABELS,
-        port=MINIO_PORT
-    )
+def port_forward(labels, port):
+    params = {
+        "labels": labels,
+        "port": port
+    }
+    return pytest.mark.parametrize("port_forward_fixture", [params], indirect=True)
 
 
 # https://github.com/kubernetes-client/python/issues/2024
@@ -52,14 +49,15 @@ def port_forward_minio():
             "log_level",
     ),
 )
-def test_loki_endpoint(endpoint_path: str, port_forward: V1Pod):
+@port_forward(labels=LOKI_BACKEND_POD_LABELS, port=LOKI_BACKEND_PORT)
+def test_loki_endpoint(endpoint_path: str, port_forward_fixture: V1Pod):
     """This will hit some endpoints in the loki API and verify that we
     get a 200 status code, to make sure Loki is working properly.
     :param endpoint_path: a loki api endpoint path
     :param port_forward: pytest fixture to port forward.
     :return:
     """
-    pod_name = port_forward.metadata.name
+    pod_name = port_forward_fixture.metadata.name
     url = f'http://{pod_name}.pod.dev.kubernetes:{LOKI_BACKEND_PORT}/{endpoint_path}'
     response = urllib_request.urlopen(url)
     response.read().decode('utf-8')
@@ -67,13 +65,14 @@ def test_loki_endpoint(endpoint_path: str, port_forward: V1Pod):
     response.close()
 
 
-def test_minio_accessible(port_forward_minio: V1Pod):
+@port_forward(labels=MINIO_POD_LABELS, port=MINIO_PORT)
+def test_minio_accessible(port_forward_fixture: V1Pod):
     """This will hit liveness endpoint of minio  API and verify that we
     get a 200 status code, to make sure minio is up and running.
     :param port_forward: pytest fixture to port forward.
     :return:
     """
-    pod_name = port_forward_minio.metadata.name
+    pod_name = port_forward_fixture.metadata.name
     url = f'http://{pod_name}.pod.dev.kubernetes:{MINIO_PORT}/minio/health/live'
     response = urllib_request.urlopen(url)
     response.read().decode('utf-8')
