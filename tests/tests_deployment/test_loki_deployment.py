50f27bc78382a347aeeec29991eecde1396de04f
Amit Kumar <dtu.amit@gmail.com>
2024-03-02 22:15:35 +0000

    improve docs

--- tests/tests_deployment/test_loki_deployment.py
+++ tests/tests_deployment/test_loki_deployment.py
@@ -1,3 +1,6 @@
+import json
+import urllib.parse
+
 import pytest
 
 import six.moves.urllib.request as urllib_request
@@ -15,6 +18,12 @@ MINIO_PORT = 9000
 MINIO_POD_LABELS = {
     "app.kubernetes.io/instance": "nebari-loki-minio",
     "app.kubernetes.io/name": "minio"
+}
+
+LOKI_GATEWAY_PORT = 8080
+LOKI_GATEWAY_POD_LABELS = {
+    "app.kubernetes.io/instance": "nebari-loki",
+    "app.kubernetes.io/component": "gateway"
 }
 
 
@@ -77,4 +86,48 @@ def test_minio_accessible(port_forward_fixture: V1Pod):
     response = urllib_request.urlopen(url)
     response.read().decode('utf-8')
     assert response.code == 200
+    response.close()
+
+
+@port_forward(labels=LOKI_GATEWAY_POD_LABELS, port=LOKI_GATEWAY_PORT)
+def test_loki_gateway(port_forward_fixture: V1Pod):
+    """This will hit an endpoint of loki gateway API and verify that we
+    get a 200 status code, to make sure minio is up and running.
+    :param port_forward_fixture: pytest fixture to port forward.
+    :return:
+    """
+    pod_name = port_forward_fixture.metadata.name
+    url = f'http://{pod_name}.pod.dev.kubernetes:{LOKI_BACKEND_PORT}/loki/api/v1/labels'
+    response = urllib_request.urlopen(url)
+    response_content = response.read().decode('utf-8')
+    response_json = json.loads(response_content)
+    assert response.code == 200
+    assert response_json["status"] == "success"
+    response.close()
+
+
+@port_forward(labels=LOKI_GATEWAY_POD_LABELS, port=LOKI_GATEWAY_PORT)
+def test_loki_gateway_fetch_logs(port_forward_fixture: V1Pod):
+    """This will hit an endpoint of loki gateway API to fetch some logs
+    and verify logs received.
+    :param port_forward_fixture: pytest fixture to port forward.
+    :return: None
+    """
+    pod_name = port_forward_fixture.metadata.name
+    query_params = {
+        "limit": "5",
+        # Fetch logs for jupyterhub app
+        "query": '{app="jupyterhub"}'
+    }
+
+    encoded_params = urllib.parse.urlencode(query_params)
+    path = f"/loki/api/v1/query_range?{encoded_params}"
+    url = f'http://{pod_name}.pod.dev.kubernetes:{LOKI_BACKEND_PORT}/{path}'
+    response = urllib_request.urlopen(url)
+    response_content = response.read().decode('utf-8')
+    response_json = json.loads(response_content)
+    assert response.code == 200
+    assert response_json["status"] == "success"
+    # Make sure log lines received
+    assert len(response_json['data']['result'][0]["values"]) > 0
     response.close()
