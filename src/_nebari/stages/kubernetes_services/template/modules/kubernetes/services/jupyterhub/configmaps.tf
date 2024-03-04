d140bf7e235abdddec31248ffac137b9acc82401
krassowski <5832902+krassowski@users.noreply.github.com>
2024-03-04 11:22:47 +0000

    Check for syntax errors in `jupyter_jupyterlab_pioneer_config.py` too

--- src/_nebari/stages/kubernetes_services/template/modules/kubernetes/services/jupyterhub/configmaps.tf
+++ src/_nebari/stages/kubernetes_services/template/modules/kubernetes/services/jupyterhub/configmaps.tf
@@ -40,6 +40,11 @@ resource "local_file" "jupyter_server_config_py" {
 resource "local_file" "jupyter_jupyterlab_pioneer_config_py" {
   content  = local.jupyter-pioneer-config-py-template
   filename = "${path.module}/files/jupyter/jupyter_jupyterlab_pioneer_config.py"
+
+  provisioner "local-exec" {
+    # check the syntax of the config file without running it
+    command = "python -m py_compile ${self.filename}"
+  }
 }
 
 
