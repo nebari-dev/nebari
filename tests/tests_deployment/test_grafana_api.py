e821a108003239a2f5bb7749bedbe893d35730f8
Amit Kumar <dtu.amit@gmail.com>
2024-03-05 18:02:29 +0000

    use http

--- tests/tests_deployment/test_grafana_api.py
+++ tests/tests_deployment/test_grafana_api.py
@@ -9,7 +9,7 @@ def test_grafana_api_not_accessible_with_default_credentials():
     """Making sure that Grafana's API is not accessible on default user/pass"""
     user_pass_b64_encoded = base64.b64encode(b"admin:prom-operator").decode()
     response = requests.get(
-        f"https://{constants.NEBARI_HOSTNAME}/monitoring/api/datasources",
+        f"http://{constants.NEBARI_HOSTNAME}/monitoring/api/datasources",
         headers={"Authorization": f"Basic {user_pass_b64_encoded}"},
         verify=False,
     )
