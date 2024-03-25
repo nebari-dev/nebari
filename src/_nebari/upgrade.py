3e1dfde9c56cfe6c151b36333713d6fbea30bf86
Vinicius D. Cerutti <51954708+viniciusdc@users.noreply.github.com>
2024-03-25 17:35:26 -0300

    New upgrade step for 2024.3.3 (#2356)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/_nebari/upgrade.py
+++ src/_nebari/upgrade.py
@@ -727,6 +727,17 @@ class Upgrade_2024_3_2(UpgradeStep):
         return config
 
 
+class Upgrade_2024_3_3(UpgradeStep):
+    version = "2024.3.3"
+
+    def _version_specific_upgrade(
+        self, config, start_version, config_filename: Path, *args, **kwargs
+    ):
+        rich.print("Ready to upgrade to Nebari version [green]2024.3.3[/green].")
+
+        return config
+
+
 __rounded_version__ = str(rounded_ver_parse(__version__))
 
 # Manually-added upgrade steps must go above this line
