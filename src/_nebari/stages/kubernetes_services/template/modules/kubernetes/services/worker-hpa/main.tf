6b1e7ffeef7c8d3395e92bdea6ded3376f5fabee
Prashant Tiwari <prashanttiwari247@gmail.com>
2024-04-09 14:20:34 +0100

    More reactive scale up and down.

--- src/_nebari/stages/kubernetes_services/template/modules/kubernetes/services/worker-hpa/main.tf
+++ src/_nebari/stages/kubernetes_services/template/modules/kubernetes/services/worker-hpa/main.tf
@@ -35,11 +35,14 @@ resource "kubernetes_manifest" "scaledobject" {
         kind = "Deployment"
         name = "nebari-conda-store-worker"
       }
+      minReplicaCount: 1   # Default: 0
+      pollingInterval: 5   # Default:  30 seconds
+      cooldownPeriod: 60  # Default: 300 seconds
       triggers = [
         {
           type = "postgresql"
           metadata = {
-            query            = "SELECT COUNT(*) FROM build WHERE status='QUEUED';"
+            query            = "SELECT COUNT(*) FROM build WHERE status IN ('QUEUED', 'BUILDING');"
             targetQueryValue = "1"
             host             = "nebari-conda-store-postgresql"
             userName         = "postgres"
