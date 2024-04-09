b1be5b53b9b35c090e2d7f10b9bcc48087db7b07
Prashant Tiwari <prashanttiwari247@gmail.com>
2024-04-09 16:55:30 +0100

    Code refactor.

--- src/_nebari/stages/kubernetes_services/template/modules/kubernetes/services/conda-store/worker.tf
+++ src/_nebari/stages/kubernetes_services/template/modules/kubernetes/services/conda-store/worker.tf
@@ -202,3 +202,64 @@ resource "kubernetes_deployment" "worker" {
     }
   }
 }
+
+resource "kubernetes_manifest" "triggerauthenticator" {
+  manifest = {
+    apiVersion = "keda.sh/v1alpha1"
+    kind       = "TriggerAuthentication"
+
+    metadata = {
+      name      = "trigger-auth-postgres"
+      namespace = var.namespace
+    }
+
+    spec = {
+      secretTargetRef = [
+        {
+          name      = "nebari-conda-store-postgresql"
+          parameter = "password"
+          key       = "postgresql-password"
+        }
+      ]
+    }
+  }
+}
+
+resource "kubernetes_manifest" "scaledobject" {
+  manifest = {
+    apiVersion = "keda.sh/v1alpha1"
+    kind       = "ScaledObject"
+
+    metadata = {
+      name      = "scaled-conda-worker"
+      namespace = var.namespace
+    }
+
+    spec = {
+      scaleTargetRef = {
+        kind = "Deployment"
+        name = "nebari-conda-store-worker"
+      }
+      # minReplicaCount : 1 # Default: 0
+      pollingInterval : 5 # Default: 30 seconds
+      # cooldownPeriod : 30 # Default: 300 seconds
+      triggers = [
+        {
+          type = "postgresql"
+          metadata = {
+            query            = "SELECT COUNT(*) FROM build WHERE status IN ('QUEUED', 'BUILDING');"
+            targetQueryValue = "1"
+            host             = "nebari-conda-store-postgresql"
+            userName         = "postgres"
+            port             = "5432"
+            dbName           = "conda-store"
+            sslmode          = "disable"
+          }
+          authenticationRef = {
+            name = "trigger-auth-postgres"
+          }
+        }
+      ]
+    }
+  }
+}
--- src/_nebari/stages/kubernetes_services/template/modules/kubernetes/services/worker-hpa/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "namespace" {
-  description = "deploy argo server on this namespace"
-  type        = string
-  default     = "dev"
-}
