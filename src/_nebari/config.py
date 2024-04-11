8e3982e74746f62caddff9c39fb66d09ac82f647
Adam Lewis <23342526+Adam-D-Lewis@users.noreply.github.com>
2024-04-11 15:08:57 -0500

    Revert "dump nested models"
    This reverts commit b165abcd31f617c81595e018720e905753b5f1b0.

--- src/_nebari/config.py
+++ src/_nebari/config.py
@@ -106,7 +106,6 @@ def write_configuration(
         if isinstance(config, pydantic.BaseModel):
             yaml.dump(config.dict(), f)
         else:
-            config = dump_nested_model(config)
             yaml.dump(config, f)
 
 
