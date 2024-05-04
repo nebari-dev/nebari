dd913d79f5a0c80c1be9eced2685cf25319d208f
krassowski <5832902+krassowski@users.noreply.github.com>
2024-05-04 15:19:43 +0100

    Add a test

---
+++ tests/tests_deployment/test_jupyterhub_api.py
@@ -0,0 +1,21 @@
+import pytest
+import requests
+
+from tests.tests_deployment import constants
+from tests.tests_deployment.utils import get_jupyterhub_token
+
+
+@pytest.fixture(scope="session")
+def api_token():
+    return get_jupyterhub_token("jupyterhub-api")
+
+
+@pytest.mark.filterwarnings("ignore::urllib3.exceptions.InsecureRequestWarning")
+def test_jupyterhub_loads_roles_from_keycloak(api_token):
+    response = requests.get(
+        f"https://{constants.NEBARI_HOSTNAME}/hub/api/user/{constants.KEYCLOAK_USERNAME}",
+        headers={"Authorization": f"Bearer {api_token}"},
+        verify=False,
+    )
+    user = response.json()
+    assert user["roles"] == []
