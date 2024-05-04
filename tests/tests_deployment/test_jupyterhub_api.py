a5e76ba663b5443c6986779b24bd5219dd15af12
krassowski <5832902+krassowski@users.noreply.github.com>
2024-05-04 19:22:56 +0100

    Try using xsrf token

--- tests/tests_deployment/test_jupyterhub_api.py
+++ tests/tests_deployment/test_jupyterhub_api.py
@@ -1,20 +1,15 @@
 import pytest
-import requests
 
 from tests.tests_deployment import constants
-from tests.tests_deployment.utils import get_jupyterhub_token
-
-
-@pytest.fixture(scope="session")
-def api_token():
-    return get_jupyterhub_token("jupyterhub-api")
 
 
 @pytest.mark.filterwarnings("ignore::urllib3.exceptions.InsecureRequestWarning")
-def test_jupyterhub_loads_roles_from_keycloak(api_token):
-    response = requests.get(
+def test_jupyterhub_loads_roles_from_keycloak():
+    session = get_jupyterhub_session()
+    xsrf_token = session.cookies.get("_xsrf")
+    response = session.get(
         f"https://{constants.NEBARI_HOSTNAME}/hub/api/users",
-        headers={"Authorization": f"Bearer {api_token}"},
+        headers={"X-XSRFToken": xsrf_token},
         verify=False,
     )
     users = response.json()
