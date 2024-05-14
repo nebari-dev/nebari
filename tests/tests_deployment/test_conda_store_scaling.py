19f8b61c552a7aaaa3e4fb7857a79e98e16d6571
Prashant Tiwari <ptiwari@quansight.com>
2024-05-14 08:58:00 +0100

    Skip test_scale_up_and_down.

--- tests/tests_deployment/test_conda_store_scaling.py
+++ tests/tests_deployment/test_conda_store_scaling.py
@@ -125,6 +125,7 @@ class TestCondaStoreWorkerHPA(TestCase):
         self.builds = []
         self.count = TEST_CONDASTORE_WOKER_COUNT
 
+    @pytest.mark.skip(reason="Skiping test to check if this effects other tests.")
     def test_scale_up_and_down(self):
         with patched_secret_token(self.configuration) as (token, _api_client):
             self.request_session.headers.update({"Authorization": f"Bearer {token}"})
