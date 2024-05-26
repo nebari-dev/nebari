fb4037150f79c7e609b1fd5b8a314a7a7168cf11
Amit Kumar <aktech@users.noreply.github.com>
2024-05-26 16:33:34 +0100

    add tests for keycloak role attributes parsed correctly

--- tests/tests_deployment/test_jupyterhub_api.py
+++ tests/tests_deployment/test_jupyterhub_api.py
@@ -1,7 +1,10 @@
+import uuid
+
 import pytest
 
 from tests.tests_deployment import constants
-from tests.tests_deployment.utils import get_jupyterhub_session
+from tests.tests_deployment.utils import get_jupyterhub_session, get_jupyterhub_token, create_jupyterhub_token
+from tests.tests_deployment.keycloak_utils import create_keycloak_role, assign_keycloak_client_role_to_user
 
 
 @pytest.mark.filterwarnings("ignore::urllib3.exceptions.InsecureRequestWarning")
@@ -26,6 +29,33 @@ def test_jupyterhub_loads_roles_from_keycloak():
         "grafana_developer",
         "manage-account-links",
         "view-profile",
+    }
+
+
+@pytest.mark.filterwarnings("ignore::urllib3.exceptions.InsecureRequestWarning")
+@pytest.mark.filterwarnings("ignore:.*auto_refresh_token is deprecated:DeprecationWarning")
+def test_keycloak_roles_attributes_parsed_as_jhub_scopes(cleanup_keycloak_roles):
+    # check token scopes before role creation and assignment
+    response_before = create_jupyterhub_token(note="before-scope-parsing-test")
+    # create keycloak role with jupyterhub scopes in attributes
+    role = create_keycloak_role(
+        client_name="jupyterhub",
+        # Note: we're clearing this role after every test case, and we're clearing
+        # it by name, so it must start with test- to be deleted afterward
+        role_name=f"test-custom-role",
+        scopes="read:users:shares,read:groups:shares,users:shares",
+        component="jupyterhub"
+    )
+    assert role
+    # assign created role to the user
+    assign_keycloak_client_role_to_user(constants.KEYCLOAK_USERNAME, client_name="jupyterhub", role=role)
+    response_after = create_jupyterhub_token(note="after-role-parsing")
+    # verify new scopes added/removed
+    scopes_difference = set(response_after.json()['scopes']) - set(response_before.json()['scopes'])
+    assert scopes_difference == {
+        'read:groups:shares',
+        'users:shares',
+        'read:users:shares',
     }
 
 
