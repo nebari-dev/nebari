1364974d25efe3ab6eb6c661c71bfe0598b537bd
Marcelo Villa <mvilla@quansight.com>
2024-05-29 16:13:55 -0500

    Enable video recording.

--- tests/tests_e2e/cypress.config.js
+++ tests/tests_e2e/cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  video: true,
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
