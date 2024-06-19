3e94a6a07007da345e9941b5df0a25b28b184a0b
Chuck McAndrew <6248903+dcmcand@users.noreply.github.com>
2024-06-19 14:11:57 -0700

    restrict public access and add buckett encryption using cmk

--- src/_nebari/stages/infrastructure/template/aws/modules/s3/main.tf
+++ src/_nebari/stages/infrastructure/template/aws/modules/s3/main.tf
@@ -1,9 +1,24 @@
+resource "aws_kms_key" "main" {
+  description         = "KMS key for ${var.name}"
+  enable_key_rotation = true
+}
+
 resource "aws_s3_bucket" "main" {
   bucket = var.name
   acl    = var.public ? "public-read" : "private"
 
   versioning {
     enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.main.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+
   }
 
   tags = merge({
