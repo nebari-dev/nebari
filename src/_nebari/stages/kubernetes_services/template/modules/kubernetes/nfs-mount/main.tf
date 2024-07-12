9e9036d82ef35d62727cabccc742a6c23de7bf3e
Adam Lewis <23342526+Adam-D-Lewis@users.noreply.github.com>
2024-07-12 17:02:14 -0500

    restore nfs mount

--- src/_nebari/stages/kubernetes_services/template/modules/kubernetes/nfs-mount/main.tf
+++ src/_nebari/stages/kubernetes_services/template/modules/kubernetes/nfs-mount/main.tf
@@ -1,35 +1,29 @@
-# resource "kubernetes_storage_class" "main" {
-#   metadata {
-#     name = "${var.name}-${var.namespace}-share"
-#   }
-#   storage_provisioner = "kubernetes.io/fake-nfs"
-# }
+resource "kubernetes_storage_class" "main" {
+  metadata {
+    name = "${var.name}-${var.namespace}-share"
+  }
+  storage_provisioner = "kubernetes.io/fake-nfs"
+}
 
 
-# resource "kubernetes_persistent_volume" "main" {
-#   metadata {
-#     name = "${var.name}-${var.namespace}-share"
-#   }
-#   spec {
-#     capacity = {
-#       storage = var.nfs_capacity
-#     }
-#     storage_class_name = "ceph-filesystem" #  # kubernetes_storage_class.main.metadata.0.name  # TODO: Get this from a terraform output
-#     access_modes       = ["ReadWriteMany"]
-#     persistent_volume_source {
-#       csi {
-#         driver = "rook-ceph.cephfs.csi.ceph.com"
-#         volume_handle = "myfs"
-#         # fstype = "ext4"
-#         volume_attributes = {
-#           # "clusterID" = "mycluster"
-#           # "path" = "/myfs"
-#         }
-
-#       }
-#     }
-#   }
-# }
+resource "kubernetes_persistent_volume" "main" {
+  metadata {
+    name = "${var.name}-${var.namespace}-share"
+  }
+  spec {
+    capacity = {
+      storage = var.nfs_capacity
+    }
+    storage_class_name = kubernetes_storage_class.main.metadata.0.name
+    access_modes       = ["ReadWriteMany"]
+    persistent_volume_source {
+      nfs {
+        path   = "/"
+        server = var.nfs_endpoint
+      }
+    }
+  }
+}
 
 
 resource "kubernetes_persistent_volume_claim" "main" {
@@ -40,7 +34,7 @@ resource "kubernetes_persistent_volume_claim" "main" {
 
   spec {
     access_modes       = ["ReadWriteMany"]
-    storage_class_name = "ceph-filesystem-retain" # kubernetes_storage_class.main.metadata.0.name  # Get this from a terraform output
+    storage_class_name = kubernetes_storage_class.main.metadata.0.name
     resources {
       requests = {
         storage = var.nfs_capacity
