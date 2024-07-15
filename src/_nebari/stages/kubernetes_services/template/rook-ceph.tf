daaeec74763ff58c0d1f7a8f0427aa46473c249d
Adam Lewis <23342526+Adam-D-Lewis@users.noreply.github.com>
2024-07-15 17:31:59 -0500

    cleanup

--- src/_nebari/stages/kubernetes_services/template/rook-ceph.tf
+++ src/_nebari/stages/kubernetes_services/template/rook-ceph.tf
@@ -1,25 +1,4 @@
 # ======================= VARIABLES ======================
-# variable "argo-workflows-overrides" {
-#   description = "Argo Workflows helm chart overrides"
-#   type        = list(string)
-# }
-
-# variable "nebari-workflow-controller" {
-#   description = "Nebari Workflow Controller enabled"
-#   type        = bool
-# }
-
-
-# variable "keycloak-read-only-user-credentials" {
-#   description = "Keycloak password for nebari-bot"
-#   type        = map(string)
-# }
-
-# variable "workflow-controller-image-tag" {
-#   description = "Image tag for nebari-workflow-controller"
-#   type        = string
-# }
-
 variable "rook_ceph_storage_class_name" {
   description = "Name of the storage class to create"
   type        = string
@@ -36,11 +15,5 @@ module "rook-ceph" {
   storage_class_name = var.rook_ceph_storage_class_name
   node_group         = var.node_groups.general
   # external-url = var.endpoint
-  # realm_id     = var.realm_id
-
-
   # overrides = var.rook-ceph-overrides
-  # keycloak-read-only-user-credentials = var.keycloak-read-only-user-credentials
-  # workflow-controller-image-tag       = var.workflow-controller-image-tag
-  # nebari-workflow-controller          = var.nebari-workflow-controller
 }
