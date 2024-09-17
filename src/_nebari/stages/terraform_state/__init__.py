a800a5bfbfff2cf5a4f47daa8b19e5abf033af28
Adam Lewis <23342526+Adam-D-Lewis@users.noreply.github.com>
2024-09-17 11:15:55 -0500

    add test for changing dicts and lists (#2724)

--- src/_nebari/stages/terraform_state/__init__.py
+++ src/_nebari/stages/terraform_state/__init__.py
@@ -1,6 +1,5 @@
 import contextlib
 import enum
-import functools
 import inspect
 import os
 import pathlib
@@ -266,9 +265,15 @@ class TerraformStateStage(NebariTerraformStage):
         for keys, old, new in nebari_config_diff.modified():
             bottom_level_schema = self.config
             if len(keys) > 1:
-                bottom_level_schema = functools.reduce(
-                    lambda m, k: getattr(m, k), keys[:-1], self.config
-                )
+                for key in keys[:-1]:
+                    try:
+                        bottom_level_schema = getattr(bottom_level_schema, key)
+                    except AttributeError as e:
+                        if isinstance(bottom_level_schema, dict):
+                            # handle case where value is a dict
+                            bottom_level_schema = bottom_level_schema[key]
+                        else:
+                            raise e
             extra_field_schema = schema.ExtraFieldSchema(
                 **bottom_level_schema.model_fields[keys[-1]].json_schema_extra or {}
             )
