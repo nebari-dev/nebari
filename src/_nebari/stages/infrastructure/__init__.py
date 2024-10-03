21e8487d4f7e2f964183324ed4f90b29740a9bbe
joneszc <joneszc@iu.edu>
2024-10-03 12:53:56 -0400

    Add validation checks for config option amazon_web_services.eks_kms_arn to ensure KMS-key ARN available

--- src/_nebari/stages/infrastructure/__init__.py
+++ src/_nebari/stages/infrastructure/__init__.py
@@ -562,6 +562,37 @@ class AmazonWebServicesProvider(schema.Base):
                         f"Amazon Web Services instance {node_group.instance} not one of available instance types={available_instances}"
                     )
 
+        # check if kms key is valid
+        available_kms_keys = amazon_web_services.kms_key_arns(data["region"])
+        if "eks_kms_arn" in data and data["eks_kms_arn"] is not None:
+            key_id = [
+                id for id in available_kms_keys.keys() if id in data["eks_kms_arn"]
+            ]
+            if (
+                len(key_id) == 1
+                and available_kms_keys[key_id[0]]["Arn"] == data["eks_kms_arn"]
+            ):
+                key_id = key_id[0]
+                # Symmetric KMS keys with Encrypt and decrypt key-usage have the SYMMETRIC_DEFAULT key-spec
+                # EKS cluster encryption requires a Symmetric key that is set to encrypt and decrypt data
+                if available_kms_keys[key_id]["KeySpec"] != "SYMMETRIC_DEFAULT":
+                    if available_kms_keys[key_id]["KeyUsage"] == "GENERATE_VERIFY_MAC":
+                        raise ValueError(
+                            f"Amazon Web Services KMS Key with ID {key_id} does not have KeyUsage set to 'Encrypt and decrypt' data"
+                        )
+                    elif available_kms_keys[key_id]["KeyUsage"] != "ENCRYPT_DECRYPT":
+                        raise ValueError(
+                            f"Amazon Web Services KMS Key with ID {key_id} is not of type Symmetric, and KeyUsage not set to 'Encrypt and decrypt' data"
+                        )
+                    else:
+                        raise ValueError(
+                            f"Amazon Web Services KMS Key with ID {key_id} is not of type Symmetric"
+                        )
+            else:
+                raise ValueError(
+                    f"Amazon Web Services KMS Key with ARN {data['eks_kms_arn']} not one of available/enabled keys={[v['Arn'] for v in available_kms_keys.values()]}"
+                )
+
         return data
 
 
