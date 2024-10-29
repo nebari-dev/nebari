d1572ebf44aa504e9fa833ef868b5653a984f123
krassowski <5832902+krassowski@users.noreply.github.com>
2024-10-29 10:43:29 +0000

    Disable `jupyterlab-jhub-apps` extension when jhub-apps is disabled (if installed)

--- src/_nebari/stages/kubernetes_services/template/modules/kubernetes/services/jupyterhub/main.tf
+++ src/_nebari/stages/kubernetes_services/template/modules/kubernetes/services/jupyterhub/main.tf
@@ -104,6 +104,11 @@ resource "helm_release" "jupyterhub" {
               kind      = "configmap"
             }
 
+            "/opt/conda/envs/default/etc/jupyter/labconfig/page_config.json" = {
+              name      = kubernetes_config_map.jupyterlab-page-config.metadata.0.name
+              namespace = kubernetes_config_map.jupyterlab-page-config.metadata.0.namespace
+              kind      = "configmap"
+            }
           }
         )
         environments = var.conda-store-environments
