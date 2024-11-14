3929ded46d583d26e9c3f612fae680af84cec0d0
Chuck McAndrew <6248903+dcmcand@users.noreply.github.com>
2024-11-14 11:28:04 +0100

    make tests pass

--- tests/tests_deployment/test_jupyterhub_ssh.py
+++ tests/tests_deployment/test_jupyterhub_ssh.py
@@ -1,5 +1,7 @@
+import re
 import string
 import time
+import uuid
 
 import paramiko
 import pytest
@@ -28,78 +30,60 @@ def paramiko_object(jupyterhub_access_token):
         "password": jupyterhub_access_token,
         "allow_agent": constants.PARAMIKO_SSH_ALLOW_AGENT,
         "look_for_keys": constants.PARAMIKO_SSH_LOOK_FOR_KEYS,
-        "auth_timeout": TIMEOUT_SECS,
-        "banner_timeout": TIMEOUT_SECS,
     }
 
     ssh_client = paramiko.SSHClient()
     ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-    max_attempts = int(TIMEOUT_SECS / 10)
-    delay = 10  # seconds
-
-    for attempt in range(max_attempts):
-        try:
-            ssh_client.connect(**params)
-            break
-        except (
-            paramiko.ssh_exception.NoValidConnectionsError,
-            paramiko.ssh_exception.SSHException,
-        ):
-            print(
-                f"SSH connection failed on attempt {attempt + 1}/{max_attempts}, "
-                f"retrying in {delay} seconds..."
-            )
-            time.sleep(delay)
-    else:
-        pytest.fail("Could not establish SSH connection after multiple attempts.")
-
-    yield ssh_client
+    yield ssh_client, params
 
     ssh_client.close()
 
 
-def run_command(command, channel):
-    _in, _out, _err = channel.exec_command(command)
-
-    return _out.read().decode("utf-8").strip()
-    # delimiter = uuid.uuid4().hex
-    # channel.send(f"echo {delimiter}start; {command}; echo {delimiter}end\n")
-
-    # output = ""
-
-    # while True:
-    #     if channel.recv_ready():
-    #         recv = channel.recv(1024).decode("utf-8")
-    #         output += recv
-    #         if f"{delimiter}end" in recv:
-    #             break
-    #     else:
-    #         time.sleep(0.1)  # Slight delay to prevent busy-waiting
-
-    # # Extract the command output between the start and end delimiters
-    # match = re.search(f"{delimiter}start(.*){delimiter}end", output, re.DOTALL)
-    # if match:
-    #     return match.group(1).strip()
-    # else:
-    #     return output.strip()
+def invoke_shell(
+    client: paramiko.SSHClient, params: dict[str, any]
+) -> paramiko.Channel:
+    client.connect(**params)
+    return client.invoke_shell()
 
 
-# @pytest.mark.timeout(TIMEOUT_SECS)
-# @pytest.mark.filterwarnings("ignore::urllib3.exceptions.InsecureRequestWarning")
-# @pytest.mark.filterwarnings("ignore::ResourceWarning")
-# def test_simple_jupyterhub_ssh(paramiko_object):
-#     channel = paramiko_object.invoke_shell()
-# channel.close()
+def extract_output(delimiter: str, output: str) -> str:
+    # Extract the command output between the start and end delimiters
+    match = re.search(rf"{delimiter}start\n(.*)\n{delimiter}end", output, re.DOTALL)
+    if match:
+        print(match.group(1).strip())
+        return match.group(1).strip()
+    else:
+        return output.strip()
+
+
+def run_command_list(commands: list[str], channel: paramiko.Channel) -> dict[str, str]:
+    command_delimiters = {}
+    for command in commands:
+        delimiter = uuid.uuid4().hex
+        command_delimiters[command] = delimiter
+        b = channel.send(f"echo {delimiter}start; {command}; echo {delimiter}end\n")
+        if b == 0:
+            print(f"Command '{command}' failed to send")
+    while not channel.recv_ready():
+        time.sleep(1)
+        print("Waiting for output")
+    output = ""
+    while channel.recv_ready():
+        output += channel.recv(65535).decode("utf-8")
+    outputs = {}
+    for command, delimiter in command_delimiters.items():
+        command_output = extract_output(delimiter, output)
+        outputs[command] = command_output
+    return outputs
 
 
 @pytest.mark.timeout(TIMEOUT_SECS)
 @pytest.mark.filterwarnings("ignore::urllib3.exceptions.InsecureRequestWarning")
 @pytest.mark.filterwarnings("ignore::ResourceWarning")
 def test_print_jupyterhub_ssh(paramiko_object):
-    channel = paramiko_object
-    # channel = paramiko_object.invoke_shell()
-
+    client, params = paramiko_object
+    channel = invoke_shell(client, params)
     # Commands to run and just print the output
     commands_print = [
         "id",
@@ -109,66 +93,60 @@ def test_print_jupyterhub_ssh(paramiko_object):
         "ls -la",
         "umask",
     ]
-
-    for command in commands_print:
-        print(f'COMMAND: "{command}"')
-        print(run_command(command, channel))
-
-    # channel.close()
+    outputs = run_command_list(commands_print, channel)
+    for command, output in outputs.items():
+        print(f"COMMAND: {command}")
+        print(f"OUTPUT: {output}")
+    channel.close()
 
 
 @pytest.mark.timeout(TIMEOUT_SECS)
 @pytest.mark.filterwarnings("ignore::urllib3.exceptions.InsecureRequestWarning")
 @pytest.mark.filterwarnings("ignore::ResourceWarning")
 def test_exact_jupyterhub_ssh(paramiko_object):
-    channel = paramiko_object
-    # channel = paramiko_object.invoke_shell()
-
+    client, params = paramiko_object
+    channel = invoke_shell(client, params)
     # Commands to run and exactly match output
-    commands_exact = [
-        ("id -u", "1000"),
-        ("id -g", "100"),
-        ("whoami", constants.KEYCLOAK_USERNAME),
-        ("pwd", f"/home/{constants.KEYCLOAK_USERNAME}"),
-        ("echo $HOME", f"/home/{constants.KEYCLOAK_USERNAME}"),
-        ("conda activate default && echo $CONDA_PREFIX", "/opt/conda/envs/default"),
-        (
-            "hostname",
-            f"jupyter-{escape_string(constants.KEYCLOAK_USERNAME, safe=set(string.ascii_lowercase + string.digits), escape_char='-').lower()}",
-        ),
-    ]
-
-    for command, expected_output in commands_exact:
-        output = run_command(command, channel)
+    commands_exact = {
+        "id -u": "1000",
+        "id -g": "100",
+        "whoami": constants.KEYCLOAK_USERNAME,
+        "pwd": f"/home/{constants.KEYCLOAK_USERNAME}",
+        "echo $HOME": f"/home/{constants.KEYCLOAK_USERNAME}",
+        "conda activate default && echo $CONDA_PREFIX": "/opt/conda/envs/default",
+        "hostname": f"jupyter-{escape_string(constants.KEYCLOAK_USERNAME, safe=set(string.ascii_lowercase + string.digits), escape_char='-').lower()}",
+    }
+    outputs = run_command_list(list(commands_exact.keys()), channel)
+    for command, output in outputs.items():
         assert (
-            expected_output in output
-        ), f"Command '{command}' output '{output}' does not match expected '{expected_output}'"
+            output == outputs[command]
+        ), f"Command '{command}' output '{outputs[command]}' does not match expected '{output}'"
 
-    # channel.close()
+    channel.close()
 
 
 @pytest.mark.timeout(TIMEOUT_SECS)
 @pytest.mark.filterwarnings("ignore::urllib3.exceptions.InsecureRequestWarning")
 @pytest.mark.filterwarnings("ignore::ResourceWarning")
 def test_contains_jupyterhub_ssh(paramiko_object):
-    channel = paramiko_object
-    # channel = paramiko_object.invoke_shell()
+    client, params = paramiko_object
+    channel = invoke_shell(client, params)
 
     # Commands to run and check if the output contains specific strings
-    commands_contain = [
-        ("ls -la", ".bashrc"),
-        ("cat ~/.bashrc", "Managed by Nebari"),
-        ("cat ~/.profile", "Managed by Nebari"),
-        ("cat ~/.bash_logout", "Managed by Nebari"),
+    commands_contain = {
+        "ls -la": ".bashrc",
+        "cat ~/.bashrc": "Managed by Nebari",
+        "cat ~/.profile": "Managed by Nebari",
+        "cat ~/.bash_logout": "Managed by Nebari",
         # Ensure we don't copy over extra files from /etc/skel in init container
-        ("ls -la ~/..202*", "No such file or directory"),
-        ("ls -la ~/..data", "No such file or directory"),
-    ]
+        "ls -la ~/..202*": "No such file or directory",
+        "ls -la ~/..data": "No such file or directory",
+    }
 
-    for command, expected_substring in commands_contain:
-        output = run_command(command, channel)
+    outputs = run_command_list(commands_contain.keys(), channel)
+    for command, expected_output in commands_contain.items():
         assert (
-            expected_substring in output
-        ), f"Command '{command}' output does not contain expected substring '{expected_substring}'"
+            expected_output in outputs[command]
+        ), f"Command '{command}' output does not contain expected substring '{commands_contain[output]}'. Instead got '{outputs[command]}'"
 
-    # channel.close()
+    channel.close()
