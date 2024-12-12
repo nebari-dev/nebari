b6ffec8f3c0567be991f5a722a18df69fab39ade
Adam Lewis <23342526+Adam-D-Lewis@users.noreply.github.com>
2024-12-12 16:43:52 -0600

    add version checking

--- src/_nebari/config_set.py
+++ src/_nebari/config_set.py
@@ -1,17 +1,33 @@
 import pathlib
 
-from pydantic import BaseModel, ConfigDict
+from packaging.requirements import Requirement
+from pydantic import BaseModel, ConfigDict, field_validator
 
+from _nebari._version import __version__
 from _nebari.utils import yaml
 
 
 class ConfigSetMetadata(BaseModel):
-    model_config: ConfigDict = ConfigDict(
-        extra="allow",
-    )
-    name: str = None  # for use with guided init
+    model_config: ConfigDict = ConfigDict(extra="allow", arbitrary_types_allowed=True)
+    name: str  # for use with guided init
     description: str = None
-    nebari_version: str = None
+    nebari_version: str | Requirement
+
+    @field_validator("nebari_version")
+    @classmethod
+    def validate_version_requirement(cls, version_req):
+        if isinstance(version_req, str):
+            version_req = Requirement(f"nebari{version_req}")
+            version_req.specifier.prereleases = True
+
+        return version_req
+
+    def check_version(self, version):
+        if version not in self.nebari_version.specifier:
+            raise ValueError(
+                f"Current Nebari version {__version__} is not compatible with "
+                f'required version {self.nebari_version.specifier} for "{self.name}" config set.'
+            )
 
 
 class ConfigSet(BaseModel):
@@ -27,7 +43,9 @@ def read_config_set(config_set_filepath: str):
     with filename.open() as f:
         config_set_yaml = yaml.load(f)
 
-    # TODO: Validation e.g. Check Nebari version
     config_set = ConfigSet(**config_set_yaml)
+
+    # validation
+    config_set.metadata.check_version(__version__)
 
     return config_set
