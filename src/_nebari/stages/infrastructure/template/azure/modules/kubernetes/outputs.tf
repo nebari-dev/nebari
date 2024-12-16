aaadaa70ece1517707a55ecfa8cd458a3cb2e6d6
viniciusdc <vinivdc2009@hotmail.com>
2024-12-16 13:10:58 -0300

    update kubernetes output creds based on azure rbac

--- src/_nebari/stages/infrastructure/template/azure/modules/kubernetes/outputs.tf
+++ src/_nebari/stages/infrastructure/template/azure/modules/kubernetes/outputs.tf
@@ -1,21 +1,20 @@
 output "credentials" {
-  description = "Credentials required for connecting to kubernetes cluster"
+  description = "Credentials required for connecting to Kubernetes cluster"
   sensitive   = true
   value = {
-    # see bottom of https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/kubernetes_cluster
-    endpoint               = azurerm_kubernetes_cluster.main.kube_config.0.host
-    username               = azurerm_kubernetes_cluster.main.kube_config.0.username
-    password               = azurerm_kubernetes_cluster.main.kube_config.0.password
-    client_certificate     = base64decode(azurerm_kubernetes_cluster.main.kube_config.0.client_certificate)
-    client_key             = base64decode(azurerm_kubernetes_cluster.main.kube_config.0.client_key)
-    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.main.kube_config.0.cluster_ca_certificate)
+    endpoint               = var.azure_rbac.enable ? azurerm_kubernetes_cluster.main.kube_admin_config.0.host : azurerm_kubernetes_cluster.main.kube_config.0.host
+    username               = var.azure_rbac.enable ? azurerm_kubernetes_cluster.main.kube_admin_config.0.username : azurerm_kubernetes_cluster.main.kube_config.0.username
+    password               = var.azure_rbac.enable ? azurerm_kubernetes_cluster.main.kube_admin_config.0.password : azurerm_kubernetes_cluster.main.kube_config.0.password
+    client_certificate     = var.azure_rbac.enable ? azurerm_kubernetes_cluster.main.kube_admin_config.0.client_certificate : azurerm_kubernetes_cluster.main.kube_config.0.client_certificate
+    client_key             = var.azure_rbac.enable ? azurerm_kubernetes_cluster.main.kube_admin_config.0.client_key : azurerm_kubernetes_cluster.main.kube_config.0.client_key
+    cluster_ca_certificate = var.azure_rbac.enable ? azurerm_kubernetes_cluster.main.kube_admin_config.0.cluster_ca_certificate : azurerm_kubernetes_cluster.main.kube_config.0.cluster_ca_certificate
   }
 }
 
 output "kubeconfig" {
   description = "Kubernetes connection kubeconfig"
   sensitive   = true
-  value       = azurerm_kubernetes_cluster.main.kube_config_raw
+  value       = var.azure_rbac.enable ? azurerm_kubernetes_cluster.main.kube_admin_config_raw : azurerm_kubernetes_cluster.main.kube_config_raw
 }
 
 output "cluster_oidc_issuer_url" {
