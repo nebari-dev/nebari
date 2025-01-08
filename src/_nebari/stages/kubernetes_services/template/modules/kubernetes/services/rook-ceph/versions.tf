a89b0aec17297c12eb48ef97ec7044d5b3d34508
Chuck McAndrew <6248903+dcmcand@users.noreply.github.com>
2025-01-08 11:06:57 +0100

    update to latest k8s version, set max k8s version to only major and minor versions, and upgrade k8s and helm terraform providers

--- src/_nebari/stages/kubernetes_services/template/modules/kubernetes/services/rook-ceph/versions.tf
+++ src/_nebari/stages/kubernetes_services/template/modules/kubernetes/services/rook-ceph/versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     helm = {
       source  = "hashicorp/helm"
-      version = "2.1.2"
+      version = "2.17.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "2.20.0"
+      version = "2.35.1"
     }
   }
   required_version = ">= 1.0"
--- src/_nebari/stages/kubernetes_services/template/versions.tf
+++ src/_nebari/stages/kubernetes_services/template/versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     helm = {
       source  = "hashicorp/helm"
-      version = "2.1.2"
+      version = "2.17.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "2.20.0"
+      version = "2.35.1"
     }
     keycloak = {
       source  = "mrparkers/keycloak"
