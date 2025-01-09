c7ab98a5dece36cc5011bcbcf95c148de2dc3839
Chuck McAndrew <6248903+dcmcand@users.noreply.github.com>
2025-01-09 18:24:53 +0100

    roll back helm chart provider upgrade

--- src/_nebari/stages/kubernetes_keycloak_configuration/template/versions.tf
+++ src/_nebari/stages/kubernetes_keycloak_configuration/template/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     helm = {
       source  = "hashicorp/helm"
-      version = "2.17.0"
+      version = "2.1.2"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
