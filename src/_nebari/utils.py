7e5c2b08c879c31d61dd7859ce688f10492a14a6
Adam Lewis <23342526+Adam-D-Lewis@users.noreply.github.com>
2025-01-29 16:58:21 -0600

    Revert "strip all ansi formatting sequences"
    This reverts commit 556661ff98b340438a1e175702aac4e226f15b48.

--- src/_nebari/utils.py
+++ src/_nebari/utils.py
@@ -48,9 +48,10 @@ def change_directory(directory):
 
 
 def strip_ansi_errors(line):
-    """Strips ANSI escape codes from a string."""
-    ansi_escape = re.compile(rb"\x1b\[[0-9;]*[mK]")
-    return ansi_escape.sub(b"", line)
+    """Strips red ANSI escape code from a string."""
+    ansi_escape = re.compile(rb"\x1b\[31m")
+    stripped_line = ansi_escape.sub("", line.decode("utf-8"))
+    return stripped_line.encode("utf-8")
 
 
 def process_streams(
