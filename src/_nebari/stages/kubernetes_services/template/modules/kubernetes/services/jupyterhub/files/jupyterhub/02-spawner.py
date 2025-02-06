9e7146845cb014aef41b66081c49d69a422e1d63
sophia <scastellarin@quansight.com>
2025-02-06 14:58:56 -0800

    Add comments

--- src/_nebari/stages/kubernetes_services/template/modules/kubernetes/services/jupyterhub/files/jupyterhub/02-spawner.py
+++ src/_nebari/stages/kubernetes_services/template/modules/kubernetes/services/jupyterhub/files/jupyterhub/02-spawner.py
@@ -53,11 +53,47 @@ def get_scoped_token(
     groups: list[str] = None,
     admin: bool = False,
 ):
+    """Generate a conda store auth token for a given set of groups. The
+    token will only have `view` permissions for each group (regardless if
+    the user has higher permissions associated with the group generally).
+
+    If the user is an admin, then the token will have `view` permissions
+    for all namespaces.
+
+    By default, the user will have view permissions for the following 
+    groups:
+    - `default/*`
+    - `filesystem/*`
+    - `nebari-git/*`
+    - `global/*`
+
+    Parameters
+    ----------
+    conda_store_url : str
+        The URL of the Conda Store instance.
+    admin_token : str
+        The admin token used to authenticate with the Conda Store API.
+    name : str
+        The name of the user for whom the token will be generated (the token
+        will have `view` permission to this namespace).
+    groups : list[str], optional
+        A list of group names that the user should have access to. If not provided,
+        default roles will be assigned.
+    admin : bool, optional
+        Whether the user is an admin. If true, the token will have `view` permissions
+        for all namespaces. Defaults to False.
+
+    Returns
+    -------
+    str
+        The generated token.
+    """
     import urllib3
 
     token_endpoint = f"http://{conda_store_url}/conda-store/api/v1/token/"
     http = urllib3.PoolManager()
 
+    # add default role bindings
     role_bindings = {
         "role_bindings": {
             f"{name}/*": ["viewer"],
@@ -68,16 +104,19 @@ def get_scoped_token(
         }
     }
 
+    # add role bindings for all the groups the user is part of
     if groups is not None:
         for group in groups:
             group = group.replace("/", "")
             role_bindings["role_bindings"][f"{group}/*"] = ["viewer"]
 
+    # if the user is an admin, they can view all namespace + environments
     if admin:
         role_bindings["role_bindings"]["*/*"] = ["viewer"]
 
     encoded_body = json.dumps(role_bindings)
 
+    # generate a token with with the generated role bindings
     token_response = http.request(
         "POST",
         str(token_endpoint),
@@ -95,8 +134,46 @@ def get_scoped_token(
 # adding tests in a traditional sense is not possible. See https://github.com/soapy1/nebari/tree/try-unit-test-spawner
 # for a demo on one approach to adding test.
 def get_conda_store_environments(user_info: dict):
-    import os
+    """Gets the conda-store environemnts for a given user using the v1 environment 
+    API.
 
+    This scopes permissions for the given user using the `groups` field in the
+    user_info dict. The user_info dict comes from Jupyter's user dict. The groups
+    in this dict come from the keycloak groups, which include the list of conda-store
+    namespaces the user has access to. For the purpose of this function, we can assume
+    that if the user is part of the group, it at least has `view` permissions on the 
+    conda-store namespaces.
+
+    Parameters
+    ----------
+    user_info : dict
+        A dictionary containing user information originating from the JupyterHub 
+        user info. The scheme of the user info is:
+        ```
+        {
+            "name": "string",
+            "admin": true,
+            "roles": [
+                "string"
+            ],
+            "groups": [
+                "string"
+            ],
+            "server": "string",
+            "pending": "spawn",
+            "last_activity": "2019-08-24T14:15:22Z",
+            "servers": {
+            },
+            "auth_state": {}
+        }
+        ```
+    
+    Returns
+    -------
+    list[str]
+        A list of all conda-store environments for the given user
+    """
+    import os
     import urllib3
 
     # Check for the environment variable `CONDA_STORE_API_PAGE_SIZE_LIMIT`. Fall
@@ -114,8 +191,10 @@ def get_conda_store_environments(user_info: dict):
     groups = user_info["groups"]
     name = user_info["name"]
     admin = user_info["admin"]
+    # get token with appropriate scope for the user making the request
     scoped_token = get_scoped_token(external_url, token, name, groups, admin)
 
+    # get total number of records from the endpoint
     total_records = get_total_records(base_url, scoped_token)
 
     # will contain all the environment info returned from the api
