546ff6e4ac57d4201e207955f398ce40045045de
Chuck McAndrew <6248903+dcmcand@users.noreply.github.com>
2025-03-12 14:16:26 +0100

    add 2025.3.1 upgrade

--- src/_nebari/upgrade.py
+++ src/_nebari/upgrade.py
@@ -1710,6 +1710,23 @@ class Upgrade_2025_2_1(UpgradeStep):
         return config
 
 
+class Upgrade_2025_3_1(UpgradeStep):
+    """
+    Upgrade step for Nebari version 2025.3.1
+    """
+
+    version = "2025.3.1"
+
+    @override
+    def _version_specific_upgrade(
+        self, config, start_version, config_filename: Path, *args, **kwargs
+    ):
+
+        rich.print("Ready to upgrade to Nebari version [green]2025.3.1[/green].")
+
+        return config
+
+
 __rounded_version__ = str(rounded_ver_parse(__version__))
 
 # Manually-added upgrade steps must go above this line
