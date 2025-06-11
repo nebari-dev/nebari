3e7dc3e70fe69ef23ea0ef518b43d53f56171a6c
Satrajit Ghosh <satra@mit.edu>
2025-06-11 19:30:44 -0400

    fix: run precommit

--- src/_nebari/stages/infrastructure/__init__.py
+++ src/_nebari/stages/infrastructure/__init__.py
@@ -188,7 +188,7 @@ class AWSNodeGroupInputVars(schema.Base):
     max_size: int
     single_subnet: bool
     permissions_boundary: Optional[str] = None
-    capacity_type: Optional[Literal['SPOT', 'ON_DEMAND']] = None
+    capacity_type: Optional[Literal["SPOT", "ON_DEMAND"]] = None
     ami_type: Optional[AWSAmiTypes] = None
     launch_template: Optional[AWSNodeLaunchTemplate] = None
     node_taints: list[dict]
@@ -522,7 +522,7 @@ class AWSNodeGroup(NodeGroup):
     gpu: bool = False
     single_subnet: bool = False
     permissions_boundary: Optional[str] = None
-    capacity_type: Optional[Literal['SPOT', 'ON_DEMAND']] = None
+    capacity_type: Optional[Literal["SPOT", "ON_DEMAND"]] = None
     # Disabled as part of 2024.11.1 until #2832 is resolved
     # launch_template: Optional[AWSNodeLaunchTemplate] = None
 
@@ -961,7 +961,11 @@ class KubernetesInfrastructureStage(NebariTerraformStage):
                             gpu_enabled=node_group.gpu,
                             launch_template=None,
                         ),
-                        capacity_type = node_group.capacity_type.strip() if node_group.capacity_type else "ON_DEMAND"
+                        capacity_type=(
+                            node_group.capacity_type.strip()
+                            if node_group.capacity_type
+                            else "ON_DEMAND"
+                        ),
                     )
                     for name, node_group in self.config.amazon_web_services.node_groups.items()
                 ],
