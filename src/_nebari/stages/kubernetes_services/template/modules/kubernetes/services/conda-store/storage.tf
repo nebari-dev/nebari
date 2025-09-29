ac0917d63e17beb07839664dfe490ace3d615841
Austin Macdonald <austin@dartmouth.edu>
2025-09-29 15:42:42 -0500

    override bitnami redis repo (default removed) (#3140)

--- src/_nebari/stages/kubernetes_services/template/modules/kubernetes/services/conda-store/storage.tf
+++ src/_nebari/stages/kubernetes_services/template/modules/kubernetes/services/conda-store/storage.tf
@@ -34,4 +34,18 @@ module "redis" {
   namespace = var.namespace
 
   node-group = var.node-group
+
+  # TODO: Remove hardcoded image values after Helm chart update
+  # This is a workaround due to bitnami charts deprecation
+  # See: https://github.com/bitnami/charts/issues/35164
+  # https://github.com/nebari-dev/nebari/issues/3139
+  overrides = [
+    jsonencode({
+      image = {
+        registry = "docker.io"
+        repository = "bitnamilegacy/redis"
+        tag = "7.0.4-debian-11-r4"
+      }
+    })
+  ]
 }
