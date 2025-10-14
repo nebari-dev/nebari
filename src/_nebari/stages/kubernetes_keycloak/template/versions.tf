042c7364a48ea7c503fec4cab079f21265cc0462
Tyler Potts <tylerman@Tylers-MacBook-Pro.local>
2025-10-14 13:51:19 -0600

    bump helm provider version, add standalone postgres

--- src/_nebari/stages/kubernetes_keycloak/template/versions.tf
+++ src/_nebari/stages/kubernetes_keycloak/template/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     helm = {
       source  = "hashicorp/helm"
-      version = "2.1.2"
+      version = "~> 2.16.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
